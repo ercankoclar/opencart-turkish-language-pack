42dae8a5b77f4d5a478a80cf12fbf582a4584843
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:30:22 +0300

    Update public_html/admin/language/tr-tr/localisation/geo_zone.php

--- public_html/admin/language/tr-tr/localisation/geo_zone.php
+++ public_html/admin/language/tr-tr/localisation/geo_zone.php
@@ -3,25 +3,25 @@
 $_['heading_title']      = 'Coğrafi Bölgeler';
 
 // Text
-$_['text_success']       = 'Success: You have modified geo zones!';
-$_['text_list']          = 'Geo Zone List';
-$_['text_add']           = 'Add Geo Zone';
-$_['text_edit']          = 'Edit Geo Zone';
+$_['text_success']       = 'Başarılı: Coğrafi bölgeleri değiştirdiniz!';
+$_['text_list']          = 'Coğrafi Bölge Listesi';
+$_['text_add']           = 'Coğrafi Bölge Ekle';
+$_['text_edit']          = 'Coğrafi Bölgeyi Düzenle';
 $_['text_geo_zone']      = 'Coğrafi Bölgeler';
 
 // Column
-$_['column_name']        = 'Geo Zone Name';
+$_['column_name']        = 'Coğrafi Bölge Adı';
 $_['column_description'] = 'Açıklama';
 $_['column_action']      = 'İşlem';
 
 // Entry
-$_['entry_name']         = 'Geo Zone Name';
+$_['entry_name']         = 'Coğrafi Bölge Adı';
 $_['entry_description']  = 'Açıklama';
 $_['entry_country']      = 'Ülke';
 $_['entry_zone']         = 'Bölge';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify geo zones!';
-$_['error_name']         = 'Geo Zone Name must be between 3 and 32 characters!';
-$_['error_description']  = 'Description Name must be between 3 and 255 characters!';
-$_['error_tax_rate']     = 'Warning: This geo zone cannot be deleted as it is currently assigned to one or more tax rates!';
+$_['error_permission']   = 'Uyarı: Coğrafi bölgeleri değiştirme yetkiniz yok!';
+$_['error_name']         = 'Coğrafi Bölge Adı 3 ile 32 karakter arasında olmalıdır!';
+$_['error_description']  = 'Açıklama Adı 3 ile 255 karakter arasında olmalıdır!';
+$_['error_tax_rate']     = 'Uyarı: Bu coğrafi bölge bir veya daha fazla vergi oranına atanmış olduğu için silinemez!';
