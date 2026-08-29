0fcbc57d73339c2b89906dbf9a07162f903d2b40
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:15:08 +0300

    Update public_html/admin/language/tr-tr/common/security.php

--- public_html/admin/language/tr-tr/common/security.php
+++ public_html/admin/language/tr-tr/common/security.php
@@ -1,47 +1,47 @@
 <?php
 // Heading
-$_['heading_title']                   = 'Important Security Notification!';
+$_['heading_title']                   = 'Önemli Güvenlik Bildirimi!';
 
 // Text
-$_['text_install']                    = 'Delete installation directory';
-$_['text_install_description']        = 'Your installation directory needs deleting!';
-$_['text_install_success']            = 'Success: Install directory has been deleted!';
-$_['text_storage']                    = 'Move storage directory';
-$_['text_storage_description']        = 'It is very important that you move the storage directory outside of the web directory (e.g. public_html, www or htdocs).';
-$_['text_storage_move']               = 'Moving %s to %s of %s storage files';
-$_['text_storage_success']            = 'Success: Storage directory has been moved!';
-$_['text_storage_delete']             = 'Delete previous storage directory';
-$_['text_storage_delete_description'] = 'Your previous storage directory needs deleting!';
-$_['text_storage_delete_success']     = 'Success: The previous storage directory has been deleted!';
-$_['text_admin']                      = 'Move admin directory';
-$_['text_admin_description']          = 'Please type in a new admin directory name in the field below.';
-$_['text_admin_move']                 = 'Moving %s to %s of %s admin files';
-$_['text_admin_success']              = 'Success: Admin directory has been moved!';
-$_['text_admin_delete']               = 'Delete previous admin directory';
-$_['text_admin_delete_description']   = 'Your previous admin directory needs deleting!';
-$_['text_admin_delete_success']       = 'Success: Your previous admin directory has been deleted!';
-$_['text_path']                       = 'Path';
+$_['text_install']                    = 'Kurulum dizinini sil';
+$_['text_install_description']        = 'Kurulum dizininizin silinmesi gerekiyor!';
+$_['text_install_success']            = 'Başarılı: Kurulum dizini silindi!';
+$_['text_storage']                    = 'Depolama dizinini taşı';
+$_['text_storage_description']        = 'Depolama dizinini web dizininin dışına taşımanız çok önemlidir (örneğin public_html, www veya htdocs).';
+$_['text_storage_move']               = '%s depolama dosyasından %s tanesi %s konumuna taşınıyor';
+$_['text_storage_success']            = 'Başarılı: Depolama dizini taşındı!';
+$_['text_storage_delete']             = 'Önceki depolama dizinini sil';
+$_['text_storage_delete_description'] = 'Önceki depolama dizininizin silinmesi gerekiyor!';
+$_['text_storage_delete_success']     = 'Başarılı: Önceki depolama dizini silindi!';
+$_['text_admin']                      = 'Yönetim dizinini taşı';
+$_['text_admin_description']          = 'Lütfen aşağıdaki alana yeni bir yönetim dizini adı yazın.';
+$_['text_admin_move']                 = '%s yönetim dosyasından %s tanesi %s konumuna taşınıyor';
+$_['text_admin_success']              = 'Başarılı: Yönetim dizini taşındı!';
+$_['text_admin_delete']               = 'Önceki yönetim dizinini sil';
+$_['text_admin_delete_description']   = 'Önceki yönetim dizininizin silinmesi gerekiyor!';
+$_['text_admin_delete_success']       = 'Başarılı: Önceki yönetim dizininiz silindi!';
+$_['text_path']                       = 'Yol';
 
 // Entry
-$_['entry_path_current']              = 'Current Path';
-$_['entry_path_new']                  = 'New Path';
-$_['entry_name']                      = 'Directory Name';
+$_['entry_path_current']              = 'Mevcut Yol';
+$_['entry_path_new']                  = 'Yeni Yol';
+$_['entry_name']                      = 'Dizin Adı';
 
 // Buttons
-$_['button_move']                     = 'Move';
+$_['button_move']                     = 'Taşı';
 
 // Help
-$_['help_storage']                    = 'Storage directory name must begin with storage. e.g. `storage_`.';
+$_['help_storage']                    = 'Depolama dizini adı storage ile başlamalıdır. Örneğin `storage_`.';
 
 // Error
-$_['error_permission']                = 'Warning: You do not have permission to modify security!';
-$_['error_install']                   = 'Warning: Install directory does not exist!';
-$_['error_storage']                   = 'Warning: Storage directory does not exist!';
-$_['error_storage_root']              = 'Warning: Storage directory must be outside the web root!';
-$_['error_storage_name']              = 'Warning: Storage directory name must start with `storage`! e.g. `storage_`.';
-$_['error_admin']                     = 'Warning: Admin directory does not exist!';
-$_['error_admin_allowed']             = 'Warning: This admin directory name can not be used!';
-$_['error_admin_exists']              = 'Warning: Admin directory already exists!';
-$_['error_writable']                  = 'Warning: config.php and admin/config.php need to be made writable!';
-$_['error_writable_path']             = 'Warning: The folder \'%s\' needs to be writable!';
-$_['error_remove']                    = 'Warning: Directory does not exist to remove!';
+$_['error_permission']                = 'Uyarı: Güvenlik ayarlarını değiştirme yetkiniz yok!';
+$_['error_install']                   = 'Uyarı: Kurulum dizini mevcut değil!';
+$_['error_storage']                   = 'Uyarı: Depolama dizini mevcut değil!';
+$_['error_storage_root']              = 'Uyarı: Depolama dizini web kök dizininin dışında olmalıdır!';
+$_['error_storage_name']              = 'Uyarı: Depolama dizini adı `storage` ile başlamalıdır! Örneğin `storage_`.';
+$_['error_admin']                     = 'Uyarı: Yönetim dizini mevcut değil!';
+$_['error_admin_allowed']             = 'Uyarı: Bu yönetim dizini adı kullanılamaz!';
+$_['error_admin_exists']              = 'Uyarı: Yönetim dizini zaten mevcut!';
+$_['error_writable']                  = 'Uyarı: config.php ve admin/config.php dosyalarının yazılabilir hale getirilmesi gerekiyor!';
+$_['error_writable_path']             = 'Uyarı: `%s` klasörünün yazılabilir olması gerekiyor!';
+$_['error_remove']                    = 'Uyarı: Kaldırılacak dizin mevcut değil!';
