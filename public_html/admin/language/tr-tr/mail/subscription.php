09faa576699e8c41c784234adf91d081f63264be
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:43:39 +0300

    Update public_html/admin/language/tr-tr/mail/subscription.php

--- public_html/admin/language/tr-tr/mail/subscription.php
+++ public_html/admin/language/tr-tr/mail/subscription.php
@@ -1,10 +1,10 @@
 <?php
 // Text
-$_['text_subject']             = '%s - Subscription';
-$_['text_subscription_id']     = 'Subscription ID';
-$_['text_date_added']          = 'Subscription Date:';
-$_['text_subscription_status'] = 'Your subscription has been added to the following status:';
-$_['text_comment']             = 'The comments for your subscription are:';
-$_['text_payment_method']      = 'Payment Method';
-$_['text_payment_code']        = 'Payment Code';
-$_['text_footer']              = 'Please reply to this email if you have any questions.';
+$_['text_subject']             = '%s - Abonelik';
+$_['text_subscription_id']     = 'Abonelik No';
+$_['text_date_added']          = 'Abonelik Tarihi:';
+$_['text_subscription_status'] = 'Aboneliğiniz aşağıdaki duruma güncellendi:';
+$_['text_comment']             = 'Aboneliğinizle ilgili açıklamalar:';
+$_['text_payment_method']      = 'Ödeme Yöntemi';
+$_['text_payment_code']        = 'Ödeme Kodu';
+$_['text_footer']              = 'Herhangi bir sorunuz varsa lütfen bu e-postayı yanıtlayın.';
