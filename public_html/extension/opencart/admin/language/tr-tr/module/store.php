30bccfd4535df8876929d938b5f8ab18899ee423
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:31:50 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/module/store.php

--- public_html/extension/opencart/admin/language/tr-tr/module/store.php
+++ public_html/extension/opencart/admin/language/tr-tr/module/store.php
@@ -1,15 +1,15 @@
 <?php
 // Heading
-$_['heading_title']    = 'Store';
+$_['heading_title']    = 'Mağaza';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified store module!';
-$_['text_edit']        = 'Edit Store Module';
+$_['text_success']     = 'Başarılı: Mağaza modülünü değiştirdiniz!';
+$_['text_edit']        = 'Mağaza Modülünü Düzenle';
 
 // Entry
-$_['entry_admin']      = 'Admin Users Only';
+$_['entry_admin']      = 'Yalnızca Yönetici Kullanıcıları';
 $_['entry_status']     = 'Durum';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify store module!';
+$_['error_permission'] = 'Uyarı: Mağaza modülünü değiştirme yetkiniz yok!';
