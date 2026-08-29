7c73697f64ef993c37c9b5c92e29818aa91d2f61
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:42:47 +0300

    Update public_html/admin/language/tr-tr/mail/returns.php

--- public_html/admin/language/tr-tr/mail/returns.php
+++ public_html/admin/language/tr-tr/mail/returns.php
@@ -1,8 +1,8 @@
 <?php
 // Text
-$_['text_subject']       = '%s - Return Update %s';
-$_['text_return_id']     = 'Return ID:';
-$_['text_date_added']    = 'Return Date:';
-$_['text_return_status'] = 'Your return has been updated to the following status:';
-$_['text_comment']       = 'The comments for your return are:';
-$_['text_footer']        = 'Please reply to this email if you have any questions.';
+$_['text_subject']       = '%s - İade Güncellemesi %s';
+$_['text_return_id']     = 'İade No:';
+$_['text_date_added']    = 'İade Tarihi:';
+$_['text_return_status'] = 'İadeniz aşağıdaki duruma güncellendi:';
+$_['text_comment']       = 'İadenizle ilgili açıklamalar:';
+$_['text_footer']        = 'Herhangi bir sorunuz varsa lütfen bu e-postayı yanıtlayın.';
