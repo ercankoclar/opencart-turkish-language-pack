357e070ea0aa798ab17629ad11708057934068f9
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:50:16 +0300

    Update public_html/admin/language/tr-tr/marketplace/startup.php

--- public_html/admin/language/tr-tr/marketplace/startup.php
+++ public_html/admin/language/tr-tr/marketplace/startup.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title']     = 'Startup';
+$_['heading_title']     = 'Başlangıç';
 
 // Text
-$_['text_success']      = 'Success: You have modified startup!';
-$_['text_list']         = 'Startup List';
-$_['text_info']         = 'Startup Information';
+$_['text_success']      = 'Başarılı: Başlangıç ayarlarını değiştirdiniz!';
+$_['text_list']         = 'Başlangıç Listesi';
+$_['text_info']         = 'Başlangıç Bilgileri';
 
 // Column
-$_['column_code']       = 'Startup Code';
+$_['column_code']       = 'Başlangıç Kodu';
 $_['column_sort_order'] = 'Sıralama';
 $_['column_action']     = 'İşlem';
 
@@ -16,4 +16,4 @@ $_['column_action']     = 'İşlem';
 $_['entry_description'] = 'Açıklama';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify startup!';
+$_['error_permission']  = 'Uyarı: Başlangıç ayarlarını değiştirme yetkiniz yok!';
