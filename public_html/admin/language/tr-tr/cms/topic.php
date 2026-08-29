6e1367fac3def1f75f4ef09f5d863363ff6e4552
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:10:17 +0300

    Update public_html/admin/language/tr-tr/cms/topic.php

--- public_html/admin/language/tr-tr/cms/topic.php
+++ public_html/admin/language/tr-tr/cms/topic.php
@@ -1,40 +1,41 @@
 <?php
+
 // Heading
-$_['heading_title']           = 'Topics';
+$_['heading_title']           = 'Konular';
 
 // Text
-$_['text_success']            = 'Success: You have modified topics!';
-$_['text_list']               = 'Topic List';
-$_['text_add']                = 'Add Topic';
-$_['text_edit']               = 'Edit Topic';
+$_['text_success']            = 'Başarılı: Konuları değiştirdiniz!';
+$_['text_list']               = 'Konu Listesi';
+$_['text_add']                = 'Konu Ekle';
+$_['text_edit']               = 'Konuyu Düzenle';
 $_['text_default']            = 'Varsayılan';
-$_['text_general']            = 'General';
-$_['text_meta']               = 'Meta Tags';
-$_['text_keyword']            = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['text_general']            = 'Genel';
+$_['text_meta']               = 'Meta Etiketleri';
+$_['text_keyword']            = 'Boşluk kullanmayın, bunun yerine boşlukları - ile değiştirin ve SEO URL adresinin genel olarak benzersiz olduğundan emin olun.';
 
 // Column
-$_['column_name']             = 'Topic Name';
+$_['column_name']             = 'Konu Adı';
 $_['column_sort_order']       = 'Sıralama';
 $_['column_action']           = 'İşlem';
 
 // Entry
 $_['entry_image']             = 'Görsel';
-$_['entry_name']              = 'Topic Name';
+$_['entry_name']              = 'Konu Adı';
 $_['entry_description']       = 'Açıklama';
-$_['entry_meta_title']        = 'Meta Tag Title';
-$_['entry_meta_keyword']      = 'Meta Tag Keywords';
-$_['entry_meta_description']  = 'Meta Tag Description';
+$_['entry_meta_title']        = 'Meta Etiket Başlığı';
+$_['entry_meta_keyword']      = 'Meta Etiket Anahtar Kelimeleri';
+$_['entry_meta_description']  = 'Meta Etiket Açıklaması';
 $_['entry_store']             = 'Mağazalar';
 $_['entry_sort_order']        = 'Sıralama';
 $_['entry_status']            = 'Durum';
 $_['entry_keyword']           = 'Anahtar Kelime';
-$_['entry_layout']            = 'Layout Override';
+$_['entry_layout']            = 'Yerleşim Geçersiz Kılma';
 
 // Error
 $_['error_warning']           = 'Uyarı: Lütfen formu hatalar açısından dikkatlice kontrol edin!';
-$_['error_permission']        = 'Warning: You do not have permission to modify topics!';
-$_['error_name']              = 'Topic Name must be between 1 and 255 characters!';
-$_['error_meta_title']        = 'Meta Title must be greater than 1 and less than 255 characters!';
-$_['error_keyword']           = 'SEO URL must be between 1 and 64 characters!';
-$_['error_keyword_exists']    = 'SEO URL must be unique!';
-$_['error_keyword_character'] = 'Keyword can only use characters a-z, 0-9, - and _!';
+$_['error_permission']        = 'Uyarı: Konuları değiştirme yetkiniz yok!';
+$_['error_name']              = 'Konu Adı 1 ile 255 karakter arasında olmalıdır!';
+$_['error_meta_title']        = 'Meta Başlık 1 karakterden uzun ve 255 karakterden kısa olmalıdır!';
+$_['error_keyword']           = 'SEO URL 1 ile 64 karakter arasında olmalıdır!';
+$_['error_keyword_exists']    = 'SEO URL benzersiz olmalıdır!';
+$_['error_keyword_character'] = 'Anahtar Kelime yalnızca a-z, 0-9, - ve _ karakterlerini kullanabilir!';
