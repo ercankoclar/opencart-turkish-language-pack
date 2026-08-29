ceee0163e8c7dc7197c11f45e5ca48db01308c05
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:59:01 +0300

    Update public_html/admin/language/tr-tr/user/user_group.php

--- public_html/admin/language/tr-tr/user/user_group.php
+++ public_html/admin/language/tr-tr/user/user_group.php
@@ -5,21 +5,21 @@ $_['heading_title']    = 'Kullanıcı Grupları';
 // Text
 $_['text_success']     = 'Başarılı: Kullanıcı gruplarını değiştirdiniz!';
 $_['text_list']        = 'Kullanıcı Grubu';
-$_['text_add']         = 'Add User Group';
-$_['text_edit']        = 'Edit User Group';
+$_['text_add']         = 'Kullanıcı Grubu Ekle';
+$_['text_edit']        = 'Kullanıcı Grubunu Düzenle';
 $_['text_access']      = 'Erişim';
 $_['text_modify']      = 'Değiştir';
 
 // Column
-$_['column_name']      = 'User Group Name';
+$_['column_name']      = 'Kullanıcı Grubu Adı';
 $_['column_action']    = 'İşlem';
 
 // Entry
-$_['entry_name']       = 'User Group Name';
+$_['entry_name']       = 'Kullanıcı Grubu Adı';
 $_['entry_permission'] = 'Yetkiler';
 $_['entry_extension']  = 'Eklentiler';
 
 // Error
 $_['error_permission'] = 'Uyarı: Kullanıcı gruplarını değiştirme yetkiniz yok!';
 $_['error_name']       = 'Kullanıcı Grubu Adı 3 ile 64 karakter arasında olmalıdır!';
-$_['error_user']       = 'Warning: This user group cannot be deleted as it is currently assigned to %s users!';
+$_['error_user']       = 'Uyarı: Bu kullanıcı grubu %s kullanıcıya atanmış olduğu için silinemez!';
