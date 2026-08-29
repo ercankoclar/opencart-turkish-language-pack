613fc28cdeb619f8bb5a6bad3228967ed4410be9
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:33:06 +0300

    Update public_html/admin/language/tr-tr/localisation/return_reason.php

--- public_html/admin/language/tr-tr/localisation/return_reason.php
+++ public_html/admin/language/tr-tr/localisation/return_reason.php
@@ -3,19 +3,19 @@
 $_['heading_title']    = 'İade Nedenleri';
 
 // Text
-$_['text_success']     = 'Success: You have modified return reasons!';
-$_['text_list']        = 'Return Reason List';
-$_['text_add']         = 'Add Return Reason';
-$_['text_edit']        = 'Edit Return Reason';
+$_['text_success']     = 'Başarılı: İade nedenlerini değiştirdiniz!';
+$_['text_list']        = 'İade Nedeni Listesi';
+$_['text_add']         = 'İade Nedeni Ekle';
+$_['text_edit']        = 'İade Nedenini Düzenle';
 
 // Column
-$_['column_name']      = 'Return Reason Name';
+$_['column_name']      = 'İade Nedeni Adı';
 $_['column_action']    = 'İşlem';
 
 // Entry
-$_['entry_name']       = 'Return Reason Name';
+$_['entry_name']       = 'İade Nedeni Adı';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify return reasons!';
-$_['error_name']       = 'Return Reason Name must be between 3 and 128 characters!';
-$_['error_return']     = 'Warning: This return reason cannot be deleted as it is currently assigned to %s returned products!';
+$_['error_permission'] = 'Uyarı: İade nedenlerini değiştirme yetkiniz yok!';
+$_['error_name']       = 'İade Nedeni Adı 3 ile 128 karakter arasında olmalıdır!';
+$_['error_return']     = 'Uyarı: Bu iade nedeni %s iade edilmiş ürüne atanmış olduğu için silinemez!';
