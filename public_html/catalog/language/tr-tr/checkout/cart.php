63488d907ea2a2623d6831f19c4b445e0b8ddc37
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:04:46 +0300

    Update public_html/catalog/language/tr-tr/checkout/cart.php

--- public_html/catalog/language/tr-tr/checkout/cart.php
+++ public_html/catalog/language/tr-tr/checkout/cart.php
@@ -3,36 +3,36 @@
 $_['heading_title']              = 'Alışveriş Sepeti';
 
 // Text
-$_['text_success']               = 'Success: You have added <a href="%s">%s</a> to your <a href="%s">shopping cart</a>!';
+$_['text_success']               = 'Başarılı: <a href="%s">%s</a> <a href="%s">alışveriş sepetinize</a> eklendi!';
 $_['text_edit']                  = 'Başarılı: Alışveriş sepetiniz güncellendi!';
-$_['text_remove']                = 'Success: You have removed an item from your shopping cart!';
-$_['text_login']                 = 'Attention: You must <a href="%s">login</a> or <a href="%s">create an account</a> to view prices!';
+$_['text_remove']                = 'Başarılı: Alışveriş sepetinizden bir ürün kaldırıldı!';
+$_['text_login']                 = 'Dikkat: Fiyatları görüntülemek için <a href="%s">giriş yapmalı</a> veya <a href="%s">bir hesap oluşturmalısınız</a>!';
 $_['text_no_results']            = 'Alışveriş sepetiniz boş!';
 $_['text_model']                 = 'Model';
-$_['text_subscription']          = 'Subscription';
-$_['text_subscription_trial']    = '%s every %d %s(s) for %d payment(s) then ';
-$_['text_subscription_duration'] = '%s every %d %s(s) for %d payment(s)';
-$_['text_subscription_cancel']   = '%s every %d %s(s) until canceled';
-$_['text_day']                   = 'day';
-$_['text_week']                  = 'week';
-$_['text_semi_month']            = 'half-month';
-$_['text_month']                 = 'month';
-$_['text_year']                  = 'year';
+$_['text_subscription']          = 'Abonelik';
+$_['text_subscription_trial']    = '%s her %d %s için %d ödeme boyunca, ardından ';
+$_['text_subscription_duration'] = '%s her %d %s için %d ödeme boyunca';
+$_['text_subscription_cancel']   = '%s her %d %s için iptal edilene kadar';
+$_['text_day']                   = 'gün';
+$_['text_week']                  = 'hafta';
+$_['text_semi_month']            = 'yarım ay';
+$_['text_month']                 = 'ay';
+$_['text_year']                  = 'yıl';
 $_['text_points']                = 'Ödül Puanları';
-$_['text_next']                  = 'What would you like to do next?';
-$_['text_next_choice']           = 'Choose if you have a discount code or reward points you want to use or would like to estimate your delivery cost.';
+$_['text_next']                  = 'Sonra ne yapmak istersiniz?';
+$_['text_next_choice']           = 'İndirim kodunuz veya kullanmak istediğiniz ödül puanlarınız varsa ya da teslimat ücretinizi hesaplamak istiyorsanız seçim yapın.';
 
 // Column
-$_['column_image']               = 'Image';
+$_['column_image']               = 'Görsel';
 $_['column_name']                = 'Ürün';
 $_['column_quantity']            = 'Miktar';
-$_['column_price']               = 'Unit Price';
+$_['column_price']               = 'Birim Fiyat';
 $_['column_total']               = 'Toplam';
 
 // Error
-$_['error_stock']                = 'Products marked with *** are not available in the desired quantity or not in stock!';
-$_['error_minimum']              = 'Minimum order amount is %s!';
-$_['error_required']             = '%s required!';
-$_['error_regex']                = '%s is not a valid input!';
-$_['error_product']              = 'Warning: Product could not be found!';
-$_['error_subscription']         = 'Subscription Plan is not valid!';
+$_['error_stock']                = '*** ile işaretlenen ürünler istediğiniz miktarda mevcut değil veya stokta bulunmuyor!';
+$_['error_minimum']              = 'Minimum sipariş tutarı %s!';
+$_['error_required']             = '%s gereklidir!';
+$_['error_regex']                = '%s geçerli bir giriş değil!';
+$_['error_product']              = 'Uyarı: Ürün bulunamadı!';
+$_['error_subscription']         = 'Abonelik Planı geçerli değil!';
