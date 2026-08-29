0a51f7a32a94584f811cbd5963d87bb2a3175f03
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:23:18 +0300

    Update public_html/admin/language/tr-tr/extension/feed.php

--- public_html/admin/language/tr-tr/extension/feed.php
+++ public_html/admin/language/tr-tr/extension/feed.php
@@ -1,16 +1,16 @@
 <?php
 // Heading
-$_['heading_title']    = 'Feeds';
+$_['heading_title']    = 'Akışlar';
 
 // Text
-$_['text_success']     = 'Success: You have modified feeds!';
-$_['text_list']        = 'Feed List';
+$_['text_success']     = 'Başarılı: Akışları değiştirdiniz!';
+$_['text_list']        = 'Akış Listesi';
 
 // Column
-$_['column_name']      = 'Product Feed Name';
+$_['column_name']      = 'Ürün Akış Adı';
 $_['column_status']    = 'Durum';
 $_['column_action']    = 'İşlem';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify feeds!';
-$_['error_extension']  = 'Warning: Extension does not exist!';
+$_['error_permission'] = 'Uyarı: Akışları değiştirme yetkiniz yok!';
+$_['error_extension']  = 'Uyarı: Eklenti mevcut değil!';
