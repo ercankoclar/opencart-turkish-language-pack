c721ef529bc959f067de8d264d1cf4f137f8feb2
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:46:50 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/total/coupon.php

--- public_html/extension/opencart/admin/language/tr-tr/total/coupon.php
+++ public_html/extension/opencart/admin/language/tr-tr/total/coupon.php
@@ -4,8 +4,8 @@ $_['heading_title']    = 'Kupon';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified coupon total!';
-$_['text_edit']        = 'Edit Coupon Total';
+$_['text_success']     = 'Başarılı: Kupon toplamını değiştirdiniz!';
+$_['text_edit']        = 'Kupon Toplamını Düzenle';
 
 // Entry
 $_['entry_coupon']     = 'Kupon';
@@ -13,4 +13,4 @@ $_['entry_status']     = 'Durum';
 $_['entry_sort_order'] = 'Sıralama';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify coupon total!';
+$_['error_permission'] = 'Uyarı: Kupon toplamını değiştirme yetkiniz yok!';
