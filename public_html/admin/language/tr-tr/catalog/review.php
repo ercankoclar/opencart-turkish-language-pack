eb1c20bee63d18fe0d27df568e3c0e10d04793a3
ercankoclar <ercankoclar@duck.com>
2026-08-29 17:54:45 +0300

    Upload files to "public_html/admin/language/tr-tr/catalog"

---
+++ public_html/admin/language/tr-tr/catalog/review.php
@@ -0,0 +1,43 @@
+<?php
+// Heading
+$_['heading_title']     = 'Değerlendirmeler';
+
+// Text
+$_['text_success']      = 'Başarılı: Değerlendirmeleri değiştirdiniz!';
+$_['text_next']         = 'Başarılı: %s ile %s arasındaki %s ürün puanını değiştirdiniz!';
+$_['text_list']         = 'Değerlendirme Listesi';
+$_['text_add']          = 'Değerlendirme Ekle';
+$_['text_edit']         = 'Değerlendirmeyi Düzenle';
+$_['text_filter']       = 'Filtrele';
+
+// Column
+$_['column_product']    = 'Ürün';
+$_['column_author']     = 'Yazar';
+$_['column_rating']     = 'Puan';
+$_['column_status']     = 'Durum';
+$_['column_date_added'] = 'Eklenme Tarihi';
+$_['column_action']     = 'İşlem';
+
+// Entry
+$_['entry_product']     = 'Ürün';
+$_['entry_author']      = 'Yazar';
+$_['entry_rating']      = 'Puan';
+$_['entry_status']      = 'Durum';
+$_['entry_text']        = 'Metin';
+$_['entry_date_added']  = 'Eklenme Tarihi';
+$_['entry_date_from']   = 'Başlangıç Tarihi';
+$_['entry_date_to']     = 'Bitiş Tarihi';
+
+// Help
+$_['help_product']      = '(Otomatik Tamamlama)';
+
+// Button
+$_['button_rating']     = 'Ürün Puanlarını Senkronize Et';
+
+// Error
+$_['error_warning']     = 'Uyarı: Lütfen formu hatalar açısından dikkatlice kontrol edin!';
+$_['error_permission']  = 'Uyarı: Değerlendirmeleri değiştirme yetkiniz yok!';
+$_['error_product']     = 'Ürün gereklidir!';
+$_['error_author']      = 'Yazar adı 3 ile 64 karakter arasında olmalıdır!';
+$_['error_text']        = 'Değerlendirme metni en az 1 karakter olmalıdır!';
+$_['error_rating']      = 'Değerlendirme puanı gereklidir!';
---
+++ public_html/admin/language/tr-tr/catalog/subscription_plan.php
@@ -0,0 +1,48 @@
+<?php
+// Heading
+$_['heading_title']          = 'Abonelik Planları';
+
+// Text
+$_['text_success']           = 'Başarılı: Abonelik planlarını değiştirdiniz!';
+$_['text_list']              = 'Abonelik Planı Listesi';
+$_['text_add']               = 'Abonelik Planı Ekle';
+$_['text_edit']              = 'Abonelik Planını Düzenle';
+$_['text_subscription']      = 'Abonelik';
+$_['text_trial']             = 'Deneme';
+$_['text_day']               = 'Gün';
+$_['text_week']              = 'Hafta';
+$_['text_semi_month']        = 'Yarım Ay';
+$_['text_month']             = 'Ay';
+$_['text_year']              = 'Yıl';
+
+// Entry
+$_['entry_name']             = 'Abonelik Planı Adı';
+$_['entry_trial_duration']   = 'Deneme Süresi';
+$_['entry_trial_cycle']      = 'Deneme Döngüsü';
+$_['entry_trial_frequency']  = 'Deneme Sıklığı';
+$_['entry_trial_status']     = 'Deneme Durumu';
+$_['entry_duration']         = 'Süre';
+$_['entry_cycle']            = 'Döngü';
+$_['entry_frequency']        = 'Sıklık';
+$_['entry_status']           = 'Durum';
+$_['entry_sort_order']       = 'Sıralama';
+
+// Column
+$_['column_name']            = 'Abonelik Planı Adı';
+$_['column_sort_order']      = 'Sıralama';
+$_['column_action']          = 'İşlem';
+
+// Help
+$_['help_trial_duration']    = 'Süre, kullanıcının ödeme yapacağı toplam dönem sayısıdır.';
+$_['help_trial_cycle']       = 'Abonelik tutarları sıklık ve döngülere göre hesaplanır.';
+$_['help_trial_frequency']   = '"week" sıklığını ve "2" döngüsünü kullanırsanız kullanıcıdan her 2 haftada bir ücret alınır.';
+$_['help_duration']          = 'Süre, kullanıcının ödeme yapacağı toplam dönem sayısıdır; ödemelerin iptal edilene kadar devam etmesini istiyorsanız bunu 0 olarak ayarlayın.';
+$_['help_cycle']             = 'Abonelik tutarları sıklık ve döngülere göre hesaplanır.';
+$_['help_frequency']         = '"week" sıklığını ve "2" döngüsünü kullanırsanız kullanıcıdan her 2 haftada bir ücret alınır.';
+
+// Error
+$_['error_warning']          = 'Uyarı: Lütfen formu hatalar açısından dikkatlice kontrol edin!';
+$_['error_permission']       = 'Uyarı: Abonelik planlarını değiştirme yetkiniz yok!';
+$_['error_name']             = 'Abonelik planı adı 3 karakterden uzun ve 255 karakterden kısa olmalıdır!';
+$_['error_trial_duration']   = 'Deneme süresi 0\'dan büyük olmalıdır!';
+$_['error_product']          = 'Uyarı: Bu abonelik planı %s ürüne atandığı için silinemiyor!';
