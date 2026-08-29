08bfea5cde7613cf025d387c3e9366fd15282630
ercankoclar <ercankoclar@duck.com>
2026-08-29 17:58:12 +0300

    Upload files to "public_html/admin/language/tr-tr/localisation"

---
+++ public_html/admin/language/tr-tr/localisation/geo_zone.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']      = 'Coğrafi Bölgeler';
+
+// Text
+$_['text_success']       = 'Success: You have modified geo zones!';
+$_['text_list']          = 'Geo Zone List';
+$_['text_add']           = 'Add Geo Zone';
+$_['text_edit']          = 'Edit Geo Zone';
+$_['text_geo_zone']      = 'Coğrafi Bölgeler';
+
+// Column
+$_['column_name']        = 'Geo Zone Name';
+$_['column_description'] = 'Açıklama';
+$_['column_action']      = 'İşlem';
+
+// Entry
+$_['entry_name']         = 'Geo Zone Name';
+$_['entry_description']  = 'Açıklama';
+$_['entry_country']      = 'Ülke';
+$_['entry_zone']         = 'Bölge';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify geo zones!';
+$_['error_name']         = 'Geo Zone Name must be between 3 and 32 characters!';
+$_['error_description']  = 'Description Name must be between 3 and 255 characters!';
+$_['error_tax_rate']     = 'Warning: This geo zone cannot be deleted as it is currently assigned to one or more tax rates!';
