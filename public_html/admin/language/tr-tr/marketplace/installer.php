7378b3a5dd65d26817211112fae36672c5230611
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:48:40 +0300

    Update public_html/admin/language/tr-tr/marketplace/installer.php

--- public_html/admin/language/tr-tr/marketplace/installer.php
+++ public_html/admin/language/tr-tr/marketplace/installer.php
@@ -3,53 +3,53 @@
 $_['heading_title']          = 'Eklenti Yükleyici';
 
 // Text
-$_['text_upload']            = 'Success: The extension has been uploaded!';
-$_['text_success']           = 'Success: You have modified extensions!';
-$_['text_progress']          = 'Install Progress';
-$_['text_installed']         = 'Installed Extensions';
-$_['text_info']              = 'Extension Information';
-$_['text_install']           = 'Installing files %s to %s of %s';
+$_['text_upload']            = 'Başarılı: Eklenti yüklendi!';
+$_['text_success']           = 'Başarılı: Eklentileri değiştirdiniz!';
+$_['text_progress']          = 'Yükleme İlerlemesi';
+$_['text_installed']         = 'Yüklü Eklentiler';
+$_['text_info']              = 'Eklenti Bilgileri';
+$_['text_install']           = '%s - %s arasındaki %s dosyası yükleniyor';
 
 // Column
-$_['column_image']           = 'Image';
+$_['column_image']           = 'Görsel';
 $_['column_name']            = 'Eklenti Adı';
 $_['column_version']         = 'Sürüm';
 $_['column_date_added']      = 'Eklenme Tarihi';
 $_['column_action']          = 'İşlem';
 
 // Entry
-$_['entry_progress']         = 'Progress';
+$_['entry_progress']         = 'İlerleme';
 $_['entry_name']             = 'Eklenti Adı';
 $_['entry_description']      = 'Açıklama';
 $_['entry_code']             = 'Kod';
 
 // Error
-$_['error_permission']       = 'Warning: You do not have permission to modify extensions!';
-$_['error_install']          = 'Warning: Could not find install.json!';
-$_['error_default']          = 'Default extension could not be uninstalled or deleted!';
-$_['error_extension']        = 'Installed extension could not be found!';
-$_['error_installed']        = 'Extension already installed!';
-$_['error_uninstall']        = 'There are %s extensions that need to be uninstalled before this extension can be safely removed!';
-$_['error_name']             = 'Name must be between 3 and 128 characters!';
-$_['error_version']          = 'Version must be between 3 and 128 characters!';
-$_['error_author']           = 'Author must be between 3 and 128 characters!';
-$_['error_filename']         = 'Filename must be between 3 and 128 characters!';
-$_['error_file']             = 'Install file %s could not be found!';
-$_['error_file_exists']      = 'File already exist!';
-$_['error_file_type']        = 'Invalid file type!';
-$_['error_directory']        = 'Install directory %s could not be found!';
-$_['error_directory_exists'] = 'Path %s already exists!';
-$_['error_unzip']            = 'Zip file could not be opened!';
-$_['error_upload']           = 'File could not be uploaded!';
-$_['error_unknown']          = 'An unknown error occurred!';
+$_['error_permission']       = 'Uyarı: Eklentileri değiştirme yetkiniz yok!';
+$_['error_install']          = 'Uyarı: install.json dosyası bulunamadı!';
+$_['error_default']          = 'Varsayılan eklenti kaldırılamaz veya silinemez!';
+$_['error_extension']        = 'Yüklü eklenti bulunamadı!';
+$_['error_installed']        = 'Eklenti zaten yüklü!';
+$_['error_uninstall']        = 'Bu eklentinin güvenli bir şekilde kaldırılabilmesi için önce kaldırılması gereken %s eklenti var!';
+$_['error_name']             = 'Ad 3 ile 128 karakter arasında olmalıdır!';
+$_['error_version']          = 'Sürüm 3 ile 128 karakter arasında olmalıdır!';
+$_['error_author']           = 'Yazar 3 ile 128 karakter arasında olmalıdır!';
+$_['error_filename']         = 'Dosya adı 3 ile 128 karakter arasında olmalıdır!';
+$_['error_file']             = '%s yükleme dosyası bulunamadı!';
+$_['error_file_exists']      = 'Dosya zaten mevcut!';
+$_['error_file_type']        = 'Geçersiz dosya türü!';
+$_['error_directory']        = '%s yükleme dizini bulunamadı!';
+$_['error_directory_exists'] = '%s yolu zaten mevcut!';
+$_['error_unzip']             = 'ZIP dosyası açılamadı!';
+$_['error_upload']           = 'Dosya yüklenemedi!';
+$_['error_unknown']          = 'Bilinmeyen bir hata oluştu!';
 
 // Zip errors
-$_['zip_error_exists']       = 'File already exists!';
-$_['zip_error_incons']       = 'Zip archive inconsistent!';
-$_['zip_error_inval']        = 'Invalid argument!';
-$_['zip_error_memory']       = 'Memory allocation failure!';
-$_['zip_error_noent']        = 'No such file!';
-$_['zip_error_nozip']        = 'Not a zip archive!';
-$_['zip_error_open']         = 'Can not open file!';
-$_['zip_error_read']         = 'Read error!';
-$_['zip_error_seek']         = 'Seek error!';
+$_['zip_error_exists']       = 'Dosya zaten mevcut!';
+$_['zip_error_incons']       = 'ZIP arşivi tutarsız!';
+$_['zip_error_inval']        = 'Geçersiz bağımsız değişken!';
+$_['zip_error_memory']       = 'Bellek ayırma hatası!';
+$_['zip_error_noent']        = 'Böyle bir dosya yok!';
+$_['zip_error_nozip']        = 'ZIP arşivi değil!';
+$_['zip_error_open']         = 'Dosya açılamıyor!';
+$_['zip_error_read']         = 'Okuma hatası!';
+$_['zip_error_seek']         = 'Konumlandırma hatası!';
