45ea302f52d7ad4c7a09900497287834aa53d75a
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:45:59 +0300

    Update public_html/admin/language/tr-tr/marketing/marketing.php

--- public_html/admin/language/tr-tr/marketing/marketing.php
+++ public_html/admin/language/tr-tr/marketing/marketing.php
@@ -3,41 +3,41 @@
 $_['heading_title']     = 'Pazarlama Takibi';
 
 // Text
-$_['text_success']      = 'Success: You have modified marketing tracking!';
-$_['text_list']         = 'Marketing Tracking List';
-$_['text_add']          = 'Add Marketing Tracking';
-$_['text_edit']         = 'Edit Marketing Tracking';
+$_['text_success']      = 'Başarılı: Pazarlama takibini değiştirdiniz!';
+$_['text_list']         = 'Pazarlama Takibi Listesi';
+$_['text_add']          = 'Pazarlama Takibi Ekle';
+$_['text_edit']         = 'Pazarlama Takibini Düzenle';
 $_['text_filter']       = 'Filtrele';
-$_['text_history']      = 'History';
-$_['text_history_add']  = 'Add History';
-$_['text_report']       = 'Reports';
+$_['text_history']      = 'Geçmiş';
+$_['text_history_add']  = 'Geçmiş Ekle';
+$_['text_report']       = 'Raporlar';
 
 // Column
-$_['column_name']       = 'Campaign Name';
+$_['column_name']       = 'Kampanya Adı';
 $_['column_code']       = 'Kod';
-$_['column_clicks']     = 'Clicks';
+$_['column_clicks']     = 'Tıklamalar';
 $_['column_orders']     = 'Siparişler';
 $_['column_ip']         = 'IP';
-$_['column_account']    = 'Accounts';
+$_['column_account']    = 'Hesaplar';
 $_['column_store']      = 'Mağaza';
-$_['column_country']    = 'Country';
+$_['column_country']    = 'Ülke';
 $_['column_date_added'] = 'Eklenme Tarihi';
 $_['column_action']     = 'İşlem';
 
 // Entry
-$_['entry_name']        = 'Campaign Name';
-$_['entry_description'] = 'Campaign Description';
+$_['entry_name']        = 'Kampanya Adı';
+$_['entry_description'] = 'Kampanya Açıklaması';
 $_['entry_code']        = 'Takip Kodu';
-$_['entry_example']     = 'Examples';
-$_['entry_date_from']   = 'Date From';
-$_['entry_date_to']     = 'Date To';
+$_['entry_example']     = 'Örnekler';
+$_['entry_date_from']   = 'Başlangıç Tarihi';
+$_['entry_date_to']     = 'Bitiş Tarihi';
 
 // Help
-$_['help_code']         = 'The tracking code that will be used to track marketing campaigns.';
-$_['help_example']      = 'So the system can track referrals you need to add the tracking code to the end of the URL linking to your site.';
+$_['help_code']         = 'Pazarlama kampanyalarını takip etmek için kullanılacak takip kodu.';
+$_['help_example']      = 'Yönlendirmeleri takip edebilmek için takip kodunu sitenize yönlendiren URL adresinin sonuna eklemeniz gerekir.';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify marketing tracking!';
-$_['error_name']        = 'Campaign must be between 1 and 32 characters!';
-$_['error_code']        = 'Tracking Code required!';
-$_['error_exists']      = 'Tracking code is being used by another campaign!';
+$_['error_permission']  = 'Uyarı: Pazarlama takibini değiştirme yetkiniz yok!';
+$_['error_name']        = 'Kampanya 1 ile 32 karakter arasında olmalıdır!';
+$_['error_code']        = 'Takip Kodu gereklidir!';
+$_['error_exists']      = 'Takip kodu başka bir kampanya tarafından kullanılıyor!';
