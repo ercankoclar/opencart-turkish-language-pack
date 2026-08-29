3ea8d6d34ed20d3eef66cd328095622c485cf37f
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:46:24 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/theme/basic.php

--- public_html/extension/opencart/admin/language/tr-tr/theme/basic.php
+++ public_html/extension/opencart/admin/language/tr-tr/theme/basic.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title']    = 'Default Store Theme';
+$_['heading_title']    = 'Varsayılan Mağaza Teması';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified the default store theme!';
-$_['text_edit']        = 'Edit Default Store Theme';
+$_['text_success']     = 'Başarılı: Varsayılan mağaza temasını değiştirdiniz!';
+$_['text_edit']        = 'Varsayılan Mağaza Temasını Düzenle';
 
 // Entry
 $_['entry_status']     = 'Durum';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify the default store theme!';
+$_['error_permission'] = 'Uyarı: Varsayılan mağaza temasını değiştirme yetkiniz yok!';
