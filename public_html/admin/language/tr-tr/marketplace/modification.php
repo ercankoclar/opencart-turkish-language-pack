715326ac69c749fc7051399d071252e7f4bd8089
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:49:29 +0300

    Update public_html/admin/language/tr-tr/marketplace/modification.php

--- public_html/admin/language/tr-tr/marketplace/modification.php
+++ public_html/admin/language/tr-tr/marketplace/modification.php
@@ -1,15 +1,15 @@
 <?php
 // Heading
-$_['heading_title']     = 'Modifications';
+$_['heading_title']     = 'Değişiklikler';
 
 // Text
-$_['text_success']      = 'Success: You have modified modifications!';
-$_['text_refresh']      = 'Whenever you enable / disable or delete a modification you need to click the refresh button to rebuild your modification cache!';
-$_['text_list']         = 'Modification List';
-$_['text_info']         = 'Modification Information';
+$_['text_success']      = 'Başarılı: Değişiklikleri değiştirdiniz!';
+$_['text_refresh']      = 'Bir değişikliği etkinleştirdiğinizde, devre dışı bıraktığınızda veya sildiğinizde değişiklik önbelleğini yeniden oluşturmak için yenile düğmesine tıklamanız gerekir!';
+$_['text_list']         = 'Değişiklik Listesi';
+$_['text_info']         = 'Değişiklik Bilgileri';
 
 // Column
-$_['column_name']       = 'Modification Name';
+$_['column_name']       = 'Değişiklik Adı';
 $_['column_author']     = 'Geliştirici';
 $_['column_version']    = 'Sürüm';
 $_['column_status']     = 'Durum';
@@ -17,10 +17,10 @@ $_['column_date_added'] = 'Eklenme Tarihi';
 $_['column_action']     = 'İşlem';
 
 // Entry
-$_['entry_name']        = 'Modification Name';
+$_['entry_name']        = 'Değişiklik Adı';
 $_['entry_description'] = 'Açıklama';
 $_['entry_code']        = 'Kod';
 $_['entry_xml']         = 'XML';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify modifications!';
+$_['error_permission']  = 'Uyarı: Değişiklikleri değiştirme yetkiniz yok!';
