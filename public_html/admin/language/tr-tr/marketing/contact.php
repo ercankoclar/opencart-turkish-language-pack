8ffdc3d6b1bf27e3f57bf02332eb1968722d3fd7
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:45:08 +0300

    Update public_html/admin/language/tr-tr/marketing/contact.php

--- public_html/admin/language/tr-tr/marketing/contact.php
+++ public_html/admin/language/tr-tr/marketing/contact.php
@@ -3,35 +3,35 @@
 $_['heading_title']        = 'Mail';
 
 // Text
-$_['text_mail']            = 'Send e-mail\'s to customers';
-$_['text_success']         = 'Your message has been successfully sent!';
-$_['text_sent']            = 'Your message has been successfully sent to %s to %s of %s recipients!';
-$_['text_default']         = 'Default';
-$_['text_newsletter']      = 'All Newsletter Subscribers';
-$_['text_customer_all']    = 'All Customers';
+$_['text_mail']            = 'Müşterilere e-posta gönder';
+$_['text_success']         = 'Mesajınız başarıyla gönderildi!';
+$_['text_sent']            = 'Mesajınız %s alıcıdan %s ile %s arasındaki alıcılara başarıyla gönderildi!';
+$_['text_default']         = 'Varsayılan';
+$_['text_newsletter']      = 'Tüm Bülten Aboneleri';
+$_['text_customer_all']    = 'Tüm Müşteriler';
 $_['text_customer_group']  = 'Müşteri Grubu';
 $_['text_customer']        = 'Müşteriler';
-$_['text_affiliate_all']   = 'All Affiliates';
+$_['text_affiliate_all']   = 'Tüm İş Ortakları';
 $_['text_affiliate']       = 'İş Ortakları';
 $_['text_product']         = 'Ürünler';
 
 // Entry
-$_['entry_store']          = 'From';
-$_['entry_to']             = 'To';
+$_['entry_store']          = 'Gönderen';
+$_['entry_to']             = 'Alıcı';
 $_['entry_customer_group'] = 'Müşteri Grubu';
 $_['entry_customer']       = 'Müşteri';
 $_['entry_affiliate']      = 'İş Ortağı';
 $_['entry_product']        = 'Ürünler';
-$_['entry_subject']        = 'Subject';
-$_['entry_message']        = 'Message';
+$_['entry_subject']        = 'Konu';
+$_['entry_message']        = 'Mesaj';
 
 // Help
-$_['help_customer']        = '(Autocomplete)';
-$_['help_affiliate']       = '(Autocomplete)';
-$_['help_product']         = 'Send only to customers who have ordered products in the list. (Autocomplete)';
+$_['help_customer']        = '(Otomatik Tamamlama)';
+$_['help_affiliate']       = '(Otomatik Tamamlama)';
+$_['help_product']         = 'Yalnızca listede yer alan ürünleri sipariş etmiş müşterilere gönderin. (Otomatik Tamamlama)';
 
 // Error
-$_['error_permission']     = 'Warning: You do not have permission to send E-Mails!';
-$_['error_subject']        = 'E-Mail Subject required!';
-$_['error_message']        = 'E-Mail Message required!';
-$_['error_email']          = 'No E-Mail recipients!';
+$_['error_permission']     = 'Uyarı: E-posta gönderme yetkiniz yok!';
+$_['error_subject']        = 'E-posta konusu gereklidir!';
+$_['error_message']        = 'E-posta mesajı gereklidir!';
+$_['error_email']          = 'E-posta alıcısı bulunamadı!';
