1de3d7db3922e72b45f963a0a87833e462a2c391
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:18:23 +0300

    Update public_html/catalog/language/tr-tr/mail/register.php

--- public_html/catalog/language/tr-tr/mail/register.php
+++ public_html/catalog/language/tr-tr/mail/register.php
@@ -1,18 +1,18 @@
 <?php
 // Text
-$_['text_subject']        = '%s - Thank you for registering';
-$_['text_welcome']        = 'Welcome and thank you for registering at %s!';
-$_['text_login']          = 'Your account has now been created and you can log in by using your email address and password by visiting our website or at the following URL:';
-$_['text_approval']       = 'Your account must be approved before you can login. Once approved you can log in by using your email address and password by visiting our website or at the following URL:';
-$_['text_service']        = 'Upon logging in, you will be able to access other services including reviewing past orders, printing invoices and editing your account information.';
-$_['text_thanks']         = 'Thanks,';
-$_['text_new_customer']   = 'New customer';
-$_['text_signup']         = 'A new customer has signed up:';
-$_['text_customer_group'] = 'Customer Group:';
-$_['text_firstname']      = 'First Name:';
-$_['text_lastname']       = 'Last Name:';
-$_['text_email']          = 'E-Mail:';
-$_['text_telephone']      = 'Telephone:';
+$_['text_subject']        = '%s - Kayıt olduğunuz için teşekkür ederiz';
+$_['text_welcome']        = '%s sitesine kayıt olduğunuz için hoş geldiniz ve teşekkür ederiz!';
+$_['text_login']          = 'Hesabınız oluşturuldu. E-posta adresinizi ve şifrenizi kullanarak web sitemizi ziyaret ederek veya aşağıdaki URL üzerinden giriş yapabilirsiniz:';
+$_['text_approval']       = 'Giriş yapabilmeniz için hesabınızın onaylanması gerekiyor. Onaylandıktan sonra e-posta adresinizi ve şifrenizi kullanarak web sitemizi ziyaret ederek veya aşağıdaki URL üzerinden giriş yapabilirsiniz:';
+$_['text_service']        = 'Giriş yaptıktan sonra geçmiş siparişlerinizi görüntüleme, faturalarınızı yazdırma ve hesap bilgilerinizi düzenleme gibi diğer hizmetlere erişebilirsiniz.';
+$_['text_thanks']         = 'Teşekkürler,';
+$_['text_new_customer']   = 'Yeni müşteri';
+$_['text_signup']         = 'Yeni bir müşteri kayıt oldu:';
+$_['text_customer_group'] = 'Müşteri Grubu:';
+$_['text_firstname']      = 'Ad:';
+$_['text_lastname']       = 'Soyad:';
+$_['text_email']          = 'E-posta:';
+$_['text_telephone']      = 'Telefon:';
 
 // Button
-$_['button_login']        = 'Login';
+$_['button_login']        = 'Giriş Yap';
