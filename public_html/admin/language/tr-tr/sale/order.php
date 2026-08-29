285fa045214d01a26bbbe3c7d2e9a1e50087849b
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:01:01 +0300

    Upload files to "public_html/admin/language/tr-tr/sale"

---
+++ public_html/admin/language/tr-tr/sale/order.php
@@ -0,0 +1,134 @@
+<?php
+// Heading
+$_['heading_title']              = 'Siparişler';
+
+// Text
+$_['text_success']               = 'Success: You have modified orders!';
+$_['text_list']                  = 'Order List';
+$_['text_add']                   = 'Add Order';
+$_['text_edit']                  = 'Order (#%s)';
+$_['text_filter']                = 'Filtrele';
+$_['text_store']                 = 'Mağaza';
+$_['text_date_added']            = 'Eklenme Tarihi';
+$_['text_customer']              = 'Müşteri';
+$_['text_product_add']           = 'Add Product';
+$_['text_model'] 			     = 'Model';
+$_['text_reward']                = 'Ödül Puanları';
+$_['text_points']                = 'Points';
+$_['text_reward_add']            = 'Success: Reward points added!';
+$_['text_reward_remove']         = 'Success: Reward points removed!';
+$_['text_affiliate']             = 'Affiliate';
+$_['text_commission']            = 'Commission';
+$_['text_commission_add']        = 'Success: Commission added!';
+$_['text_commission_remove']     = 'Success: Commission removed!';
+$_['text_upload']                = 'Your file was successfully uploaded!';
+$_['text_subscription']          = 'Subscription';
+$_['text_subscription_trial']    = '%s every %d %s(s) for %d payment(s) then ';
+$_['text_subscription_duration'] = '%s every %d %s(s) for %d payment(s)';
+$_['text_subscription_cancel']   = '%s every %d %s(s) until canceled';
+$_['text_day']                   = 'day';
+$_['text_week']                  = 'week';
+$_['text_semi_month']            = 'half-month';
+$_['text_month']                 = 'month';
+$_['text_year']                  = 'year';
+$_['text_more']                  = 'More..';
+$_['text_less']                  = 'Less..';
+$_['text_payment_address']       = 'Payment Address';
+$_['text_payment_method']        = 'Payment Method';
+$_['text_payment']               = 'Please select the preferred payment method to use for this order.';
+$_['text_shipping_address']      = 'Shipping Address';
+$_['text_shipping_method']       = 'Shipping Method';
+$_['text_shipping']              = 'Please select the preferred shipping method to use for this order.';
+$_['text_comment']               = 'Comment';
+$_['text_history']               = 'History';
+$_['text_history_add']           = 'Add History';
+$_['text_browser']               = 'Browser';
+$_['text_ip']                    = 'IP Address';
+$_['text_forwarded_ip']          = 'Forwarded IP';
+$_['text_user_agent']            = 'User Agent';
+$_['text_accept_language']       = 'Accept Language';
+$_['text_order_id']              = 'Order ID';
+$_['text_website']               = 'Web Site';
+$_['text_invoice']               = 'Fatura';
+$_['text_tbc']                   = 'TBC';
+$_['text_store_address']         = 'Store Address';
+$_['text_store_telephone']       = 'Store Telephone';
+$_['text_store_email']           = 'Store E-Mail';
+$_['text_customer_email']        = 'Customer E-Mail';
+$_['text_customer_telephone']    = 'Customer Telephone';
+$_['text_missing']               = 'Missing Orders';
+$_['text_default']               = 'Default';
+$_['text_picklist']              = 'Dispatch Note';
+$_['text_sku']                   = 'SKU';
+$_['text_upc']                   = 'UPC';
+$_['text_ean']                   = 'EAN';
+$_['text_jan']                   = 'JAN';
+$_['text_isbn']                  = 'ISBN';
+$_['text_mpn']                   = 'MPN';
+
+// Column
+$_['column_order_id']            = 'Order ID';
+$_['column_customer']            = 'Müşteri';
+$_['column_store']               = 'Mağaza';
+$_['column_status']              = 'Durum';
+$_['column_date_added']          = 'Eklenme Tarihi';
+$_['column_date_modified']       = 'Değiştirilme Tarihi';
+$_['column_total']               = 'Toplam';
+$_['column_product']             = 'Ürün';
+$_['column_model']               = 'Model';
+$_['column_quantity']            = 'Miktar';
+$_['column_price']               = 'Unit Price';
+$_['column_comment']             = 'Comment';
+$_['column_notify']              = 'Customer Notified';
+$_['column_location']            = 'Location';
+$_['column_reference']           = 'Reference';
+$_['column_weight']              = 'Product Weight';
+$_['column_action']              = 'İşlem';
+
+// Entry
+$_['entry_store']                = 'Mağaza';
+$_['entry_customer']             = 'Müşteri';
+$_['entry_customer_group']       = 'Müşteri Grubu';
+$_['entry_firstname']            = 'First Name';
+$_['entry_lastname']             = 'Last Name';
+$_['entry_email']                = 'E-Mail';
+$_['entry_telephone']            = 'Telephone';
+$_['entry_address']              = 'Choose Address';
+$_['entry_company']              = 'Company';
+$_['entry_address_1']            = 'Address 1';
+$_['entry_address_2']            = 'Address 2';
+$_['entry_city']                 = 'City';
+$_['entry_postcode']             = 'Postcode';
+$_['entry_country']              = 'Ülke';
+$_['entry_zone']                 = 'Region / State';
+$_['entry_product']              = 'Choose Product';
+$_['entry_option']               = 'Choose Option(s)';
+$_['entry_subscription']         = 'Choose Subscription';
+$_['entry_quantity']             = 'Miktar';
+$_['entry_order_status']         = 'Order Status';
+$_['entry_notify']               = 'Notify Customer';
+$_['entry_payment_method']       = 'Payment Method';
+$_['entry_override']             = 'Override';
+$_['entry_comment']              = 'Comment';
+$_['entry_language']             = 'Dil';
+$_['entry_currency']             = 'Para Birimi';
+$_['entry_affiliate']            = 'Affiliate';
+$_['entry_order_id']             = 'Order ID';
+$_['entry_total']                = 'Toplam';
+$_['entry_date_from']            = 'Date From';
+$_['entry_date_to']              = 'Date To';
+$_['entry_date_modified_from']   = 'Modified Date From';
+$_['entry_date_modified_to']     = 'Modified Date To';
+
+// Help
+$_['help_override']              = 'If the customers order is being blocked from changing the order status due to an anti-fraud extension enable override.';
+
+// Error
+$_['error_warning']              = 'Uyarı: Lütfen formu hatalar açısından dikkatlice kontrol edin!';
+$_['error_permission']           = 'Warning: You do not have permission to modify orders!';
+$_['error_invoice_no']           = 'Warning: Invoice No. has already been created!';
+$_['error_order']                = 'Warning: Order does not exist!';
+$_['error_affiliate']            = 'Warning: Affiliate does not exist!';
+$_['error_reward_add']           = 'Warning: Reward points for this order has already been added!';
+$_['error_reward_guest']         = 'Warning: Guests can not use reward points!';
+$_['error_commission_add']       = 'Warning: Affiliate commission for this order has already been added!';
