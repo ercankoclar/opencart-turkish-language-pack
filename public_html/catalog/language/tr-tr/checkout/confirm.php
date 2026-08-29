81bf53a653a69c071f6a6658bbc536ab9e55b90e
ercankoclar <ercankoclar@duck.com>
2026-08-29 15:35:07 +0300

    Upload files to "public_html/catalog/language/tr-tr/checkout"

---
+++ public_html/catalog/language/tr-tr/checkout/confirm.php
@@ -0,0 +1,14 @@
+<?php
+// Text
+$_['text_model']                 = 'Model';
+$_['text_subscription']          = 'Subscription';
+$_['text_subscription_trial']    = '%s every %d %s(s) for %d payment(s) then ';
+$_['text_subscription_duration'] = '%s every %d %s(s) for %d payment(s)';
+$_['text_subscription_cancel']   = '%s every %d %s(s) until canceled';
+$_['text_points']                = 'Ödül Puanları';
+
+// Column
+$_['column_product']    = 'Ürün';
+$_['column_quantity']   = 'Miktar';
+$_['column_price']      = 'Unit Price';
+$_['column_total']      = 'Toplam';
