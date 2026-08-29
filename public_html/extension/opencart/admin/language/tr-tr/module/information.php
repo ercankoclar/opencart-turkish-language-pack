8cffc6b90d8647763a418e17dfa801b0d6584fea
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:30:35 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/module/information.php

--- public_html/extension/opencart/admin/language/tr-tr/module/information.php
+++ public_html/extension/opencart/admin/language/tr-tr/module/information.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title']    = 'Information';
+$_['heading_title']    = 'Bilgi';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified information module!';
-$_['text_edit']        = 'Edit Information Module';
+$_['text_success']     = 'Başarılı: Bilgi modülünü değiştirdiniz!';
+$_['text_edit']        = 'Bilgi Modülünü Düzenle';
 
 // Entry
 $_['entry_status']     = 'Durum';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify information module!';
+$_['error_permission'] = 'Uyarı: Bilgi modülünü değiştirme yetkiniz yok!';
