b9e74822c12c6e63a4dddc2c3c38af0859a7065f
ercankoclar <ercankoclar@duck.com>
2026-08-29 16:09:02 +0300

    Upload files to "public_html/extension/opencart/admin/language/tr-tr/captcha"

---
+++ public_html/extension/opencart/admin/language/tr-tr/captcha/basic.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'Temel CAPTCHA';
+
+// Text
+$_['text_extension']   = 'Eklentiler';
+$_['text_success']     = 'Başarılı: Temel CAPTCHA ayarlarını değiştirdiniz!';
+$_['text_edit']        = 'Temel CAPTCHA\'yı Düzenle';
+
+// Entry
+$_['entry_status']     = 'Durum';
+
+// Error
+$_['error_permission'] = 'Uyarı: Temel CAPTCHA\'yı değiştirme yetkiniz yok!';
