2cd223e8cce55e64620616a153401588f852c3a6
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:13:43 +0300

    Update public_html/catalog/language/tr-tr/information/gdpr.php

--- public_html/catalog/language/tr-tr/information/gdpr.php
+++ public_html/catalog/language/tr-tr/information/gdpr.php
@@ -1,25 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'General Data Protection Regulation Request';
+$_['heading_title']     = 'Genel Veri Koruma Yönetmeliği (GDPR) Talebi';
 
 // Text
-$_['text_account']      = 'Account';
-$_['text_gdpr']         = 'You can view %s GDPR policy on the <a href="%s" target="_blank">%s</a> page.';
-$_['text_verification'] = 'Account Verification';
-$_['text_email']        = 'Before we can carry out any GDPR requests we must validate you account. Please enter your E-Mail Address below.';
-$_['text_action']       = 'Choose an action';
-$_['text_export']       = 'Export Personal Data';
-$_['text_remove']       = 'Remove Personal Data';
-$_['text_warning']      = 'Warning: You will lose access to your account!';
-$_['text_access']       = 'You will no longer have access to your %s account.';
-$_['text_history']      = 'You will no longer have access to your order history, invoices, wishlists or downloads.';
-$_['text_limit']        = 'Account deletion requests will be processed after <strong>%s days</strong> so any fraud detection, chargebacks or refunds can be processed.';
-$_['text_success']      = 'Success: An email has been sent to your email address!';
-$_['text_cancel']		= 'Cancel';
+$_['text_account']      = 'Hesap';
+$_['text_gdpr']         = '%s GDPR politikasını <a href="%s" target="_blank">%s</a> sayfasından görüntüleyebilirsiniz.';
+$_['text_verification'] = 'Hesap Doğrulama';
+$_['text_email']        = 'Herhangi bir GDPR talebini gerçekleştirebilmemiz için hesabınızı doğrulamamız gerekir. Lütfen aşağıya E-posta Adresinizi girin.';
+$_['text_action']       = 'Bir işlem seçin';
+$_['text_export']       = 'Kişisel Verileri Dışa Aktar';
+$_['text_remove']       = 'Kişisel Verileri Sil';
+$_['text_warning']      = 'Uyarı: Hesabınıza erişiminizi kaybedeceksiniz!';
+$_['text_access']       = '%s hesabınıza artık erişemeyeceksiniz.';
+$_['text_history']      = 'Sipariş geçmişinize, faturalarınıza, istek listenize veya indirmelerinize artık erişemeyeceksiniz.';
+$_['text_limit']        = 'Hesap silme talepleri, dolandırıcılık tespiti, ters ibraz veya geri ödeme işlemlerinin gerçekleştirilebilmesi için <strong>%s gün</strong> sonra işleme alınacaktır.';
+$_['text_success']      = 'Başarılı: E-posta adresinize bir e-posta gönderildi!';
+$_['text_cancel']       = 'İptal';
 
 // Entry
-$_['entry_email']       = 'E-Mail';
+$_['entry_email']       = 'E-posta';
 
 // Error
-$_['error_email']       = 'E-Mail Address does not appear to be valid!';
-$_['error_action']      = 'You must select a valid GDPR action!';
+$_['error_email']       = 'E-posta Adresi geçerli görünmüyor!';
+$_['error_action']      = 'Geçerli bir GDPR işlemi seçmelisiniz!';
