69bd37056c1bcd9f99ed085384ee520e7075a9c0
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:15:30 +0300

    Update public_html/catalog/language/tr-tr/mail/authorize.php

--- public_html/catalog/language/tr-tr/mail/authorize.php
+++ public_html/catalog/language/tr-tr/mail/authorize.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_subject'] = 'Security';
-$_['text_code']    = 'Please enter the following security code to sign in to your account:';
+$_['text_subject'] = 'Güvenlik';
+$_['text_code']    = 'Hesabınıza giriş yapmak için lütfen aşağıdaki güvenlik kodunu girin:';
 $_['text_ip']      = 'IP:';
-$_['text_regards'] = 'Best Regards';
+$_['text_regards'] = 'Saygılarımla';
