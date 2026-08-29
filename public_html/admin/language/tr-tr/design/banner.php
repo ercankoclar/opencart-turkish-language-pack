28175ca4c3978012ab0b88d76f98fc5344398dac
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:18:08 +0300

    Update public_html/admin/language/tr-tr/design/banner.php

--- public_html/admin/language/tr-tr/design/banner.php
+++ public_html/admin/language/tr-tr/design/banner.php
@@ -1,28 +1,28 @@
 <?php
 // Heading
-$_['heading_title']    = 'Banners';
+$_['heading_title']    = 'Bannerlar';
 
 // Text
-$_['text_success']     = 'Success: You have modified banners!';
-$_['text_list']        = 'Banner List';
-$_['text_add']         = 'Add Banner';
-$_['text_edit']        = 'Edit Banner';
+$_['text_success']     = 'Başarılı: Bannerları değiştirdiniz!';
+$_['text_list']        = 'Banner Listesi';
+$_['text_add']         = 'Banner Ekle';
+$_['text_edit']        = 'Bannerı Düzenle';
 $_['text_default']     = 'Varsayılan';
 
 // Column
-$_['column_name']      = 'Banner Name';
+$_['column_name']      = 'Banner Adı';
 $_['column_status']    = 'Durum';
 $_['column_action']    = 'İşlem';
 
 // Entry
-$_['entry_name']       = 'Banner Name';
-$_['entry_title']      = 'Title';
-$_['entry_link']       = 'Link';
-$_['entry_image']      = 'Image';
+$_['entry_name']       = 'Banner Adı';
+$_['entry_title']      = 'Başlık';
+$_['entry_link']       = 'Bağlantı';
+$_['entry_image']      = 'Görsel';
 $_['entry_status']     = 'Durum';
 $_['entry_sort_order'] = 'Sıralama';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify banners!';
-$_['error_name']       = 'Banner Name must be between 3 and 64 characters!';
-$_['error_title']      = 'Banner Title must be between 2 and 64 characters!';
+$_['error_permission'] = 'Uyarı: Bannerları değiştirme yetkiniz yok!';
+$_['error_name']       = 'Banner Adı 3 ile 64 karakter arasında olmalıdır!';
+$_['error_title']      = 'Banner Başlığı 2 ile 64 karakter arasında olmalıdır!';
