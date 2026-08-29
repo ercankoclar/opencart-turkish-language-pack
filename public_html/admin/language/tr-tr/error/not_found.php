40ec7ac84b8d166202b05b195863905405592f4a
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:20:43 +0300

    Update public_html/admin/language/tr-tr/error/not_found.php

--- public_html/admin/language/tr-tr/error/not_found.php
+++ public_html/admin/language/tr-tr/error/not_found.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']  = 'Page Not Found!';
+$_['heading_title']  = 'Sayfa Bulunamadı!';
 
 // Text
-$_['text_not_found'] = 'The page you are looking for could not be found! Please contact your administrator if the problem persists.';
+$_['text_not_found'] = 'Aradığınız sayfa bulunamadı! Sorun devam ederse lütfen yöneticinizle iletişime geçin.';
