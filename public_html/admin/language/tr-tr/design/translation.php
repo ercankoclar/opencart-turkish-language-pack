175f550beaa4b9e4092ef1fd7c83c6cd1c082a09
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:19:55 +0300

    Update public_html/admin/language/tr-tr/design/translation.php

--- public_html/admin/language/tr-tr/design/translation.php
+++ public_html/admin/language/tr-tr/design/translation.php
@@ -1,12 +1,12 @@
 <?php
 // Heading
-$_['heading_title']    = 'Language Editor';
+$_['heading_title']    = 'Dil Düzenleyici';
 
 // Text
-$_['text_success']     = 'Success: You have modified language editor!';
-$_['text_list']        = 'Translation List';
-$_['text_add']         = 'Add Translation';
-$_['text_edit']        = 'Edit Translation';
+$_['text_success']     = 'Başarılı: Dil düzenleyicisini değiştirdiniz!';
+$_['text_list']        = 'Çeviri Listesi';
+$_['text_add']         = 'Çeviri Ekle';
+$_['text_edit']        = 'Çeviriyi Düzenle';
 $_['text_default']     = 'Varsayılan';
 $_['text_store']       = 'Mağaza';
 $_['text_language']    = 'Dil';
@@ -15,18 +15,18 @@ $_['text_language']    = 'Dil';
 $_['column_store']     = 'Mağaza';
 $_['column_language']  = 'Dil';
 $_['column_route']     = 'Rota';
-$_['column_key']       = 'Key';
-$_['column_value']     = 'Value';
+$_['column_key']       = 'Anahtar';
+$_['column_value']     = 'Değer';
 $_['column_action']    = 'İşlem';
 
 // Entry
 $_['entry_store']      = 'Mağaza';
 $_['entry_language']   = 'Dil';
 $_['entry_route']      = 'Rota';
-$_['entry_key']        = 'Key';
+$_['entry_key']        = 'Anahtar';
 $_['entry_default']    = 'Varsayılan';
-$_['entry_value']      = 'Value';
+$_['entry_value']      = 'Değer';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify language editor!';
-$_['error_key']        = 'Key must be between 3 and 64 characters!';
+$_['error_permission'] = 'Uyarı: Dil düzenleyicisini değiştirme yetkiniz yok!';
+$_['error_key']        = 'Anahtar 3 ile 64 karakter arasında olmalıdır!';
