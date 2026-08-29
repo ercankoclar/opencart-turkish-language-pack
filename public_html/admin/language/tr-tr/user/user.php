48209aaa265b51fd0372c3ad3c35a5b9ae4a50b7
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:59:30 +0300

    Update public_html/admin/language/tr-tr/user/user.php

--- public_html/admin/language/tr-tr/user/user.php
+++ public_html/admin/language/tr-tr/user/user.php
@@ -4,14 +4,14 @@ $_['heading_title']            = 'Kullanıcılar';
 
 // Text
 $_['text_success']             = 'Başarılı: Kullanıcıları değiştirdiniz!';
-$_['text_list']                = 'User List';
-$_['text_add']                 = 'Add User';
-$_['text_edit']                = 'Edit User';
-$_['text_user']                = 'Users Details';
+$_['text_list']                = 'Kullanıcı Listesi';
+$_['text_add']                 = 'Kullanıcı Ekle';
+$_['text_edit']                = 'Kullanıcıyı Düzenle';
+$_['text_user']                = 'Kullanıcı Bilgileri';
 $_['text_password']            = 'Şifre';
-$_['text_other']               = 'Other';
-$_['text_login']               = 'Login History';
-$_['text_authorize']           = 'Authorize History';
+$_['text_other']               = 'Diğer';
+$_['text_login']               = 'Giriş Geçmişi';
+$_['text_authorize']           = 'Yetkilendirme Geçmişi';
 $_['text_filter']              = 'Filtrele';
 
 // Column
@@ -21,44 +21,44 @@ $_['column_email']             = 'E-posta';
 $_['column_user_group']        = 'Kullanıcı Grubu';
 $_['column_status']            = 'Durum';
 $_['column_ip']                = 'IP';
-$_['column_user_agent']        = 'User Agent';
+$_['column_user_agent']        = 'Kullanıcı Aracısı';
 $_['column_date_added']        = 'Eklenme Tarihi';
-$_['column_date_expire']       = 'Date Expire';
+$_['column_date_expire']       = 'Son Kullanma Tarihi';
 $_['column_action']            = 'İşlem';
 
 // Entry
 $_['entry_username']           = 'Kullanıcı Adı';
 $_['entry_user_group']         = 'Kullanıcı Grubu';
 $_['entry_password']           = 'Şifre';
-$_['entry_confirm']            = 'Confirm';
+$_['entry_confirm']            = 'Onayla';
 $_['entry_firstname']          = 'Ad';
 $_['entry_lastname']           = 'Soyad';
 $_['entry_email']              = 'E-posta';
-$_['entry_image']              = 'Image';
+$_['entry_image']              = 'Görsel';
 $_['entry_status']             = 'Durum';
 $_['entry_name']               = 'Ad';
 $_['entry_ip']                 = 'IP';
-$_['entry_date_from']          = 'Date From';
-$_['entry_date_to']            = 'Date To';
+$_['entry_date_from']          = 'Başlangıç Tarihi';
+$_['entry_date_to']            = 'Bitiş Tarihi';
 
 // Tabs
-$_['tab_authorize']            = 'Authorize';
-$_['tab_login']                = 'Login';
+$_['tab_authorize']            = 'Yetkilendirme';
+$_['tab_login']                = 'Giriş';
 
 // Error
 $_['error_permission']         = 'Uyarı: Kullanıcıları değiştirme yetkiniz yok!';
 $_['error_account']            = 'Uyarı: Kendi hesabınızı silemezsiniz!';
-$_['error_login']              = 'Warning: User login does not exist!';
-$_['error_username']           = 'Username must be between 3 and 20 characters!';
-$_['error_username_exists']    = 'Warning: Username is already in use!';
+$_['error_login']              = 'Uyarı: Kullanıcı girişi mevcut değil!';
+$_['error_username']           = 'Kullanıcı Adı 3 ile 20 karakter arasında olmalıdır!';
+$_['error_username_exists']    = 'Uyarı: Kullanıcı adı zaten kullanılıyor!';
 $_['error_firstname']          = 'Ad 1 ile 32 karakter arasında olmalıdır!';
 $_['error_lastname']           = 'Soyad 1 ile 32 karakter arasında olmalıdır!';
 $_['error_email']              = 'E-posta Adresi geçerli görünmüyor!';
-$_['error_email_exists']       = 'Warning: E-Mail Address is already registered!';
-$_['error_password']           = 'Password must contain a %s and be between %d and 20 characters!';
-$_['error_password_uppercase'] = 'uppercase';
-$_['error_password_lowercase'] = 'lowercase';
-$_['error_password_number']    = 'number';
-$_['error_password_symbol']    = 'symbol';
-$_['error_password_length']    = 'Password must be between %d and 20 characters!';
-$_['error_confirm']            = 'Password and password confirmation do not match!';
+$_['error_email_exists']       = 'Uyarı: E-posta Adresi zaten kayıtlı!';
+$_['error_password']           = 'Şifre %s içermeli ve %d ile 20 karakter arasında olmalıdır!';
+$_['error_password_uppercase'] = 'büyük harf';
+$_['error_password_lowercase'] = 'küçük harf';
+$_['error_password_number']    = 'rakam';
+$_['error_password_symbol']    = 'sembol';
+$_['error_password_length']    = 'Şifre %d ile 20 karakter arasında olmalıdır!';
+$_['error_confirm']            = 'Şifre ve şifre onayı eşleşmiyor!';
