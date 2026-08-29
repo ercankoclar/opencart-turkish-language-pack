d8b7840ec45461578ea0ec547c7d0d3981f05c5f
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:16:42 +0300

    Update public_html/catalog/language/tr-tr/mail/gdpr.php

--- public_html/catalog/language/tr-tr/mail/gdpr.php
+++ public_html/catalog/language/tr-tr/mail/gdpr.php
@@ -1,14 +1,14 @@
 <?php
 // Text
-$_['text_subject']  = '%s - GDPR Export/Deletion Request!';
-$_['text_export']   = 'Export Data Request';
-$_['text_remove']   = 'Account Deletion Request';
-$_['text_gdpr']     = 'A GDPR request from this email address, To confirm this action please click on the link below:';
-$_['text_ip']       = 'The IP used to make this request was:';
-$_['text_contact']  = 'If you did not make this request please contact the store owner here:';
-$_['text_thanks']   = 'Thanks,';
-$_['text_ignore']   = 'If you did not created this request, please ignore this email.';
+$_['text_subject']  = '%s - GDPR Dışa Aktarma/Silme Talebi!';
+$_['text_export']   = 'Veri Dışa Aktarma Talebi';
+$_['text_remove']   = 'Hesap Silme Talebi';
+$_['text_gdpr']     = 'Bu e-posta adresinden bir GDPR talebi gönderildi. Bu işlemi onaylamak için lütfen aşağıdaki bağlantıya tıklayın:';
+$_['text_ip']       = 'Bu talebin gönderildiği IP adresi:';
+$_['text_contact']  = 'Bu talebi siz oluşturmadıysanız lütfen mağaza sahibiyle buradan iletişime geçin:';
+$_['text_thanks']   = 'Teşekkürler,';
+$_['text_ignore']   = 'Bu talebi siz oluşturmadıysanız lütfen bu e-postayı dikkate almayın.';
 
 // Buttons
-$_['button_export'] = 'I confirm export my data';
-$_['button_remove'] = 'I confirm delete my account';
+$_['button_export'] = 'Verilerimi dışa aktarmayı onaylıyorum';
+$_['button_remove'] = 'Hesabımı silmeyi onaylıyorum';
