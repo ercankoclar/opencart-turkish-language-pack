20707fb0e7e598aad4d8cf73f2f100be668bb0c8
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:09:57 +0300

    Update public_html/catalog/language/tr-tr/cms/comment.php

--- public_html/catalog/language/tr-tr/cms/comment.php
+++ public_html/catalog/language/tr-tr/cms/comment.php
@@ -4,40 +4,40 @@ $_['heading_title']         = 'Yorumlar';
 
 // Text
 $_['text_by']               = 'Yazar';
-$_['text_write']            = 'Write a comment';
-$_['text_login']            = 'Please <a href="%s">login</a> or <a href="%s">register</a> to comment';
-$_['text_no_results']       = 'Be the first to write a comment for this article.';
-$_['text_sort']             = 'Sort By';
-$_['text_rating_asc']       = 'Rating (Lowest)';
-$_['text_rating_desc']      = 'Rating (Highest)';
-$_['text_date_added_asc']   = 'Date Added (Oldest)';
-$_['text_date_added_desc']  = 'Date Added (Latest)';
-$_['text_like']             = 'Like!';
-$_['text_dislike']          = 'Dislike!';
-$_['text_success']          = 'Success: Thank you for your comment!';
-$_['text_queue']            = 'Success: Your comment has been added to our moderation queue!';
-$_['text_rating']           = 'Success: Thank you for your rating!';
+$_['text_write']            = 'Yorum yaz';
+$_['text_login']            = 'Yorum yapmak için lütfen <a href="%s">giriş yapın</a> veya <a href="%s">kayıt olun</a>';
+$_['text_no_results']       = 'Bu makale için ilk yorumu siz yazın.';
+$_['text_sort']             = 'Sıralama';
+$_['text_rating_asc']       = 'Puan (Düşükten Yükseğe)';
+$_['text_rating_desc']      = 'Puan (Yüksekten Düşüğe)';
+$_['text_date_added_asc']   = 'Eklenme Tarihi (Eskiden Yeniye)';
+$_['text_date_added_desc']  = 'Eklenme Tarihi (Yeniden Eskiye)';
+$_['text_like']             = 'Beğen!';
+$_['text_dislike']          = 'Beğenme!';
+$_['text_success']          = 'Başarılı: Yorumunuz için teşekkür ederiz!';
+$_['text_queue']            = 'Başarılı: Yorumunuz moderasyon kuyruğuna eklendi!';
+$_['text_rating']           = 'Başarılı: Puanınız için teşekkür ederiz!';
 
 // Entry
 $_['entry_author']          = 'Adınız';
 $_['entry_comment']         = 'Yorum';
 
 // Buttons
-$_['button_comment']        = 'Post Comment';
-$_['button_reply']          = 'Post Reply';
-$_['button_replies']        = 'Show Replies';
-$_['button_more']           = 'See more replies...';
-$_['button_login']          = 'Login to Comment';
-$_['button_login_reply']    = 'Login to Reply';
-$_['button_like']           = 'Like';
-$_['button_dislike']        = 'Dislike';
+$_['button_comment']        = 'Yorum Gönder';
+$_['button_reply']          = 'Yanıt Gönder';
+$_['button_replies']        = 'Yanıtları Göster';
+$_['button_more']           = 'Daha fazla yanıt göster...';
+$_['button_login']          = 'Yorum Yapmak İçin Giriş Yap';
+$_['button_login_reply']    = 'Yanıtlamak İçin Giriş Yap';
+$_['button_like']           = 'Beğen';
+$_['button_dislike']        = 'Beğenme';
 
 // Error
-$_['error_article']         = 'Warning: Article could not be found!';
-$_['error_article_comment'] = 'Warning: Article comment could not be found!';
-$_['error_token']           = 'Warning: Comment token invalid!';
-$_['error_author']          = 'Your Name must be between 3 and 25 characters!';
-$_['error_comment']         = 'Comment must be between 25 and 1000 characters!';
-$_['error_interval']        = 'Warning: you must wait %s minutes before making another comment!';
-$_['error_login']           = 'You must login to comment on the article or rate it!';
-$_['error_status']          = 'Comments are disabled!';
+$_['error_article']         = 'Uyarı: Makale bulunamadı!';
+$_['error_article_comment'] = 'Uyarı: Makale yorumu bulunamadı!';
+$_['error_token']           = 'Uyarı: Yorum belirteci geçersiz!';
+$_['error_author']          = 'Adınız 3 ile 25 karakter arasında olmalıdır!';
+$_['error_comment']         = 'Yorum 25 ile 1000 karakter arasında olmalıdır!';
+$_['error_interval']        = 'Uyarı: Başka bir yorum yapmadan önce %s dakika beklemelisiniz!';
+$_['error_login']           = 'Makaleye yorum yapmak veya puan vermek için giriş yapmalısınız!';
+$_['error_status']          = 'Yorumlar devre dışı bırakılmış!';
