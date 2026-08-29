df30995945a39678dda6ccdca57144983895f4a2
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:30:46 +0300

    Update public_html/admin/language/tr-tr/localisation/language.php

--- public_html/admin/language/tr-tr/localisation/language.php
+++ public_html/admin/language/tr-tr/localisation/language.php
@@ -3,36 +3,36 @@
 $_['heading_title']     = 'Diller';
 
 // Text
-$_['text_success']      = 'Success: You have modified languages!';
-$_['text_list']         = 'Language List';
-$_['text_add']          = 'Add Language';
-$_['text_edit']         = 'Edit Language';
+$_['text_success']      = 'Başarılı: Dilleri değiştirdiniz!';
+$_['text_list']         = 'Dil Listesi';
+$_['text_add']          = 'Dil Ekle';
+$_['text_edit']         = 'Dili Düzenle';
 
 // Column
-$_['column_name']       = 'Language Name';
+$_['column_name']       = 'Dil Adı';
 $_['column_code']       = 'Kod';
 $_['column_sort_order'] = 'Sıralama';
 $_['column_action']     = 'İşlem';
 
 // Entry
-$_['entry_name']        = 'Language Name';
+$_['entry_name']        = 'Dil Adı';
 $_['entry_code']        = 'Kod';
-$_['entry_extension']   = 'Extension';
-$_['entry_locale']      = 'Locale';
+$_['entry_extension']   = 'Eklenti';
+$_['entry_locale']      = 'Yerel Ayar';
 $_['entry_status']      = 'Durum';
 $_['entry_sort_order']  = 'Sıralama';
 
 // Help
-$_['help_locale']       = 'Example: en_US.UTF-8,en_US,en-gb,en_gb,english';
-$_['help_status']       = 'Hide/Show it in language dropdown';
+$_['help_locale']       = 'Örnek: en_US.UTF-8,en_US,en-gb,en_gb,english';
+$_['help_status']       = 'Dil açılır menüsünde gizleyin/gösterin';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify languages!';
-$_['error_exists']      = 'Warning: You have already added this language!';
-$_['error_name']        = 'Language Name must be between 1 and 32 characters!';
-$_['error_code']        = 'Language Code must be between 2 and 5 characters!';
-$_['error_locale']      = 'Locale required!';
-$_['error_default']     = 'Warning: This language cannot be deleted as it is currently assigned as the default store language!';
-$_['error_admin']       = 'Warning: This Language cannot be deleted as it is currently assigned as the administration language!';
-$_['error_store']       = 'Warning: This language cannot be deleted as it is currently assigned to %s stores!';
-$_['error_order']       = 'Warning: This language cannot be deleted as it is currently assigned to %s orders!';
+$_['error_permission']  = 'Uyarı: Dilleri değiştirme yetkiniz yok!';
+$_['error_exists']      = 'Uyarı: Bu dili zaten eklediniz!';
+$_['error_name']        = 'Dil Adı 1 ile 32 karakter arasında olmalıdır!';
+$_['error_code']        = 'Dil Kodu 2 ile 5 karakter arasında olmalıdır!';
+$_['error_locale']      = 'Yerel Ayar gereklidir!';
+$_['error_default']     = 'Uyarı: Bu dil varsayılan mağaza dili olarak atandığı için silinemez!';
+$_['error_admin']       = 'Uyarı: Bu dil yönetim dili olarak atandığı için silinemez!';
+$_['error_store']       = 'Uyarı: Bu dil %s mağazaya atanmış olduğu için silinemez!';
+$_['error_order']       = 'Uyarı: Bu dil %s siparişe atanmış olduğu için silinemez!';
