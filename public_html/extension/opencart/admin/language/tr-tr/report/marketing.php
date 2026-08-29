d95a0ade3886c2f6db38f2429b598ad9f19d4bcf
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:37:50 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/report/marketing.php

--- public_html/extension/opencart/admin/language/tr-tr/report/marketing.php
+++ public_html/extension/opencart/admin/language/tr-tr/report/marketing.php
@@ -1,27 +1,27 @@
 <?php
 // Heading
-$_['heading_title']      = 'Marketing Report';
+$_['heading_title']      = 'Pazarlama Raporu';
 
 // Text
 $_['text_extension']     = 'Eklentiler';
-$_['text_edit']          = 'Edit Marketing Report';
-$_['text_success']       = 'Success: You have modified marketing report!';
+$_['text_edit']          = 'Pazarlama Raporunu Düzenle';
+$_['text_success']       = 'Başarılı: Pazarlama raporunu değiştirdiniz!';
 $_['text_filter']        = 'Filtrele';
-$_['text_all_status']    = 'All Statuses';
+$_['text_all_status']    = 'Tüm Durumlar';
 
 // Column
-$_['column_campaign']    = 'Campaign Name';
-$_['column_code']        = 'Code';
-$_['column_clicks']      = 'Clicks';
-$_['column_orders']      = 'No. Orders';
+$_['column_campaign']    = 'Kampanya Adı';
+$_['column_code']        = 'Kod';
+$_['column_clicks']      = 'Tıklamalar';
+$_['column_orders']      = 'Sipariş Sayısı';
 $_['column_total']       = 'Toplam';
 
 // Entry
 $_['entry_date_start']   = 'Başlangıç Tarihi';
 $_['entry_date_end']     = 'Bitiş Tarihi';
-$_['entry_order_status'] = 'Order Status';
+$_['entry_order_status'] = 'Sipariş Durumu';
 $_['entry_status']       = 'Durum';
 $_['entry_sort_order']   = 'Sıralama';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify marketing report!';
+$_['error_permission']   = 'Uyarı: Pazarlama raporunu değiştirme yetkiniz yok!';
