ef8049d4b0e6d454dce6cb81fed591333eaff82f
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:25:27 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/dashboard/order.php

--- public_html/extension/opencart/admin/language/tr-tr/dashboard/order.php
+++ public_html/extension/opencart/admin/language/tr-tr/dashboard/order.php
@@ -1,17 +1,17 @@
 <?php
 // Heading
-$_['heading_title']    = 'Total Orders';
+$_['heading_title']    = 'Toplam Sipariş';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified dashboard orders!';
-$_['text_edit']        = 'Edit Dashboard Orders';
-$_['text_view']        = 'View more...';
+$_['text_success']     = 'Başarılı: Kontrol paneli siparişler bölümünü değiştirdiniz!';
+$_['text_edit']        = 'Kontrol Paneli Siparişler Bölümünü Düzenle';
+$_['text_view']        = 'Daha fazlasını görüntüle...';
 
 // Entry
 $_['entry_status']     = 'Durum';
-$_['entry_sort_order'] = 'Sort Order';
-$_['entry_width']      = 'Width';
+$_['entry_sort_order'] = 'Sıralama';
+$_['entry_width']      = 'Genişlik';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify dashboard orders!';
+$_['error_permission'] = 'Uyarı: Kontrol paneli siparişler bölümünü değiştirme yetkiniz yok!';
