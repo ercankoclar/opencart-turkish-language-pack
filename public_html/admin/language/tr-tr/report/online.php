dd0d76cba5a0a3e100722342e3b29e566514412f
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:00:43 +0300

    Upload files to "public_html/admin/language/tr-tr/report"

---
+++ public_html/admin/language/tr-tr/report/online.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']     = 'Online Report';
+
+// Text
+$_['text_extension']    = 'Eklentiler';
+$_['text_success']      = 'Success: You have modified customers online report!';
+$_['text_list']         = 'Online List';
+$_['text_filter']       = 'Filtrele';
+$_['text_guest']        = 'Guest';
+
+// Column
+$_['column_ip']         = 'IP';
+$_['column_customer']   = 'Müşteri';
+$_['column_url']        = 'Last Page Visited';
+$_['column_referer']    = 'Referer';
+$_['column_date_added'] = 'Last Click';
+$_['column_action']     = 'İşlem';
+
+// Entry
+$_['entry_ip']          = 'IP';
+$_['entry_customer']    = 'Müşteri';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify customers online report!';
