735d7f8f3e623af786bfd222eb76f0b760a013a7
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:41:03 +0300

    Update public_html/admin/language/tr-tr/mail/gdpr_approve.php

--- public_html/admin/language/tr-tr/mail/gdpr_approve.php
+++ public_html/admin/language/tr-tr/mail/gdpr_approve.php
@@ -1,11 +1,11 @@
 <?php
 // Text
-$_['text_subject'] = '%s - GDPR request approved!';
-$_['text_request'] = 'Account Deletion Request';
-$_['text_hello']   = 'Hello <strong>%s</strong>,';
-$_['text_user']    = 'User';
-$_['text_gdpr']    = 'Your GDPR data deletion request has been approved and will be deleted in <strong>%s Days</strong>.';
-$_['text_q']       = 'Q. Why do we not delete your data straight away?';
-$_['text_a']       = 'A. Account deletion requests will process after <strong>%s days</strong> so any refunds, chargebacks or fraud detection can be processed.';
-$_['text_delete']  = 'You will receive a email informing you when your account has been deleted.';
-$_['text_thanks']  = 'Thanks,';
+$_['text_subject'] = '%s - GDPR talebiniz onaylandı!';
+$_['text_request'] = 'Hesap Silme Talebi';
+$_['text_hello']   = 'Merhaba <strong>%s</strong>,';
+$_['text_user']    = 'Kullanıcı';
+$_['text_gdpr']    = 'GDPR kapsamındaki veri silme talebiniz onaylandı ve <strong>%s Gün</strong> içinde verileriniz silinecek.';
+$_['text_q']       = 'S. Verilerinizi neden hemen silmiyoruz?';
+$_['text_a']       = 'C. Hesap silme talepleri, iadelerin, ters ibrazların veya dolandırıcılık tespitinin işleme alınabilmesi için <strong>%s gün</strong> sonra gerçekleştirilir.';
+$_['text_delete']  = 'Hesabınız silindiğinde sizi bilgilendiren bir e-posta alacaksınız.';
+$_['text_thanks']  = 'Teşekkürler,';
