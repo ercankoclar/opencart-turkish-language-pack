10bd51f1e138ec1f9b294498a9b9a7690b2b0819
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:40:11 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/report/sale_order.php

--- public_html/extension/opencart/admin/language/tr-tr/report/sale_order.php
+++ public_html/extension/opencart/admin/language/tr-tr/report/sale_order.php
@@ -1,33 +1,33 @@
 <?php
 // Heading
-$_['heading_title']     = 'Sales Report';
+$_['heading_title']     = 'Satış Raporu';
 
 // Text
-$_['text_extension']     = 'Eklentiler';
-$_['text_edit']          = 'Edit Sales Report';
-$_['text_success']       = 'Success: You have modified sales report!';
-$_['text_filter']        = 'Filtrele';
-$_['text_year']          = 'Years';
-$_['text_month']         = 'Months';
-$_['text_week']          = 'Weeks';
-$_['text_day']           = 'Days';
-$_['text_all_status']    = 'All Statuses';
+$_['text_extension']    = 'Eklentiler';
+$_['text_edit']         = 'Satış Raporunu Düzenle';
+$_['text_success']      = 'Başarılı: Satış raporunu değiştirdiniz!';
+$_['text_filter']       = 'Filtrele';
+$_['text_year']         = 'Yıllar';
+$_['text_month']        = 'Aylar';
+$_['text_week']         = 'Haftalar';
+$_['text_day']          = 'Günler';
+$_['text_all_status']   = 'Tüm Durumlar';
 
 // Column
-$_['column_date_start']  = 'Başlangıç Tarihi';
-$_['column_date_end']    = 'Bitiş Tarihi';
-$_['column_orders']      = 'No. Orders';
-$_['column_products']    = 'No. Products';
-$_['column_tax']         = 'Vergi';
-$_['column_total']       = 'Toplam';
+$_['column_date_start'] = 'Başlangıç Tarihi';
+$_['column_date_end']   = 'Bitiş Tarihi';
+$_['column_orders']     = 'Sipariş Sayısı';
+$_['column_products']   = 'Ürün Sayısı';
+$_['column_tax']        = 'Vergi';
+$_['column_total']      = 'Toplam';
 
 // Entry
-$_['entry_date_start']   = 'Başlangıç Tarihi';
-$_['entry_date_end']     = 'Bitiş Tarihi';
-$_['entry_group']        = 'Group By';
-$_['entry_order_status'] = 'Order Status';
-$_['entry_status']       = 'Durum';
-$_['entry_sort_order']   = 'Sıralama';
+$_['entry_date_start']  = 'Başlangıç Tarihi';
+$_['entry_date_end']    = 'Bitiş Tarihi';
+$_['entry_group']       = 'Gruplama';
+$_['entry_order_status'] = 'Sipariş Durumu';
+$_['entry_status']      = 'Durum';
+$_['entry_sort_order']  = 'Sıralama';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify sales report!';
+$_['error_permission']  = 'Uyarı: Satış raporunu değiştirme yetkiniz yok!';
