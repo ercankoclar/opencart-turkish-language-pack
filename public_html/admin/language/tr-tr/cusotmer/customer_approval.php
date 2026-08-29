bf794441a29257183b77c195142f8c3d9dc1c8cd
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:16:02 +0300

    Update public_html/admin/language/tr-tr/cusotmer/customer_approval.php

--- public_html/admin/language/tr-tr/cusotmer/customer_approval.php
+++ public_html/admin/language/tr-tr/cusotmer/customer_approval.php
@@ -1,22 +1,22 @@
 <?php
 // Heading
-$_['heading_title']         = 'Customer Approvals';
+$_['heading_title']         = 'Müşteri Onayları';
 
 // Text
-$_['text_success']          = 'Success: You have modified customer approvals!';
-$_['text_list']             = 'Customer Approval List';
+$_['text_success']          = 'Başarılı: Müşteri onaylarını değiştirdiniz!';
+$_['text_list']             = 'Müşteri Onay Listesi';
 $_['text_default']          = 'Varsayılan';
 $_['text_customer']         = 'Müşteri';
-$_['text_affiliate']        = 'Affiliate';
+$_['text_affiliate']        = 'İş Ortağı';
 $_['text_filter']           = 'Filtrele';
 $_['text_approve']          = 'Onayla';
-$_['text_deny']             = 'Deny';
+$_['text_deny']             = 'Reddet';
 
 // Column
 $_['column_customer']       = 'Müşteri';
 $_['column_email']          = 'E-posta';
 $_['column_customer_group'] = 'Müşteri Grubu';
-$_['column_type']           = 'Type';
+$_['column_type']           = 'Tür';
 $_['column_date_added']     = 'Eklenme Tarihi';
 $_['column_action']         = 'İşlem';
 
@@ -24,9 +24,9 @@ $_['column_action']         = 'İşlem';
 $_['entry_customer']        = 'Müşteri';
 $_['entry_email']           = 'E-posta';
 $_['entry_customer_group']  = 'Müşteri Grubu';
-$_['entry_type']            = 'Type';
-$_['entry_date_from']       = 'Date From';
-$_['entry_date_to']         = 'Date To';
+$_['entry_type']            = 'Tür';
+$_['entry_date_from']       = 'Başlangıç Tarihi';
+$_['entry_date_to']         = 'Bitiş Tarihi';
 
 // Error
-$_['error_permission']      = 'Warning: You do not have permission to modify customer approvals!';
+$_['error_permission']      = 'Uyarı: Müşteri onaylarını değiştirme yetkiniz yok!';
