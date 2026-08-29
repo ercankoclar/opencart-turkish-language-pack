4814f8ee2ad6eab6a2ab5a55a77808dbd0ac573b
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:23:27 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/dashboard/activity.php

--- public_html/extension/opencart/admin/language/tr-tr/dashboard/activity.php
+++ public_html/extension/opencart/admin/language/tr-tr/dashboard/activity.php
@@ -1,32 +1,32 @@
 <?php
 // Heading
-$_['heading_title']                = 'Recent Activity';
+$_['heading_title']                = 'Son Etkinlikler';
 
 // Text
 $_['text_extension']               = 'Eklentiler';
-$_['text_success']                 = 'Success: You have modified dashboard activity!';
-$_['text_edit']                    = 'Edit Dashboard Recent Activity';
-$_['text_activity_register']       = '<a href="customer_id=%d">%s</a> registered a new account.';
-$_['text_activity_edit']           = '<a href="customer_id=%d">%s</a> updated their account details.';
-$_['text_activity_password']       = '<a href="customer_id=%d">%s</a> updated their account password.';
-$_['text_activity_reset']          = '<a href="customer_id=%d">%s</a> reset their account password.';
-$_['text_activity_login']          = '<a href="customer_id=%d">%s</a> logged in.';
-$_['text_activity_forgotten']      = '<a href="customer_id=%d">%s</a> has requested a reset password.';
-$_['text_activity_address_add']    = '<a href="customer_id=%d">%s</a> added a new address.';
-$_['text_activity_address_edit']   = '<a href="customer_id=%d">%s</a> updated their address.';
-$_['text_activity_address_delete'] = '<a href="customer_id=%d">%s</a> deleted one of their addresses.';
-$_['text_activity_return_account'] = '<a href="customer_id=%d">%s</a> submitted a product <a href="return_id=%d">return</a>.';
-$_['text_activity_return_guest']   = '%s submitted a product <a href="return_id=%d">return</a>.';
-$_['text_activity_order_account']  = '<a href="customer_id=%d">%s</a> added a <a href="order_id=%d">new order</a>.';
-$_['text_activity_order_guest']    = '%s created a <a href="order_id=%d">new order</a>.';
-$_['text_activity_affiliate_add']  = '<a href="customer_id=%d">%s</a> registered for a affiliate account.';
-$_['text_activity_affiliate_edit'] = '<a href="customer_id=%d">%s</a> updated their affiliate details.';
-$_['text_activity_transaction']    = '<a href="customer_id=%d">%s</a> received commission from an new <a href="order_id=%d">order</a>.';
+$_['text_success']                 = 'Başarılı: Kontrol paneli etkinliklerini değiştirdiniz!';
+$_['text_edit']                    = 'Kontrol Paneli Son Etkinliklerini Düzenle';
+$_['text_activity_register']       = '<a href="customer_id=%d">%s</a> yeni bir hesap oluşturdu.';
+$_['text_activity_edit']           = '<a href="customer_id=%d">%s</a> hesap bilgilerini güncelledi.';
+$_['text_activity_password']       = '<a href="customer_id=%d">%s</a> hesap şifresini güncelledi.';
+$_['text_activity_reset']          = '<a href="customer_id=%d">%s</a> hesap şifresini sıfırladı.';
+$_['text_activity_login']           = '<a href="customer_id=%d">%s</a> giriş yaptı.';
+$_['text_activity_forgotten']      = '<a href="customer_id=%d">%s</a> şifre sıfırlama talebinde bulundu.';
+$_['text_activity_address_add']    = '<a href="customer_id=%d">%s</a> yeni bir adres ekledi.';
+$_['text_activity_address_edit']   = '<a href="customer_id=%d">%s</a> adresini güncelledi.';
+$_['text_activity_address_delete'] = '<a href="customer_id=%d">%s</a> adreslerinden birini sildi.';
+$_['text_activity_return_account'] = '<a href="customer_id=%d">%s</a> bir ürün <a href="return_id=%d">iade</a> talebi oluşturdu.';
+$_['text_activity_return_guest']   = '%s bir ürün <a href="return_id=%d">iade</a> talebi oluşturdu.';
+$_['text_activity_order_account']  = '<a href="customer_id=%d">%s</a> <a href="order_id=%d">yeni bir sipariş</a> oluşturdu.';
+$_['text_activity_order_guest']    = '%s <a href="order_id=%d">yeni bir sipariş</a> oluşturdu.';
+$_['text_activity_affiliate_add']  = '<a href="customer_id=%d">%s</a> satış ortağı hesabı oluşturdu.';
+$_['text_activity_affiliate_edit'] = '<a href="customer_id=%d">%s</a> satış ortağı bilgilerini güncelledi.';
+$_['text_activity_transaction']    = '<a href="customer_id=%d">%s</a> yeni bir <a href="order_id=%d">siparişten</a> komisyon aldı.';
 
 // Entry
 $_['entry_status']                 = 'Durum';
-$_['entry_sort_order']             = 'Sort Order';
-$_['entry_width']                  = 'Width';
+$_['entry_sort_order']             = 'Sıralama';
+$_['entry_width']                  = 'Genişlik';
 
 // Error
-$_['error_permission']             = 'Warning: You do not have permission to modify dashboard activity!';
+$_['error_permission']             = 'Uyarı: Kontrol paneli etkinliklerini değiştirme yetkiniz yok!';
