c1f56dad89bfa75f4c830b991d28e77fe0706c84
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:31:58 +0300

    Update public_html/admin/language/tr-tr/localisation/order_status.php

--- public_html/admin/language/tr-tr/localisation/order_status.php
+++ public_html/admin/language/tr-tr/localisation/order_status.php
@@ -3,21 +3,21 @@
 $_['heading_title']    = 'Sipariş Durumları';
 
 // Text
-$_['text_success']     = 'Success: You have modified order statuses!';
-$_['text_list']        = 'Order Status List';
-$_['text_add']         = 'Add Order Status';
-$_['text_edit']        = 'Edit Order Status';
+$_['text_success']     = 'Başarılı: Sipariş durumlarını değiştirdiniz!';
+$_['text_list']        = 'Sipariş Durumu Listesi';
+$_['text_add']         = 'Sipariş Durumu Ekle';
+$_['text_edit']        = 'Sipariş Durumunu Düzenle';
 
 // Column
-$_['column_name']      = 'Order Status Name';
+$_['column_name']      = 'Sipariş Durumu Adı';
 $_['column_action']    = 'İşlem';
 
 // Entry
-$_['entry_name']       = 'Order Status Name';
+$_['entry_name']       = 'Sipariş Durumu Adı';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify order statuses!';
-$_['error_name']       = 'Order Status Name must be between 3 and 32 characters!';
-$_['error_default']    = 'Warning: This order status cannot be deleted as it is currently assigned as the default store order status!';
-$_['error_download']   = 'Warning: This order status cannot be deleted as it is currently assigned as the default download status!';
-$_['error_order']      = 'Warning: This order status cannot be deleted as it is currently assigned to %s orders!';
+$_['error_permission'] = 'Uyarı: Sipariş durumlarını değiştirme yetkiniz yok!';
+$_['error_name']       = 'Sipariş Durumu Adı 3 ile 32 karakter arasında olmalıdır!';
+$_['error_default']    = 'Uyarı: Bu sipariş durumu varsayılan mağaza sipariş durumu olarak atandığı için silinemez!';
+$_['error_download']   = 'Uyarı: Bu sipariş durumu varsayılan indirme durumu olarak atandığı için silinemez!';
+$_['error_order']      = 'Uyarı: Bu sipariş durumu %s siparişe atanmış olduğu için silinemez!';
