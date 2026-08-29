55eaab827f9dfa01c8b6eb5299d64dbfdc4c9902
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:26:08 +0300

    Update public_html/admin/language/tr-tr/extension/payment.php

--- public_html/admin/language/tr-tr/extension/payment.php
+++ public_html/admin/language/tr-tr/extension/payment.php
@@ -1,18 +1,18 @@
 <?php
 // Heading
-$_['heading_title']     = 'Payments';
+$_['heading_title']     = 'Ödemeler';
 
 // Text
-$_['text_success']      = 'Success: You have modified payments!';
-$_['text_list']         = 'Payment List';
+$_['text_success']      = 'Başarılı: Ödeme yöntemlerini değiştirdiniz!';
+$_['text_list']         = 'Ödeme Listesi';
 
 // Column
-$_['column_name']       = 'Payment Method';
-$_['column_vendor']     = 'Vendor';
+$_['column_name']       = 'Ödeme Yöntemi';
+$_['column_vendor']     = 'Sağlayıcı';
 $_['column_status']     = 'Durum';
 $_['column_sort_order'] = 'Sıralama';
 $_['column_action']     = 'İşlem';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify payments!';
-$_['error_extension']   = 'Warning: Extension does not exist!';
+$_['error_permission']  = 'Uyarı: Ödemeleri değiştirme yetkiniz yok!';
+$_['error_extension']   = 'Uyarı: Eklenti mevcut değil!';
