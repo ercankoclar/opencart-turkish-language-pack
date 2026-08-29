35e276a080747437b950ca43a90a3d1cac969287
ercankoclar <ercankoclar@duck.com>
2026-08-29 15:38:14 +0300

    Upload files to "public_html/catalog/language/tr-tr/information"

---
+++ public_html/catalog/language/tr-tr/information/contact.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']  = 'Bize Ulaşın';
+
+// Text
+$_['text_location']  = 'Our Locations';
+$_['text_store']     = 'Our Stores';
+$_['text_contact']   = 'Contact Form';
+$_['text_address']   = 'Adres';
+$_['text_telephone'] = 'Telefon';
+$_['text_open']      = 'Opening Times';
+$_['text_comment']   = 'Comments';
+$_['text_message']   = '<p>Your enquiry has been successfully sent to the store owner!</p>';
+
+// Entry
+$_['entry_name']     = 'Your Name';
+$_['entry_email']    = 'E-posta Adresi';
+$_['entry_enquiry']  = 'Enquiry';
+
+// Email
+$_['email_subject']  = 'Enquiry %s';
+
+// Errors
+$_['error_name']     = 'Name must be between 3 and 32 characters!';
+$_['error_email']    = 'E-Mail Address does not appear to be valid!';
+$_['error_enquiry']  = 'Enquiry must be between 10 and 3000 characters!';
---
+++ public_html/catalog/language/tr-tr/information/sitemap.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']    = 'Site Haritası';
+
+// Text
+$_['text_special']     = 'Special Offers';
+$_['text_account']     = 'My Account';
+$_['text_edit']        = 'Edit Information';
+$_['text_password']    = 'Password';
+$_['text_address']     = 'Address Book';
+$_['text_history']     = 'Order History';
+$_['text_download']    = 'Downloads';
+$_['text_cart']        = 'Shopping Cart';
+$_['text_checkout']    = 'Checkout';
+$_['text_search']      = 'Ara';
+$_['text_information'] = 'Bilgi';
+$_['text_contact']     = 'Bize Ulaşın';
