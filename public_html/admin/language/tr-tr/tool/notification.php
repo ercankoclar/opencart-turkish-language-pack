42bb298d72a474515b3c6f179aa1835f23400983
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:56:41 +0300

    Update public_html/admin/language/tr-tr/tool/notification.php

--- public_html/admin/language/tr-tr/tool/notification.php
+++ public_html/admin/language/tr-tr/tool/notification.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title']    = 'Notifications';
+$_['heading_title']    = 'Bildirimler';
 
 // Text
-$_['text_success']     = 'Success: You have modified notifications!';
-$_['text_list']        = 'Notification List';
+$_['text_success']     = 'Başarılı: Bildirimleri değiştirdiniz!';
+$_['text_list']        = 'Bildirim Listesi';
 
 // Column
-$_['column_message']   = 'Message';
+$_['column_message']   = 'Mesaj';
 $_['column_action']    = 'İşlem';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify notifications!';
+$_['error_permission'] = 'Uyarı: Bildirimleri değiştirme yetkiniz yok!';
