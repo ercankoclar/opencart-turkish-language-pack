84d9ce619518d8b776091c530f91e81d0008ff7b
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:26:20 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/dashboard/sale.php

--- public_html/extension/opencart/admin/language/tr-tr/dashboard/sale.php
+++ public_html/extension/opencart/admin/language/tr-tr/dashboard/sale.php
@@ -1,17 +1,17 @@
 <?php
 // Heading
-$_['heading_title']    = 'Total Sales';
+$_['heading_title']    = 'Toplam Satışlar';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified dashboard sales!';
-$_['text_edit']        = 'Edit Dashboard Sales';
-$_['text_view']        = 'View more...';
+$_['text_success']     = 'Başarılı: Kontrol paneli satışlar bölümünü değiştirdiniz!';
+$_['text_edit']        = 'Kontrol Paneli Satışlar Bölümünü Düzenle';
+$_['text_view']        = 'Daha fazlasını görüntüle...';
 
 // Entry
 $_['entry_status']     = 'Durum';
-$_['entry_sort_order'] = 'Sort Order';
-$_['entry_width']      = 'Width';
+$_['entry_sort_order'] = 'Sıralama';
+$_['entry_width']      = 'Genişlik';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify dashboard sales!';
+$_['error_permission'] = 'Uyarı: Kontrol paneli satışlar bölümünü değiştirme yetkiniz yok!';
