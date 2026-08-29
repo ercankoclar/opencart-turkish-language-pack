c7d852f50e53816946b0b7219cfb7a134450b474
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:48:53 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/total/reward.php

--- public_html/extension/opencart/admin/language/tr-tr/total/reward.php
+++ public_html/extension/opencart/admin/language/tr-tr/total/reward.php
@@ -4,12 +4,12 @@ $_['heading_title']    = 'Ödül Puanları';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified reward points total!';
-$_['text_edit']        = 'Edit Reward Points Total';
+$_['text_success']     = 'Başarılı: Ödül puanları toplamını değiştirdiniz!';
+$_['text_edit']        = 'Ödül Puanları Toplamını Düzenle';
 
 // Entry
 $_['entry_status']     = 'Durum';
 $_['entry_sort_order'] = 'Sıralama';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify reward points total!';
+$_['error_permission'] = 'Uyarı: Ödül puanları toplamını değiştirme yetkiniz yok!';
