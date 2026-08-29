4441c9140dc56214181ec3aafc8297d94c246f81
ercankoclar <ercankoclar@duck.com>
2026-08-29 16:10:48 +0300

    Upload files to "public_html/extension/opencart/admin/language/tr-tr/module"

---
+++ public_html/extension/opencart/admin/language/tr-tr/module/account.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'Account';
+
+// Text
+$_['text_extension']   = 'Eklentiler';
+$_['text_success']     = 'Success: You have modified account module!';
+$_['text_edit']        = 'Edit Account Module';
+
+// Entry
+$_['entry_status']     = 'Durum';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify account module!';
