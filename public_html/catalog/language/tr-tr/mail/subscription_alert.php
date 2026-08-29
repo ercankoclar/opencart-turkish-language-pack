66872e592384a44949c06a7fce4346eccb9e2100
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:19:20 +0300

    Update public_html/catalog/language/tr-tr/mail/subscription_alert.php

--- public_html/catalog/language/tr-tr/mail/subscription_alert.php
+++ public_html/catalog/language/tr-tr/mail/subscription_alert.php
@@ -1,10 +1,10 @@
 <?php
 // Text
-$_['text_subject']              = '%s - Order %s - Canceled Subscription';
-$_['text_received']             = 'You have received a canceled subscription.';
-$_['text_orders_id']            = 'Order ID:';
-$_['text_subscription_id']      = 'Subscription ID';
-$_['text_date_added']           = 'Date Added:';
-$_['text_subscription_status']  = 'Subscription Status:';
-$_['text_comment']              = 'The comments for your subscription are:';
-$_['text_canceled']             = 'Success: The Subscription profile has been canceled!';
+$_['text_subject']              = '%s - Sipariş %s - İptal Edilen Abonelik';
+$_['text_received']             = 'İptal edilen bir abonelik aldınız.';
+$_['text_orders_id']            = 'Sipariş No:';
+$_['text_subscription_id']      = 'Abonelik No';
+$_['text_date_added']           = 'Eklenme Tarihi:';
+$_['text_subscription_status']  = 'Abonelik Durumu:';
+$_['text_comment']              = 'Aboneliğinizle ilgili yorumlar:';
+$_['text_canceled']             = 'Başarılı: Abonelik profili iptal edildi!';
