309328a6bc9c1ab5905faec85f627851ab46d589
ercankoclar <ercankoclar@duck.com>
2026-08-29 17:03:03 +0300

    Upload files to "public_html/extension/opencart/catalog/language/tr-tr/api"

---
+++ public_html/extension/opencart/catalog/language/tr-tr/api/coupon.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_success']   = 'Başarılı: Kupon indiriminiz uygulandı!';
+
+// Error
+$_['error_customer'] = 'Uyarı: Müşteri bilgileri gereklidir!';
+$_['error_product']  = 'Uyarı: Ürünler gereklidir!';
+$_['error_coupon']   = 'Uyarı: Kupon geçersiz, süresi dolmuş veya kullanım limitine ulaşmış!';
+$_['error_status']   = 'Uyarı: Kuponlar bu mağazada etkin değil!';
+$_['error_confirm']  = 'Uyarı: Lütfen kupon formunu hatalar açısından dikkatlice kontrol edin!';
---
+++ public_html/extension/opencart/catalog/language/tr-tr/api/reward.php
@@ -0,0 +1,11 @@
+<?php
+// Text
+$_['text_success']   = 'Başarılı: Ödül puanı indiriminiz uygulandı!';
+
+// Error
+$_['error_customer'] = 'Uyarı: Müşteri bilgileri gereklidir!';
+$_['error_product']  = 'Uyarı: Ürünler gereklidir!';
+$_['error_points']   = 'Uyarı: %s ödül puanınız yok!';
+$_['error_maximum']  = 'Uyarı: Uygulanabilecek maksimum puan sayısı %s!';
+$_['error_status']   = 'Uyarı: Ödül puanları bu mağazada etkin değil!';
+$_['error_confirm']  = 'Uyarı: Lütfen ödül puanı formunu hatalar açısından dikkatlice kontrol edin!';
