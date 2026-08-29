a53237dff051c62a4fd1af88925ea019e7b4d241
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:38:10 +0300

    Update public_html/admin/language/tr-tr/mail/authorize_reset.php

--- public_html/admin/language/tr-tr/mail/authorize_reset.php
+++ public_html/admin/language/tr-tr/mail/authorize_reset.php
@@ -1,7 +1,7 @@
 <?php
 // Text
-$_['text_subject'] = 'Reset security code attempts';
-$_['text_reset']   = 'Some one entered the security code wrongly more than 3 times.';
-$_['text_link']    = 'Click on the link below to reset account security:';
+$_['text_subject'] = 'Güvenlik kodu denemelerini sıfırla';
+$_['text_reset']   = 'Birisi güvenlik kodunu 3 kereden fazla yanlış girdi.';
+$_['text_link']    = 'Hesap güvenliğini sıfırlamak için aşağıdaki bağlantıya tıklayın:';
 $_['text_ip']      = 'IP:';
-$_['text_regards'] = 'Best Regards';
+$_['text_regards'] = 'Saygılarımla';
