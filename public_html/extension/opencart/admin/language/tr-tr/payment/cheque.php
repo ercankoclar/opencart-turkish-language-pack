2916058bc0ead9f60b303fde60848b6e219a1f58
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:33:14 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/payment/cheque.php

--- public_html/extension/opencart/admin/language/tr-tr/payment/cheque.php
+++ public_html/extension/opencart/admin/language/tr-tr/payment/cheque.php
@@ -1,19 +1,19 @@
 <?php
 // Heading
-$_['heading_title']      = 'Cheque / Money Order';
+$_['heading_title']      = 'Çek / Posta Havalesi';
 
 // Text
 $_['text_extension']     = 'Eklentiler';
-$_['text_success']       = 'Success: You have modified cheque / money order account details!';
-$_['text_edit']          = 'Edit Cheque / Money Order';
+$_['text_success']       = 'Başarılı: Çek / posta havalesi hesap bilgilerini değiştirdiniz!';
+$_['text_edit']          = 'Çek / Posta Havalesini Düzenle';
 
 // Entry
-$_['entry_payable']      = 'Payable To';
+$_['entry_payable']      = 'Ödeme Yapılacak Kişi / Kurum';
 $_['entry_order_status'] = 'Sipariş Durumu';
 $_['entry_geo_zone']     = 'Coğrafi Bölge';
 $_['entry_status']       = 'Durum';
 $_['entry_sort_order']   = 'Sıralama';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify payment cheque / money order!';
-$_['error_payable']      = 'Payable To required!';
+$_['error_permission']   = 'Uyarı: Çek / posta havalesi ödeme yöntemini değiştirme yetkiniz yok!';
+$_['error_payable']      = 'Ödeme yapılacak kişi / kurum gereklidir!';
