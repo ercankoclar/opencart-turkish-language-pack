2032019e3b411c98bd3df4702f5b6ae8690daccd
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:36:54 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/report/customer.php

--- public_html/extension/opencart/admin/language/tr-tr/report/customer.php
+++ public_html/extension/opencart/admin/language/tr-tr/report/customer.php
@@ -1,29 +1,29 @@
 <?php
 // Heading
-$_['heading_title']     = 'Customer Report';
+$_['heading_title']     = 'Müşteri Raporu';
 
 // Text
 $_['text_extension']    = 'Eklentiler';
-$_['text_edit']         = 'Edit Customer Report';
-$_['text_success']      = 'Success: You have modified customers report!';
+$_['text_edit']         = 'Müşteri Raporunu Düzenle';
+$_['text_success']      = 'Başarılı: Müşteri raporunu değiştirdiniz!';
 $_['text_filter']       = 'Filtrele';
-$_['text_year']         = 'Years';
-$_['text_month']        = 'Months';
-$_['text_week']         = 'Weeks';
-$_['text_day']          = 'Days';
-$_['text_all_status']   = 'All Statuses';
+$_['text_year']         = 'Yıllar';
+$_['text_month']        = 'Aylar';
+$_['text_week']         = 'Haftalar';
+$_['text_day']          = 'Günler';
+$_['text_all_status']   = 'Tüm Durumlar';
 
 // Column
 $_['column_date_start'] = 'Başlangıç Tarihi';
 $_['column_date_end']   = 'Bitiş Tarihi';
-$_['column_total']      = 'No. Customers';
+$_['column_total']      = 'Müşteri Sayısı';
 
 // Entry
 $_['entry_date_start']  = 'Başlangıç Tarihi';
 $_['entry_date_end']    = 'Bitiş Tarihi';
-$_['entry_group']       = 'Group By';
+$_['entry_group']       = 'Gruplama';
 $_['entry_status']      = 'Durum';
 $_['entry_sort_order']  = 'Sıralama';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify customers report!';
+$_['error_permission']  = 'Uyarı: Müşteri raporunu değiştirme yetkiniz yok!';
