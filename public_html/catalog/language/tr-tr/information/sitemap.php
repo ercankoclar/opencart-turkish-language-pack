b9762d1d5e36af6ddcc2cc0da666b8e8618856ee
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:14:24 +0300

    Update public_html/catalog/language/tr-tr/information/sitemap.php

--- public_html/catalog/language/tr-tr/information/sitemap.php
+++ public_html/catalog/language/tr-tr/information/sitemap.php
@@ -3,15 +3,15 @@
 $_['heading_title']    = 'Site Haritası';
 
 // Text
-$_['text_special']     = 'Special Offers';
-$_['text_account']     = 'My Account';
-$_['text_edit']        = 'Edit Information';
-$_['text_password']    = 'Password';
-$_['text_address']     = 'Address Book';
-$_['text_history']     = 'Order History';
-$_['text_download']    = 'Downloads';
-$_['text_cart']        = 'Shopping Cart';
-$_['text_checkout']    = 'Checkout';
+$_['text_special']     = 'Özel Teklifler';
+$_['text_account']     = 'Hesabım';
+$_['text_edit']        = 'Bilgileri Düzenle';
+$_['text_password']    = 'Şifre';
+$_['text_address']     = 'Adres Defteri';
+$_['text_history']     = 'Sipariş Geçmişi';
+$_['text_download']    = 'İndirmeler';
+$_['text_cart']        = 'Alışveriş Sepeti';
+$_['text_checkout']    = 'Ödeme';
 $_['text_search']      = 'Ara';
 $_['text_information'] = 'Bilgi';
 $_['text_contact']     = 'Bize Ulaşın';
