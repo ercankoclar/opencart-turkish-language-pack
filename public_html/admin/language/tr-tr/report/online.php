a1afd23830ddd5354b5d2a895e208f47c59e1cfa
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:50:43 +0300

    Update public_html/admin/language/tr-tr/report/online.php

--- public_html/admin/language/tr-tr/report/online.php
+++ public_html/admin/language/tr-tr/report/online.php
@@ -1,20 +1,20 @@
 <?php
 // Heading
-$_['heading_title']     = 'Online Report';
+$_['heading_title']     = 'Çevrimiçi Rapor';
 
 // Text
 $_['text_extension']    = 'Eklentiler';
-$_['text_success']      = 'Success: You have modified customers online report!';
-$_['text_list']         = 'Online List';
+$_['text_success']      = 'Başarılı: Çevrimiçi müşteri raporunu değiştirdiniz!';
+$_['text_list']         = 'Çevrimiçi Listesi';
 $_['text_filter']       = 'Filtrele';
-$_['text_guest']        = 'Guest';
+$_['text_guest']        = 'Misafir';
 
 // Column
 $_['column_ip']         = 'IP';
 $_['column_customer']   = 'Müşteri';
-$_['column_url']        = 'Last Page Visited';
-$_['column_referer']    = 'Referer';
-$_['column_date_added'] = 'Last Click';
+$_['column_url']        = 'Son Ziyaret Edilen Sayfa';
+$_['column_referer']    = 'Yönlendiren';
+$_['column_date_added'] = 'Son Tıklama';
 $_['column_action']     = 'İşlem';
 
 // Entry
@@ -22,4 +22,4 @@ $_['entry_ip']          = 'IP';
 $_['entry_customer']    = 'Müşteri';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify customers online report!';
+$_['error_permission']  = 'Uyarı: Çevrimiçi müşteri raporunu değiştirme yetkiniz yok!';
