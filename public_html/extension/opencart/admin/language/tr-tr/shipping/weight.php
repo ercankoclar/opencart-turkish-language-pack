07edb78c50ff2fc4cc0388ac4009301f7aea1976
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:45:24 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/shipping/weight.php

--- public_html/extension/opencart/admin/language/tr-tr/shipping/weight.php
+++ public_html/extension/opencart/admin/language/tr-tr/shipping/weight.php
@@ -4,18 +4,18 @@ $_['heading_title']    = 'Ağırlığa Göre Kargo';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified weight based shipping!';
-$_['text_edit']        = 'Edit Weight Based Shipping';
+$_['text_success']     = 'Başarılı: Ağırlığa göre kargo ayarlarını değiştirdiniz!';
+$_['text_edit']        = 'Ağırlığa Göre Kargoyu Düzenle';
 
 // Entry
-$_['entry_rate']       = 'Rates';
+$_['entry_rate']       = 'Ücretler';
 $_['entry_tax_class']  = 'Vergi Sınıfı';
 $_['entry_geo_zone']   = 'Coğrafi Bölge';
 $_['entry_status']     = 'Durum';
 $_['entry_sort_order'] = 'Sıralama';
 
 // Help
-$_['help_rate']        = 'Example: 5:10.00,7:12.00 Weight:Cost,Weight:Cost, etc.';
+$_['help_rate']        = 'Örnek: 5:10.00,7:12.00 Ağırlık:Ücret,Ağırlık:Ücret vb.';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify weight based shipping!';
+$_['error_permission'] = 'Uyarı: Ağırlığa göre kargo ayarlarını değiştirme yetkiniz yok!';
