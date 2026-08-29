af77de888211000bea552c05a788e53aca8960a9
ercankoclar <ercankoclar@duck.com>
2026-08-29 15:35:17 +0300

    Upload files to "public_html/catalog/language/tr-tr/checkout"

---
+++ public_html/catalog/language/tr-tr/checkout/register.php
@@ -0,0 +1,56 @@
+<?php
+// Heading
+$_['heading_title']            = 'Your Personal Details';
+
+// Text
+$_['text_login']               = 'If you already have an account with us, please login at the <a href="%s"><strong>login page</strong></a>.';
+$_['text_register']            = 'Hesap Oluştur';
+$_['text_guest']               = 'Üyeliksiz Ödeme';
+$_['text_payment_address']     = 'Fatura Adresi';
+$_['text_shipping_address']    = 'Teslimat Adresi';
+$_['text_your_password']       = 'Your Password';
+$_['text_agree']               = 'I have read and agree to the <a href="%s" class="modal-link"><b>%s</b></a>';
+$_['text_success_add']         = 'Success: Your account has been created!';
+$_['text_success_guest']       = 'Success: Your guest account information has been saved!';
+$_['text_success_edit']        = 'Success: Your account has been successfully updated.';
+
+// Entry
+$_['entry_customer_group']     = 'Account Type';
+$_['entry_firstname']          = 'Ad';
+$_['entry_lastname']           = 'Soyad';
+$_['entry_email']              = 'E-Mail';
+$_['entry_telephone']          = 'Telephone';
+$_['entry_password']           = 'Şifre';
+$_['entry_company']            = 'Şirket';
+$_['entry_address_1']          = 'Adres 1';
+$_['entry_address_2']          = 'Adres 2';
+$_['entry_postcode']           = 'Post Code';
+$_['entry_city']               = 'Şehir';
+$_['entry_country']            = 'Ülke';
+$_['entry_zone']               = 'Region / State';
+$_['entry_match']              = 'My delivery and billing addresses are the same.';
+$_['entry_newsletter']         = 'I wish to subscribe to the %s newsletter.';
+
+// Error
+$_['error_guest']              = 'Warning: Items in your shopping cart require you to register for an account!';
+$_['error_firstname']          = 'First Name must be between 1 and 32 characters!';
+$_['error_lastname']           = 'Last Name must be between 1 and 32 characters!';
+$_['error_customer_group']     = 'Customer Group does not appear to be valid!';
+$_['error_customer_approval']  = 'Warning: Your this customer group requires approval and cannot be used with guest account.';
+$_['error_email']              = 'E-Mail address does not appear to be valid!';
+$_['error_exists']             = 'Warning: E-Mail Address is already registered!';
+$_['error_telephone']          = 'Telephone must be between 3 and 32 characters!';
+$_['error_password']           = 'Password must contain a %s and be between %d and 40 characters!';
+$_['error_password_uppercase'] = 'uppercase';
+$_['error_password_lowercase'] = 'lowercase';
+$_['error_password_number']    = 'number';
+$_['error_password_symbol']    = 'symbol';
+$_['error_password_length']    = 'Password must be between %d and 40 characters!';
+$_['error_address_1']          = 'Address 1 must be between 3 and 128 characters!';
+$_['error_city']               = 'City must be between 2 and 128 characters!';
+$_['error_postcode']           = 'Postcode must be between 2 and 10 characters!';
+$_['error_country']            = 'Please select a country!';
+$_['error_zone']               = 'Please select a region / state!';
+$_['error_agree']              = 'Uyarı: %s koşullarını kabul etmelisiniz!';
+$_['error_custom_field']       = '%s required!';
+$_['error_regex']              = '%s is not a valid input!';
