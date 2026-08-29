74d815eabced9e78a118b9a3e9c2166790c9bf8d
ercankoclar <ercankoclar@duck.com>
2026-08-29 17:57:16 +0300

    Upload files to "public_html/admin/language/tr-tr/extension"

---
+++ public_html/admin/language/tr-tr/extension/analytics.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']    = 'Analytics';
+
+// Text
+$_['text_success']     = 'Success: You have modified analytics!';
+$_['text_list']        = 'Analytics List';
+
+// Column
+$_['column_name']      = 'Analytics Name';
+$_['column_status']    = 'Durum';
+$_['column_action']    = 'İşlem';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify analytics!';
+$_['error_extension']  = 'Warning: Extension does not exist!';
---
+++ public_html/admin/language/tr-tr/extension/dashboard.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']     = 'Dashboard';
+
+// Text
+$_['text_success']      = 'Success: You have modified dashboards!';
+$_['text_list']         = 'Dashboard List';
+
+// Column
+$_['column_name']       = 'Dashboard Name';
+$_['column_width']      = 'Width';
+$_['column_status']     = 'Durum';
+$_['column_sort_order'] = 'Sıralama';
+$_['column_action']     = 'İşlem';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify dashboards!';
+$_['error_extension']   = 'Warning: Extension does not exist!';
