4ea6aed7d809a816b975fc5ee10f4b95e0592a74
ercankoclar <ercankoclar@duck.com>
2026-08-29 16:12:17 +0300

    Upload files to "public_html/extension/opencart/admin/language/tr-tr/report"

---
+++ public_html/extension/opencart/admin/language/tr-tr/report/sale_shipping.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']      = 'Shipping Report';
+
+// Text
+$_['text_extension']     = 'Eklentiler';
+$_['text_edit']          = 'Edit Shipping Report';
+$_['text_success']       = 'Success: You have modified shipping report!';
+$_['text_filter']        = 'Filtrele';
+$_['text_year']          = 'Years';
+$_['text_month']         = 'Months';
+$_['text_week']          = 'Weeks';
+$_['text_day']           = 'Days';
+$_['text_all_status']    = 'All Statuses';
+
+// Column
+$_['column_date_start']  = 'Başlangıç Tarihi';
+$_['column_date_end']    = 'Bitiş Tarihi';
+$_['column_title']       = 'Shipping Title';
+$_['column_orders']      = 'No. Orders';
+$_['column_total']       = 'Toplam';
+
+// Entry
+$_['entry_date_start']   = 'Başlangıç Tarihi';
+$_['entry_date_end']     = 'Bitiş Tarihi';
+$_['entry_group']        = 'Group By';
+$_['entry_order_status'] = 'Order Status';
+$_['entry_status']       = 'Durum';
+$_['entry_sort_order']   = 'Sıralama';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify shipping report!';
---
+++ public_html/extension/opencart/admin/language/tr-tr/report/subscription.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']             = 'Subscriptions Report';
+
+// Text
+$_['text_extension']            = 'Eklentiler';
+$_['text_edit']                 = 'Edit Subscriptions Report';
+$_['text_success']              = 'Success: You have modified subscriptions report!';
+$_['text_filter']               = 'Filtrele';
+$_['text_year']                 = 'Years';
+$_['text_month']                = 'Months';
+$_['text_week']                 = 'Weeks';
+$_['text_day']                  = 'Days';
+$_['text_all_status']           = 'All Statuses';
+
+// Column
+$_['column_date_start']         = 'Başlangıç Tarihi';
+$_['column_date_end']           = 'Bitiş Tarihi';
+$_['column_subscriptions']      = 'No. Subscriptions';
+$_['column_products']           = 'No. Products';
+$_['column_tax']                = 'Vergi';
+$_['column_total']              = 'Toplam';
+
+// Entry
+$_['entry_date_start']          = 'Başlangıç Tarihi';
+$_['entry_date_end']            = 'Bitiş Tarihi';
+$_['entry_group']               = 'Group By';
+$_['entry_subscription_status'] = 'Subscription Status';
+$_['entry_status']              = 'Durum';
+$_['entry_sort_order']          = 'Sıralama';
+
+// Error
+$_['error_permission']          = 'Warning: You do not have permission to modify subscriptions report!';
