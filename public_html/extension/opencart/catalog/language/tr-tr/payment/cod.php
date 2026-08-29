dcee77e77a6dc337af04376a01d430a620eeac97
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:59:45 +0300

    Update public_html/extension/opencart/catalog/language/tr-tr/payment/cod.php

--- public_html/extension/opencart/catalog/language/tr-tr/payment/cod.php
+++ public_html/extension/opencart/catalog/language/tr-tr/payment/cod.php
@@ -1,7 +1,7 @@
 <?php
 // Heading
-$_['heading_title']        = 'Cash On Delivery';
+$_['heading_title']        = 'Kapıda Ödeme';
 
 // Error
-$_['error_order_id']       = 'No order ID in the session!';
-$_['error_payment_method'] = 'Payment method is incorrect!';
+$_['error_order_id']       = 'Oturumda sipariş numarası bulunamadı!';
+$_['error_payment_method'] = 'Ödeme yöntemi hatalı!';
