1c2343b3a1d6eddb023f8c3d966326349b32ee9d
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:14:40 +0300

    Update public_html/admin/language/tr-tr/common/login.php

--- public_html/admin/language/tr-tr/common/login.php
+++ public_html/admin/language/tr-tr/common/login.php
@@ -1,18 +1,18 @@
 <?php
 // Heading
-$_['heading_title']  = 'Administration';
+$_['heading_title']  = 'Yönetim';
 
 // Text
-$_['text_login']     = 'Please enter your login details.';
+$_['text_login']     = 'Lütfen giriş bilgilerinizi girin.';
 $_['text_forgotten'] = 'Şifremi Unuttum';
 
 // Entry
-$_['entry_username'] = 'Username';
+$_['entry_username'] = 'Kullanıcı Adı';
 $_['entry_password'] = 'Şifre';
 
 // Button
 $_['button_login']   = 'Giriş Yap';
 
 // Error
-$_['error_login']    = 'No match for Username and/or Password.';
-$_['error_token']    = 'Invalid token session. Please login again.';
+$_['error_login']    = 'Kullanıcı Adı ve/veya Şifre eşleşmiyor.';
+$_['error_token']    = 'Geçersiz oturum belirteci. Lütfen tekrar giriş yapın.';
