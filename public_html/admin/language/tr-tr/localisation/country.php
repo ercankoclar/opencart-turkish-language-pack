f45327b8e0ec1ad9a7741043dcbfde23dfb8e9ac
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:29:31 +0300

    Update public_html/admin/language/tr-tr/localisation/country.php

--- public_html/admin/language/tr-tr/localisation/country.php
+++ public_html/admin/language/tr-tr/localisation/country.php
@@ -3,33 +3,33 @@
 $_['heading_title']           = 'Ülkeler';
 
 // Text
-$_['text_success']            = 'Success: You have modified countries!';
-$_['text_list']               = 'Country List';
-$_['text_add']                = 'Add Country';
-$_['text_edit']               = 'Edit Country';
+$_['text_success']            = 'Başarılı: Ülkeleri değiştirdiniz!';
+$_['text_list']               = 'Ülke Listesi';
+$_['text_add']                = 'Ülke Ekle';
+$_['text_edit']               = 'Ülkeyi Düzenle';
 $_['text_filter']             = 'Filtrele';
 
 // Column
-$_['column_name']             = 'Country Name';
-$_['column_iso_code_2']       = 'ISO Code (2)';
-$_['column_iso_code_3']       = 'ISO Code (3)';
+$_['column_name']             = 'Ülke Adı';
+$_['column_iso_code_2']       = 'ISO Kodu (2)';
+$_['column_iso_code_3']       = 'ISO Kodu (3)';
 $_['column_action']           = 'İşlem';
 
 // Entry
-$_['entry_name']              = 'Country Name';
-$_['entry_iso_code_2']        = 'ISO Code (2)';
-$_['entry_iso_code_3']        = 'ISO Code (3)';
-$_['entry_address_format']    = 'Address Format';
-$_['entry_postcode_required'] = 'Postcode Required';
+$_['entry_name']              = 'Ülke Adı';
+$_['entry_iso_code_2']        = 'ISO Kodu (2)';
+$_['entry_iso_code_3']        = 'ISO Kodu (3)';
+$_['entry_address_format']    = 'Adres Formatı';
+$_['entry_postcode_required'] = 'Posta Kodu Gerekli';
 $_['entry_status']            = 'Durum';
 
 // Error
-$_['error_permission']        = 'Warning: You do not have permission to modify countries!';
-$_['error_name']              = 'Country Name must be between 1 and 128 characters!';
-$_['error_iso_code_2']        = 'ISO Code 2 must be 2 characters!';
-$_['error_iso_code_3']        = 'ISO Code 3 must be between 3 characters!';
-$_['error_default']           = 'Warning: This country cannot be deleted as it is currently assigned as the default store country!';
-$_['error_store']             = 'Warning: This country cannot be deleted as it is currently assigned to %s stores!';
-$_['error_address']           = 'Warning: This country cannot be deleted as it is currently assigned to %s address book entries!';
-$_['error_zone']              = 'Warning: This country cannot be deleted as it is currently assigned to %s zones!';
-$_['error_zone_to_geo_zone']  = 'Warning: This country cannot be deleted as it is currently assigned to %s zones to geo zones!';
+$_['error_permission']        = 'Uyarı: Ülkeleri değiştirme yetkiniz yok!';
+$_['error_name']              = 'Ülke Adı 1 ile 128 karakter arasında olmalıdır!';
+$_['error_iso_code_2']        = 'ISO Kodu 2, 2 karakter olmalıdır!';
+$_['error_iso_code_3']        = 'ISO Kodu 3, 3 karakter olmalıdır!';
+$_['error_default']           = 'Uyarı: Bu ülke varsayılan mağaza ülkesi olarak atandığı için silinemez!';
+$_['error_store']             = 'Uyarı: Bu ülke %s mağazaya atanmış olduğu için silinemez!';
+$_['error_address']           = 'Uyarı: Bu ülke %s adres defteri kaydına atanmış olduğu için silinemez!';
+$_['error_zone']              = 'Uyarı: Bu ülke %s bölgeye atanmış olduğu için silinemez!';
+$_['error_zone_to_geo_zone']  = 'Uyarı: Bu ülke %s coğrafi bölgeye atanmış olduğu için silinemez!';
