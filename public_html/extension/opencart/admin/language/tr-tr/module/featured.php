df93a0039a47e21f2a0469de7486a40a8a8400f9
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:29:20 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/module/featured.php

--- public_html/extension/opencart/admin/language/tr-tr/module/featured.php
+++ public_html/extension/opencart/admin/language/tr-tr/module/featured.php
@@ -1,27 +1,27 @@
 <?php
 // Heading
-$_['heading_title']    = 'Featured';
+$_['heading_title']    = 'Öne Çıkanlar';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified featured module!';
-$_['text_edit']        = 'Edit Featured Module';
-$_['text_horizontal']  = 'Horizontal';
-$_['text_vertical']    = 'Vertical';
+$_['text_success']     = 'Başarılı: Öne çıkanlar modülünü değiştirdiniz!';
+$_['text_edit']        = 'Öne Çıkanlar Modülünü Düzenle';
+$_['text_horizontal']  = 'Yatay';
+$_['text_vertical']    = 'Dikey';
 
 // Entry
-$_['entry_name']       = 'Module Name';
+$_['entry_name']       = 'Modül Adı';
 $_['entry_product']    = 'Ürünler';
-$_['entry_axis']       = 'Axis';
-$_['entry_width']      = 'Image Width';
-$_['entry_height']     = 'Image Height';
+$_['entry_axis']       = 'Yön';
+$_['entry_width']      = 'Görsel Genişliği';
+$_['entry_height']     = 'Görsel Yüksekliği';
 $_['entry_status']     = 'Durum';
 
 // Help
-$_['help_product']     = '(Autocomplete)';
+$_['help_product']     = '(Otomatik Tamamlama)';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify featured module!';
-$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
-$_['error_width']      = 'Width required!';
-$_['error_height']     = 'Height required!';
+$_['error_permission'] = 'Uyarı: Öne çıkanlar modülünü değiştirme yetkiniz yok!';
+$_['error_name']       = 'Modül Adı 3 ile 64 karakter arasında olmalıdır!';
+$_['error_width']      = 'Genişlik gereklidir!';
+$_['error_height']     = 'Yükseklik gereklidir!';
