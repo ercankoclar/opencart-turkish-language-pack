afa3b4241be31b5d18df0e0547bf7997d4e527be
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:49:52 +0300

    Update public_html/admin/language/tr-tr/marketplace/promotion.php

--- public_html/admin/language/tr-tr/marketplace/promotion.php
+++ public_html/admin/language/tr-tr/marketplace/promotion.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_recommended'] = 'Recommended';
+$_['text_recommended'] = 'Önerilen';
 $_['text_install']     = 'Yükle';
 $_['text_uninstall']   = 'Kaldır';
 $_['text_delete']      = 'Sil';
