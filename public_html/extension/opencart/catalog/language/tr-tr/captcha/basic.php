e752044f2a085b9200265f35b4f01d7fdcf5e6c5
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:51:22 +0300

    Update public_html/extension/opencart/catalog/language/tr-tr/captcha/basic.php

--- public_html/extension/opencart/catalog/language/tr-tr/captcha/basic.php
+++ public_html/extension/opencart/catalog/language/tr-tr/captcha/basic.php
@@ -1,9 +1,9 @@
 <?php
 // Text
-$_['text_captcha']  = 'Captcha';
+$_['text_captcha']  = 'Doğrulama Kodu';
 
 // Entry
-$_['entry_captcha'] = 'Enter the code in the box below';
+$_['entry_captcha'] = 'Aşağıdaki kutuya kodu girin';
 
 // Error
-$_['error_captcha'] = 'Verification code does not match the image!';
+$_['error_captcha'] = 'Doğrulama kodu görseldeki kodla eşleşmiyor!';
