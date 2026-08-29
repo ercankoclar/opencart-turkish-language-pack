a30a453a5d8630ba604b787cea21506f5af27822
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:31:10 +0300

    Update public_html/admin/language/tr-tr/localisation/length_class.php

--- public_html/admin/language/tr-tr/localisation/length_class.php
+++ public_html/admin/language/tr-tr/localisation/length_class.php
@@ -3,28 +3,28 @@
 $_['heading_title']    = 'Uzunluk Sınıfları';
 
 // Text
-$_['text_success']     = 'Success: You have modified length classes!';
-$_['text_list']        = 'Length Class List';
-$_['text_add']         = 'Add Length Class';
-$_['text_edit']        = 'Edit Length Class';
+$_['text_success']     = 'Başarılı: Uzunluk sınıflarını değiştirdiniz!';
+$_['text_list']        = 'Uzunluk Sınıfı Listesi';
+$_['text_add']         = 'Uzunluk Sınıfı Ekle';
+$_['text_edit']        = 'Uzunluk Sınıfını Düzenle';
 
 // Column
-$_['column_title']     = 'Length Title';
-$_['column_unit']      = 'Length Unit';
+$_['column_title']     = 'Uzunluk Başlığı';
+$_['column_unit']      = 'Uzunluk Birimi';
 $_['column_value']     = 'Değer';
 $_['column_action']    = 'İşlem';
 
 // Entry
-$_['entry_title']      = 'Length Title';
-$_['entry_unit']       = 'Length Unit';
+$_['entry_title']      = 'Uzunluk Başlığı';
+$_['entry_unit']       = 'Uzunluk Birimi';
 $_['entry_value']      = 'Değer';
 
 // Help
-$_['help_value']       = 'The value of your default length in the current length unit. Set to 1 for your default length.';
+$_['help_value']       = 'Varsayılan uzunluğunuzun mevcut uzunluk birimindeki değeridir. Varsayılan uzunluğunuz için 1 olarak ayarlayın.';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify length classes!';
-$_['error_title']      = 'Length Title must be between 3 and 32 characters!';
-$_['error_unit']       = 'Length Unit must be between 1 and 4 characters!';
-$_['error_default']    = 'Warning: This length class cannot be deleted as it is currently assigned as the default store length class!';
-$_['error_product']    = 'Warning: This length class cannot be deleted as it is currently assigned to %s products!';
+$_['error_permission'] = 'Uyarı: Uzunluk sınıflarını değiştirme yetkiniz yok!';
+$_['error_title']      = 'Uzunluk Başlığı 3 ile 32 karakter arasında olmalıdır!';
+$_['error_unit']       = 'Uzunluk Birimi 1 ile 4 karakter arasında olmalıdır!';
+$_['error_default']    = 'Uyarı: Bu uzunluk sınıfı varsayılan mağaza uzunluk sınıfı olarak atandığı için silinemez!';
+$_['error_product']    = 'Uyarı: Bu uzunluk sınıfı %s ürüne atanmış olduğu için silinemez!';
