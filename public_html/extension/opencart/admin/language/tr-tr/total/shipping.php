f52f5f5f2787fd0c13b9242330bc6c10ae086659
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:49:22 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/total/shipping.php

--- public_html/extension/opencart/admin/language/tr-tr/total/shipping.php
+++ public_html/extension/opencart/admin/language/tr-tr/total/shipping.php
@@ -4,13 +4,13 @@ $_['heading_title']    = 'Kargo';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified shipping total!';
-$_['text_edit']        = 'Edit Shipping Total';
+$_['text_success']     = 'Başarılı: Kargo toplamını değiştirdiniz!';
+$_['text_edit']        = 'Kargo Toplamını Düzenle';
 
 // Entry
-$_['entry_estimator']  = 'Shipping Estimator';
+$_['entry_estimator']  = 'Kargo Hesaplayıcı';
 $_['entry_status']     = 'Durum';
 $_['entry_sort_order'] = 'Sıralama';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify shipping total!';
+$_['error_permission'] = 'Uyarı: Kargo toplamını değiştirme yetkiniz yok!';
