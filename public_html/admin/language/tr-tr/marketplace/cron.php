eb9f96dc0f2f18b48c2e74cd5fb1851dd2a0b46a
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:47:27 +0300

    Update public_html/admin/language/tr-tr/marketplace/cron.php

--- public_html/admin/language/tr-tr/marketplace/cron.php
+++ public_html/admin/language/tr-tr/marketplace/cron.php
@@ -1,21 +1,21 @@
 <?php
 // Heading
-$_['heading_title']        = 'CRON Jobs';
+$_['heading_title']        = 'CRON Görevleri';
 
 // Text
-$_['text_success']         = 'Success: You have modified CRON jobs!';
-$_['text_instruction']     = 'CRON Instructions';
-$_['text_list']            = 'CRON List';
-$_['text_cron_1']          = 'CRON Job\'s are scheduled tasks that are run periodically. To setup your servers to use cron job you can read the <a href="https://docs.opencart.com/developer-guide/cron-jobs" target="_blank" class="alert-link">opencart documentation</a> page.';
-$_['text_cron_2']          = 'You need to set your CRON task to run every hour.';
-$_['text_info']            = 'CRON Information';
-$_['text_hour']            = 'Hour';
-$_['text_day']             = 'Day';
-$_['text_month']           = 'Month';
+$_['text_success']         = 'Başarılı: CRON görevlerini değiştirdiniz!';
+$_['text_instruction']     = 'CRON Talimatları';
+$_['text_list']            = 'CRON Listesi';
+$_['text_cron_1']          = 'CRON görevleri, belirli aralıklarla çalıştırılan zamanlanmış görevlerdir. Sunucunuzu CRON görevlerini kullanacak şekilde yapılandırmak için <a href="https://docs.opencart.com/developer-guide/cron-jobs" target="_blank" class="alert-link">OpenCart dokümantasyon</a> sayfasını inceleyebilirsiniz.';
+$_['text_cron_2']          = 'CRON görevinizi her saat çalışacak şekilde ayarlamanız gerekir.';
+$_['text_info']            = 'CRON Bilgileri';
+$_['text_hour']            = 'Saat';
+$_['text_day']             = 'Gün';
+$_['text_month']           = 'Ay';
 
 // Column
-$_['column_code']          = 'CRON Code';
-$_['column_cycle']         = 'Cycle';
+$_['column_code']          = 'CRON Kodu';
+$_['column_cycle']         = 'Döngü';
 $_['column_date_added']    = 'Eklenme Tarihi';
 $_['column_date_modified'] = 'Değiştirilme Tarihi';
 $_['column_action']        = 'İşlem';
@@ -25,4 +25,4 @@ $_['entry_cron']           = 'CRON URL';
 $_['entry_description']    = 'Açıklama';
 
 // Error
-$_['error_permission']     = 'Warning: You do not have permission to modify CRON jobs!';
+$_['error_permission']     = 'Uyarı: CRON görevlerini değiştirme yetkiniz yok!';
