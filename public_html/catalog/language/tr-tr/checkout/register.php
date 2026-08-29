1467ce6dd2216b46489006479c926ed91710aabd
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:07:21 +0300

    Update public_html/catalog/language/tr-tr/checkout/register.php

--- public_html/catalog/language/tr-tr/checkout/register.php
+++ public_html/catalog/language/tr-tr/checkout/register.php
@@ -1,56 +1,56 @@
 <?php
 // Heading
-$_['heading_title']            = 'Your Personal Details';
+$_['heading_title']            = 'Kişisel Bilgileriniz';
 
 // Text
-$_['text_login']               = 'If you already have an account with us, please login at the <a href="%s"><strong>login page</strong></a>.';
+$_['text_login']               = 'Bizde zaten bir hesabınız varsa lütfen <a href="%s"><strong>giriş sayfasından</strong></a> giriş yapın.';
 $_['text_register']            = 'Hesap Oluştur';
 $_['text_guest']               = 'Üyeliksiz Ödeme';
 $_['text_payment_address']     = 'Fatura Adresi';
 $_['text_shipping_address']    = 'Teslimat Adresi';
-$_['text_your_password']       = 'Your Password';
-$_['text_agree']               = 'I have read and agree to the <a href="%s" class="modal-link"><b>%s</b></a>';
-$_['text_success_add']         = 'Success: Your account has been created!';
-$_['text_success_guest']       = 'Success: Your guest account information has been saved!';
-$_['text_success_edit']        = 'Success: Your account has been successfully updated.';
+$_['text_your_password']       = 'Şifreniz';
+$_['text_agree']               = '<a href="%s" class="modal-link"><b>%s</b></a> koşullarını okudum ve kabul ediyorum';
+$_['text_success_add']         = 'Başarılı: Hesabınız oluşturuldu!';
+$_['text_success_guest']       = 'Başarılı: Üyeliksiz hesap bilgileriniz kaydedildi!';
+$_['text_success_edit']        = 'Başarılı: Hesabınız başarıyla güncellendi.';
 
 // Entry
-$_['entry_customer_group']     = 'Account Type';
+$_['entry_customer_group']     = 'Hesap Türü';
 $_['entry_firstname']          = 'Ad';
 $_['entry_lastname']           = 'Soyad';
-$_['entry_email']              = 'E-Mail';
-$_['entry_telephone']          = 'Telephone';
+$_['entry_email']              = 'E-posta';
+$_['entry_telephone']          = 'Telefon';
 $_['entry_password']           = 'Şifre';
 $_['entry_company']            = 'Şirket';
 $_['entry_address_1']          = 'Adres 1';
 $_['entry_address_2']          = 'Adres 2';
-$_['entry_postcode']           = 'Post Code';
+$_['entry_postcode']           = 'Posta Kodu';
 $_['entry_city']               = 'Şehir';
 $_['entry_country']            = 'Ülke';
-$_['entry_zone']               = 'Region / State';
-$_['entry_match']              = 'My delivery and billing addresses are the same.';
-$_['entry_newsletter']         = 'I wish to subscribe to the %s newsletter.';
+$_['entry_zone']               = 'Bölge / İl';
+$_['entry_match']              = 'Teslimat ve fatura adreslerim aynı.';
+$_['entry_newsletter']         = '%s bültenine abone olmak istiyorum.';
 
 // Error
-$_['error_guest']              = 'Warning: Items in your shopping cart require you to register for an account!';
-$_['error_firstname']          = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']           = 'Last Name must be between 1 and 32 characters!';
-$_['error_customer_group']     = 'Customer Group does not appear to be valid!';
-$_['error_customer_approval']  = 'Warning: Your this customer group requires approval and cannot be used with guest account.';
-$_['error_email']              = 'E-Mail address does not appear to be valid!';
-$_['error_exists']             = 'Warning: E-Mail Address is already registered!';
-$_['error_telephone']          = 'Telephone must be between 3 and 32 characters!';
-$_['error_password']           = 'Password must contain a %s and be between %d and 40 characters!';
-$_['error_password_uppercase'] = 'uppercase';
-$_['error_password_lowercase'] = 'lowercase';
-$_['error_password_number']    = 'number';
-$_['error_password_symbol']    = 'symbol';
-$_['error_password_length']    = 'Password must be between %d and 40 characters!';
-$_['error_address_1']          = 'Address 1 must be between 3 and 128 characters!';
-$_['error_city']               = 'City must be between 2 and 128 characters!';
-$_['error_postcode']           = 'Postcode must be between 2 and 10 characters!';
-$_['error_country']            = 'Please select a country!';
-$_['error_zone']               = 'Please select a region / state!';
+$_['error_guest']              = 'Uyarı: Alışveriş sepetinizdeki ürünler hesap oluşturmanızı gerektiriyor!';
+$_['error_firstname']          = 'Ad 1 ile 32 karakter arasında olmalıdır!';
+$_['error_lastname']           = 'Soyad 1 ile 32 karakter arasında olmalıdır!';
+$_['error_customer_group']     = 'Müşteri Grubu geçerli görünmüyor!';
+$_['error_customer_approval']  = 'Uyarı: Bu müşteri grubu onay gerektiriyor ve üyeliksiz hesapla kullanılamaz.';
+$_['error_email']              = 'E-posta adresi geçerli görünmüyor!';
+$_['error_exists']             = 'Uyarı: E-posta Adresi zaten kayıtlı!';
+$_['error_telephone']          = 'Telefon 3 ile 32 karakter arasında olmalıdır!';
+$_['error_password']           = 'Şifre bir %s içermeli ve %d ile 40 karakter arasında olmalıdır!';
+$_['error_password_uppercase'] = 'büyük harf';
+$_['error_password_lowercase'] = 'küçük harf';
+$_['error_password_number']    = 'rakam';
+$_['error_password_symbol']    = 'sembol';
+$_['error_password_length']    = 'Şifre %d ile 40 karakter arasında olmalıdır!';
+$_['error_address_1']          = 'Adres 1, 3 ile 128 karakter arasında olmalıdır!';
+$_['error_city']               = 'Şehir 2 ile 128 karakter arasında olmalıdır!';
+$_['error_postcode']           = 'Posta Kodu 2 ile 10 karakter arasında olmalıdır!';
+$_['error_country']            = 'Lütfen bir ülke seçin!';
+$_['error_zone']               = 'Lütfen bir bölge / il seçin!';
 $_['error_agree']              = 'Uyarı: %s koşullarını kabul etmelisiniz!';
-$_['error_custom_field']       = '%s required!';
-$_['error_regex']              = '%s is not a valid input!';
+$_['error_custom_field']       = '%s gereklidir!';
+$_['error_regex']              = '%s geçerli bir giriş değil!';
