3df1297a6ba040af429e5ed992a5a6c0943d0962
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:42:27 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/report/subscription.php

--- public_html/extension/opencart/admin/language/tr-tr/report/subscription.php
+++ public_html/extension/opencart/admin/language/tr-tr/report/subscription.php
@@ -1,33 +1,33 @@
 <?php
 // Heading
-$_['heading_title']             = 'Subscriptions Report';
+$_['heading_title']             = 'Abonelikler Raporu';
 
 // Text
 $_['text_extension']            = 'Eklentiler';
-$_['text_edit']                 = 'Edit Subscriptions Report';
-$_['text_success']              = 'Success: You have modified subscriptions report!';
+$_['text_edit']                 = 'Abonelikler Raporunu Düzenle';
+$_['text_success']              = 'Başarılı: Abonelikler raporunu değiştirdiniz!';
 $_['text_filter']               = 'Filtrele';
-$_['text_year']                 = 'Years';
-$_['text_month']                = 'Months';
-$_['text_week']                 = 'Weeks';
-$_['text_day']                  = 'Days';
-$_['text_all_status']           = 'All Statuses';
+$_['text_year']                 = 'Yıllar';
+$_['text_month']                = 'Aylar';
+$_['text_week']                 = 'Haftalar';
+$_['text_day']                  = 'Günler';
+$_['text_all_status']           = 'Tüm Durumlar';
 
 // Column
 $_['column_date_start']         = 'Başlangıç Tarihi';
 $_['column_date_end']           = 'Bitiş Tarihi';
-$_['column_subscriptions']      = 'No. Subscriptions';
-$_['column_products']           = 'No. Products';
+$_['column_subscriptions']      = 'Abonelik Sayısı';
+$_['column_products']           = 'Ürün Sayısı';
 $_['column_tax']                = 'Vergi';
 $_['column_total']              = 'Toplam';
 
 // Entry
 $_['entry_date_start']          = 'Başlangıç Tarihi';
 $_['entry_date_end']            = 'Bitiş Tarihi';
-$_['entry_group']               = 'Group By';
-$_['entry_subscription_status'] = 'Subscription Status';
+$_['entry_group']               = 'Gruplama';
+$_['entry_subscription_status'] = 'Abonelik Durumu';
 $_['entry_status']              = 'Durum';
 $_['entry_sort_order']          = 'Sıralama';
 
 // Error
-$_['error_permission']          = 'Warning: You do not have permission to modify subscriptions report!';
+$_['error_permission']          = 'Uyarı: Abonelikler raporunu değiştirme yetkiniz yok!';
