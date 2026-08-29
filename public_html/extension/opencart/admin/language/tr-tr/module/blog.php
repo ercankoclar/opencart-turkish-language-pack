ee8a8c7d7eeb23ed453aec2fdcfd00e41f80a509
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:28:32 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/module/blog.php

--- public_html/extension/opencart/admin/language/tr-tr/module/blog.php
+++ public_html/extension/opencart/admin/language/tr-tr/module/blog.php
@@ -4,13 +4,13 @@ $_['heading_title']    = 'Blog';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified blog module!';
-$_['text_next']        = 'Success: You have modified %s blog out of %s!';
-$_['text_edit']        = 'Edit Blog Module';
+$_['text_success']     = 'Başarılı: Blog modülünü değiştirdiniz!';
+$_['text_next']        = 'Başarılı: %s / %s blog yazısı işlendi!';
+$_['text_edit']        = 'Blog Modülünü Düzenle';
 
 // Form
-$_['text_desc']        = 'Descending Order';
-$_['text_asc']         = 'Ascending Order';
+$_['text_desc']        = 'Azalan Sıralama';
+$_['text_asc']         = 'Artan Sıralama';
 $_['text_sort_name']   = 'Ad';
 $_['text_sort_date']   = 'Eklenme Tarihi';
 
@@ -18,16 +18,16 @@ $_['text_sort_date']   = 'Eklenme Tarihi';
 $_['column_total']     = 'Toplam';
 
 // Entry
-$_['entry_name']       = 'Module Name';
+$_['entry_name']       = 'Modül Adı';
 $_['entry_limit']      = 'Limit';
-$_['entry_width']      = 'Image Width';
-$_['entry_height']     = 'Image Height';
-$_['entry_order']      = 'Sipariş';
-$_['entry_sort']   	   = 'Sort';
+$_['entry_width']      = 'Görsel Genişliği';
+$_['entry_height']     = 'Görsel Yüksekliği';
+$_['entry_order']      = 'Sıralama';
+$_['entry_sort']       = 'Sıralama';
 $_['entry_status']     = 'Durum';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify blog module!';
-$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
-$_['error_width']      = 'Width required!';
-$_['error_height']     = 'Height required!';
+$_['error_permission'] = 'Uyarı: Blog modülünü değiştirme yetkiniz yok!';
+$_['error_name']       = 'Modül Adı 3 ile 64 karakter arasında olmalıdır!';
+$_['error_width']      = 'Genişlik gereklidir!';
+$_['error_height']     = 'Yükseklik gereklidir!';
