0138fdb6f9e1a5ef6d2f24edd984945642e34d18
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:31:00 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/module/latest.php

--- public_html/extension/opencart/admin/language/tr-tr/module/latest.php
+++ public_html/extension/opencart/admin/language/tr-tr/module/latest.php
@@ -1,24 +1,24 @@
 <?php
 // Heading
-$_['heading_title']    = 'Latest';
+$_['heading_title']    = 'Son Eklenenler';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified latest module!';
-$_['text_edit']        = 'Edit Latest Module';
-$_['text_horizontal']  = 'Horizontal';
-$_['text_vertical']    = 'Vertical';
+$_['text_success']     = 'Başarılı: Son Eklenenler modülünü değiştirdiniz!';
+$_['text_edit']        = 'Son Eklenenler Modülünü Düzenle';
+$_['text_horizontal']  = 'Yatay';
+$_['text_vertical']    = 'Dikey';
 
 // Entry
-$_['entry_name']       = 'Module Name';
-$_['entry_axis']       = 'Axis';
+$_['entry_name']       = 'Modül Adı';
+$_['entry_axis']       = 'Yön';
 $_['entry_limit']      = 'Limit';
-$_['entry_width']      = 'Image Width';
-$_['entry_height']     = 'Image Height';
+$_['entry_width']      = 'Görsel Genişliği';
+$_['entry_height']     = 'Görsel Yüksekliği';
 $_['entry_status']     = 'Durum';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify latest module!';
-$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
-$_['error_width']      = 'Width required!';
-$_['error_height']     = 'Height required!';
+$_['error_permission'] = 'Uyarı: Son Eklenenler modülünü değiştirme yetkiniz yok!';
+$_['error_name']       = 'Modül Adı 3 ile 64 karakter arasında olmalıdır!';
+$_['error_width']      = 'Genişlik gereklidir!';
+$_['error_height']     = 'Yükseklik gereklidir!';
