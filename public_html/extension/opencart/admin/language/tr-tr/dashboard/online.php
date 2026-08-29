9879d0176f5e0d705155eb1188448b5653b618e8
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:25:04 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/dashboard/online.php

--- public_html/extension/opencart/admin/language/tr-tr/dashboard/online.php
+++ public_html/extension/opencart/admin/language/tr-tr/dashboard/online.php
@@ -1,17 +1,17 @@
 <?php
 // Heading
-$_['heading_title']    = 'People Online';
+$_['heading_title']    = 'Çevrimiçi Ziyaretçiler';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified dashboard online!';
-$_['text_edit']        = 'Edit Dashboard Online';
-$_['text_view']        = 'View more...';
+$_['text_success']     = 'Başarılı: Kontrol paneli çevrimiçi bölümünü değiştirdiniz!';
+$_['text_edit']        = 'Kontrol Paneli Çevrimiçi Bölümünü Düzenle';
+$_['text_view']        = 'Daha fazlasını görüntüle...';
 
 // Entry
 $_['entry_status']     = 'Durum';
-$_['entry_sort_order'] = 'Sort Order';
-$_['entry_width']      = 'Width';
+$_['entry_sort_order'] = 'Sıralama';
+$_['entry_width']      = 'Genişlik';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify dashboard online!';
+$_['error_permission'] = 'Uyarı: Kontrol paneli çevrimiçi bölümünü değiştirme yetkiniz yok!';
