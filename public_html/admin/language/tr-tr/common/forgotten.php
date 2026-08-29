3f5f28dde5f7c068a310856c3763eb034a9567c8
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:13:11 +0300

    Update public_html/admin/language/tr-tr/common/forgotten.php

--- public_html/admin/language/tr-tr/common/forgotten.php
+++ public_html/admin/language/tr-tr/common/forgotten.php
@@ -1,26 +1,26 @@
 <?php
 // Heading
-$_['heading_title']            = 'Forgot Your Password?';
-$_['heading_reset']            = 'Reset your password';
+$_['heading_title']            = 'Şifrenizi mi Unuttunuz?';
+$_['heading_reset']            = 'Şifrenizi sıfırlayın';
 
 // Text
-$_['text_email']               = 'Enter the e-mail address associated with your account. Click submit to have a password reset link e-mailed to you.';
-$_['text_success']             = 'An email with a confirmation link has been sent your admin email address.';
-$_['text_password']            = 'Enter the new password you wish to use.';
-$_['text_reset']               = 'Success: Your password has been successfully updated.';
+$_['text_email']               = 'Hesabınızla ilişkili e-posta adresini girin. Şifre sıfırlama bağlantısının e-posta ile gönderilmesi için gönder düğmesine tıklayın.';
+$_['text_success']             = 'Onay bağlantısını içeren bir e-posta yönetici e-posta adresinize gönderildi.';
+$_['text_password']            = 'Kullanmak istediğiniz yeni şifreyi girin.';
+$_['text_reset']               = 'Başarılı: Şifreniz başarıyla güncellendi.';
 
 // Entry
 $_['entry_password']           = 'Şifre';
-$_['entry_confirm']            = 'Confirm';
-$_['entry_email']              = 'E-Mail Address';
+$_['entry_confirm']            = 'Onayla';
+$_['entry_email']              = 'E-posta Adresi';
 
 // Error
-$_['error_email']              = 'Warning: The E-Mail Address was not found in our records!';
-$_['error_code']               = 'Reset code does not match!';
-$_['error_password']           = 'Password must contain %s and be between %d and 20 characters!';
-$_['error_password_uppercase'] = 'uppercase';
-$_['error_password_lowercase'] = 'lowercase';
-$_['error_password_number']    = 'number';
-$_['error_password_symbol']    = 'symbol';
-$_['error_password_length']    = 'Password must be between %d and 40 characters!';
-$_['error_confirm']            = 'Password and password confirmation do not match!';
+$_['error_email']              = 'Uyarı: E-posta Adresi kayıtlarımızda bulunamadı!';
+$_['error_code']               = 'Sıfırlama kodu eşleşmiyor!';
+$_['error_password']           = 'Şifre %s içermeli ve %d ile 20 karakter arasında olmalıdır!';
+$_['error_password_uppercase'] = 'büyük harf';
+$_['error_password_lowercase'] = 'küçük harf';
+$_['error_password_number']    = 'sayı';
+$_['error_password_symbol']    = 'sembol';
+$_['error_password_length']    = 'Şifre %d ile 40 karakter arasında olmalıdır!';
+$_['error_confirm']            = 'Şifre ve şifre onayı eşleşmiyor!';
