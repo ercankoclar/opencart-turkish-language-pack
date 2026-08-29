6e274987efe4989d5edd62bc13640a9a6653dba2
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:23:53 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/dashboard/chart.php

--- public_html/extension/opencart/admin/language/tr-tr/dashboard/chart.php
+++ public_html/extension/opencart/admin/language/tr-tr/dashboard/chart.php
@@ -1,22 +1,22 @@
 <?php
 // Heading
-$_['heading_title']    = 'Sales Analytics';
+$_['heading_title']    = 'Satış Analizi';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified dashboard chart!';
-$_['text_edit']        = 'Edit Dashboard Chart';
-$_['text_order']       = 'Orders';
-$_['text_customer']    = 'Customers';
-$_['text_day']         = 'Today';
-$_['text_week']        = 'Week';
-$_['text_month']       = 'Month';
-$_['text_year']        = 'Year';
+$_['text_success']     = 'Başarılı: Kontrol paneli grafiğini değiştirdiniz!';
+$_['text_edit']        = 'Kontrol Paneli Grafiğini Düzenle';
+$_['text_order']       = 'Siparişler';
+$_['text_customer']    = 'Müşteriler';
+$_['text_day']         = 'Bugün';
+$_['text_week']        = 'Hafta';
+$_['text_month']       = 'Ay';
+$_['text_year']        = 'Yıl';
 
 // Entry
 $_['entry_status']     = 'Durum';
-$_['entry_sort_order'] = 'Sort Order';
-$_['entry_width']      = 'Width';
+$_['entry_sort_order'] = 'Sıralama';
+$_['entry_width']      = 'Genişlik';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify dashboard chart!';
+$_['error_permission'] = 'Uyarı: Kontrol paneli grafiğini değiştirme yetkiniz yok!';
