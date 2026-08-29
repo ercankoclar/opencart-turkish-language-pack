1cf7ba2377f3dc8ca278ecbb14e58bea11e944fc
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:35:33 +0300

    Update public_html/admin/language/tr-tr/localisation/tax_rate.php

--- public_html/admin/language/tr-tr/localisation/tax_rate.php
+++ public_html/admin/language/tr-tr/localisation/tax_rate.php
@@ -3,29 +3,29 @@
 $_['heading_title']        = 'Vergi Oranları';
 
 // Text
-$_['text_success']         = 'Success: You have modified tax rates!';
-$_['text_list']            = 'Tax Rate List';
-$_['text_add']             = 'Add Tax Rate';
-$_['text_edit']            = 'Edit Tax Rate';
-$_['text_percent']         = 'Percentage';
-$_['text_amount']          = 'Fixed Amount';
+$_['text_success']         = 'Başarılı: Vergi oranlarını değiştirdiniz!';
+$_['text_list']            = 'Vergi Oranı Listesi';
+$_['text_add']             = 'Vergi Oranı Ekle';
+$_['text_edit']            = 'Vergi Oranını Düzenle';
+$_['text_percent']         = 'Yüzde';
+$_['text_amount']          = 'Sabit Tutar';
 
 // Column
-$_['column_name']          = 'Tax Name';
-$_['column_rate']          = 'Tax Rate';
-$_['column_type']          = 'Type';
+$_['column_name']          = 'Vergi Adı';
+$_['column_rate']          = 'Vergi Oranı';
+$_['column_type']          = 'Tür';
 $_['column_geo_zone']      = 'Coğrafi Bölge';
 $_['column_action']        = 'İşlem';
 
 // Entry
-$_['entry_name']           = 'Tax Name';
-$_['entry_rate']           = 'Tax Rate';
-$_['entry_type']           = 'Type';
+$_['entry_name']           = 'Vergi Adı';
+$_['entry_rate']           = 'Vergi Oranı';
+$_['entry_type']           = 'Tür';
 $_['entry_customer_group'] = 'Müşteri Grubu';
 $_['entry_geo_zone']       = 'Coğrafi Bölge';
 
 // Error
-$_['error_permission']     = 'Warning: You do not have permission to modify tax rates!';
-$_['error_tax_rule']       = 'Warning: This tax rate cannot be deleted as it is currently assigned to %s tax classes!';
-$_['error_name']           = 'Tax Name must be between 3 and 32 characters!';
-$_['error_rate']           = 'Tax Rate required!';
+$_['error_permission']     = 'Uyarı: Vergi oranlarını değiştirme yetkiniz yok!';
+$_['error_tax_rule']       = 'Uyarı: Bu vergi oranı %s vergi sınıfına atanmış olduğu için silinemez!';
+$_['error_name']           = 'Vergi Adı 3 ile 32 karakter arasında olmalıdır!';
+$_['error_rate']           = 'Vergi Oranı gereklidir!';
