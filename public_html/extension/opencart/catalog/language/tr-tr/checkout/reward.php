93de3d90a4428cc368812f7f001240a665ff76c3
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:52:10 +0300

    Update public_html/extension/opencart/catalog/language/tr-tr/checkout/reward.php

--- public_html/extension/opencart/catalog/language/tr-tr/checkout/reward.php
+++ public_html/extension/opencart/catalog/language/tr-tr/checkout/reward.php
@@ -1,15 +1,15 @@
 <?php
 // Heading
-$_['heading_title'] = 'Use Reward Points (Available %s)';
+$_['heading_title'] = 'Ödül Puanlarını Kullan (Mevcut: %s)';
 
 // Text
-$_['text_success']  = 'Success: Your reward points discount has been applied!';
+$_['text_success']  = 'Başarılı: Ödül puanı indiriminiz uygulandı!';
 
 // Entry
-$_['entry_reward']  = 'Points to use (Max %s)';
+$_['entry_reward']  = 'Kullanılacak Puan (Maks. %s)';
 
 // Error
-$_['error_reward']  = 'Warning: Please enter the amount of reward points to use!';
-$_['error_points']  = 'Warning: You don\'t have %s reward points!';
-$_['error_maximum'] = 'Warning: The maximum number of points that can be applied is %s!';
-$_['error_status']  = 'Warning: Reward points are not enabled on this store!';
+$_['error_reward']  = 'Uyarı: Lütfen kullanmak istediğiniz ödül puanı miktarını girin!';
+$_['error_points']  = 'Uyarı: %s ödül puanınız bulunmuyor!';
+$_['error_maximum'] = 'Uyarı: Uygulanabilecek maksimum puan %s!';
+$_['error_status']  = 'Uyarı: Bu mağazada ödül puanları etkin değil!';
