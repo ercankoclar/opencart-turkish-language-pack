064bb38d4b6ddb2e60c2e8f0458bcb1c5e19e038
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:37:19 +0300

    Update public_html/admin/language/tr-tr/mail/affiliate_approve.php

--- public_html/admin/language/tr-tr/mail/affiliate_approve.php
+++ public_html/admin/language/tr-tr/mail/affiliate_approve.php
@@ -1,7 +1,7 @@
 <?php
 // Text
-$_['text_subject']  = '%s - Your affiliate account has been activated!';
-$_['text_welcome']  = 'Welcome and thank you for registering at %s!';
-$_['text_login']    = 'Your account has now been approved and you can log in by using your email address and password by visiting our website or at the following URL:';
-$_['text_service']  = 'Upon logging in, you will be able to generate tracking codes, track commission payments and edit your account information.';
-$_['text_thanks']   = 'Thanks,';
+$_['text_subject']  = '%s - İş ortağı hesabınız etkinleştirildi!';
+$_['text_welcome']  = 'Hoş geldiniz ve %s sitesine kayıt olduğunuz için teşekkür ederiz!';
+$_['text_login']    = 'Hesabınız artık onaylandı. E-posta adresinizi ve şifrenizi kullanarak web sitemizi ziyaret ederek veya aşağıdaki URL üzerinden giriş yapabilirsiniz:';
+$_['text_service']  = 'Giriş yaptıktan sonra takip kodları oluşturabilir, komisyon ödemelerini takip edebilir ve hesap bilgilerinizi düzenleyebilirsiniz.';
+$_['text_thanks']   = 'Teşekkürler,';
