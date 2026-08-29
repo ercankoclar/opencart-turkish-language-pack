04684088daf1609bd4a4c2a058cd88c4b8ba6add
ercankoclar <ercankoclar@duck.com>
2026-08-29 15:33:58 +0300

    Upload files to "public_html/catalog/language/tr-tr/account"

---
+++ public_html/catalog/language/tr-tr/account/wishlist.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']   = 'İstek Listem';
+
+// Text
+$_['text_account']    = 'Hesap';
+$_['text_wishlist']   = 'İstek Listesi (%s)';
+$_['text_login']      = '<a href="%s">Giriş yapmalı</a> veya <a href="%s">hesap oluşturmalısınız</a> ki <a href="%s">%s</a> ürününü <a href="%s">istek listenize</a> kaydedebilesiniz!';
+$_['text_success']    = 'Başarılı: <a href="%s">%s</a> ürününü <a href="%s">istek listenize</a> eklediniz!';
+$_['text_remove']     = 'Başarılı: İstek listenizden bir ürün kaldırıldı';
+$_['text_no_results'] = 'İstek listeniz boş.';
+
+// Column
+$_['column_image']    = 'Görsel';
+$_['column_name']     = 'Ürün Adı';
+$_['column_model']    = 'Model';
+$_['column_stock']    = 'Stok';
+$_['column_price']    = 'Birim Fiyat';
+$_['column_action']   = 'İşlem';
+
+// Error
+$_['error_product']   = 'Uyarı: Ürün bulunamadı!';
+$_['error_login']     = 'Uyarı: İstek listenizden ürün kaldırmak için <a href="%s">giriş yapmalı</a> veya <a href="%s">hesap oluşturmalısınız</a>!';
