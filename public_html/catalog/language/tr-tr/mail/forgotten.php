55b9bfeacff8d757396d2666a80191f10d3e1e23
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:15:55 +0300

    Update public_html/catalog/language/tr-tr/mail/forgotten.php

--- public_html/catalog/language/tr-tr/mail/forgotten.php
+++ public_html/catalog/language/tr-tr/mail/forgotten.php
@@ -1,9 +1,9 @@
 <?php
 // Text
-$_['text_subject']  = '%s - Password reset request';
-$_['text_greeting'] = 'A new password was requested for %s customer account.';
-$_['text_change']   = 'To reset your password click on the link below:';
-$_['text_ip']       = 'The IP used to make this request was:';
+$_['text_subject']  = '%s - Şifre sıfırlama talebi';
+$_['text_greeting'] = '%s müşteri hesabı için yeni bir şifre talep edildi.';
+$_['text_change']   = 'Şifrenizi sıfırlamak için aşağıdaki bağlantıya tıklayın:';
+$_['text_ip']       = 'Bu talebin gönderildiği IP adresi:';
 
 // Button
-$_['button_reset']  = 'Reset Password';
+$_['button_reset']  = 'Şifreyi Sıfırla';
