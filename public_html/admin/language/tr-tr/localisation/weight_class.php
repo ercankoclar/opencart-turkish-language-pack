d5bec765bea5314a7890d48f3911cfe920ec1c66
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:35:58 +0300

    Update public_html/admin/language/tr-tr/localisation/weight_class.php

--- public_html/admin/language/tr-tr/localisation/weight_class.php
+++ public_html/admin/language/tr-tr/localisation/weight_class.php
@@ -3,28 +3,28 @@
 $_['heading_title']    = 'Ağırlık Sınıfları';
 
 // Text
-$_['text_success']     = 'Success: You have modified weight classes!';
-$_['text_list']        = 'Weight Class List';
-$_['text_add']         = 'Add Weight Class';
-$_['text_edit']        = 'Edit Weight Class';
+$_['text_success']     = 'Başarılı: Ağırlık sınıflarını değiştirdiniz!';
+$_['text_list']        = 'Ağırlık Sınıfı Listesi';
+$_['text_add']         = 'Ağırlık Sınıfı Ekle';
+$_['text_edit']        = 'Ağırlık Sınıfını Düzenle';
 
 // Column
-$_['column_title']     = 'Weight Title';
-$_['column_unit']      = 'Weight Unit';
+$_['column_title']     = 'Ağırlık Başlığı';
+$_['column_unit']      = 'Ağırlık Birimi';
 $_['column_value']     = 'Değer';
 $_['column_action']    = 'İşlem';
 
 // Entry
-$_['entry_title']      = 'Weight Title';
-$_['entry_unit']       = 'Weight Unit';
+$_['entry_title']      = 'Ağırlık Başlığı';
+$_['entry_unit']       = 'Ağırlık Birimi';
 $_['entry_value']      = 'Değer';
 
 // Help
-$_['help_value']       = 'The value of your default weight in the current weight unit. Set to 1 for your default weight.';
+$_['help_value']       = 'Varsayılan ağırlığınızın mevcut ağırlık birimindeki değeridir. Varsayılan ağırlığınız için 1 olarak ayarlayın.';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify weight classes!';
-$_['error_title']      = 'Weight Title must be between 3 and 32 characters!';
-$_['error_unit']       = 'Weight Unit must be between 1 and 4 characters!';
-$_['error_default']    = 'Warning: This weight class cannot be deleted as it is currently assigned as the default store weight class!';
-$_['error_product']    = 'Warning: This weight class cannot be deleted as it is currently assigned to %s products!';
+$_['error_permission'] = 'Uyarı: Ağırlık sınıflarını değiştirme yetkiniz yok!';
+$_['error_title']      = 'Ağırlık Başlığı 3 ile 32 karakter arasında olmalıdır!';
+$_['error_unit']       = 'Ağırlık Birimi 1 ile 4 karakter arasında olmalıdır!';
+$_['error_default']    = 'Uyarı: Bu ağırlık sınıfı varsayılan mağaza ağırlık sınıfı olarak atandığı için silinemez!';
+$_['error_product']    = 'Uyarı: Bu ağırlık sınıfı %s ürüne atanmış olduğu için silinemez!';
