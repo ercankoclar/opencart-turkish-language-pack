3a2181b10062239a98048ef63bf1511579e0e4ef
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:39:33 +0300

    Update public_html/admin/language/tr-tr/mail/customer_deny.php

--- public_html/admin/language/tr-tr/mail/customer_deny.php
+++ public_html/admin/language/tr-tr/mail/customer_deny.php
@@ -1,9 +1,9 @@
 <?php
 // Text
-$_['text_subject']   = '%s - Your Account has been denied!';
-$_['text_welcome']   = 'Welcome and thank you for registering at %s!';
-$_['text_denied']    = 'Unfortunately your request has been denied. For more information you can contact the store owner here:';
-$_['text_thanks']    = 'Thanks,';
+$_['text_subject']   = '%s - Hesabınız reddedildi!';
+$_['text_welcome']   = 'Hoş geldiniz ve %s sitesine kayıt olduğunuz için teşekkür ederiz!';
+$_['text_denied']    = 'Maalesef talebiniz reddedildi. Daha fazla bilgi için mağaza sahibiyle buradan iletişime geçebilirsiniz:';
+$_['text_thanks']    = 'Teşekkürler,';
 
 // Button
-$_['button_contact'] = 'Contact Us';
+$_['button_contact'] = 'Bize Ulaşın';
