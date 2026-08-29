8cd93363be85c302f9d30dd3a2ff330c6877915a
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:40:51 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/report/sale_return.php

--- public_html/extension/opencart/admin/language/tr-tr/report/sale_return.php
+++ public_html/extension/opencart/admin/language/tr-tr/report/sale_return.php
@@ -1,30 +1,30 @@
 <?php
 // Heading
-$_['heading_title']       = 'Returns Report';
+$_['heading_title']       = 'İadeler Raporu';
 
 // Text
 $_['text_extension']      = 'Eklentiler';
-$_['text_edit']           = 'Edit Returns Report';
-$_['text_success']        = 'Success: You have modified returns report!';
+$_['text_edit']           = 'İadeler Raporunu Düzenle';
+$_['text_success']        = 'Başarılı: İadeler raporunu değiştirdiniz!';
 $_['text_filter']         = 'Filtrele';
-$_['text_year']           = 'Years';
-$_['text_month']          = 'Months';
-$_['text_week']           = 'Weeks';
-$_['text_day']            = 'Days';
-$_['text_all_status']     = 'All Statuses';
+$_['text_year']           = 'Yıllar';
+$_['text_month']          = 'Aylar';
+$_['text_week']           = 'Haftalar';
+$_['text_day']            = 'Günler';
+$_['text_all_status']     = 'Tüm Durumlar';
 
 // Column
 $_['column_date_start']   = 'Başlangıç Tarihi';
 $_['column_date_end']     = 'Bitiş Tarihi';
-$_['column_returns']      = 'No. Returns';
+$_['column_returns']      = 'İade Sayısı';
 
 // Entry
 $_['entry_date_start']    = 'Başlangıç Tarihi';
 $_['entry_date_end']      = 'Bitiş Tarihi';
-$_['entry_group']         = 'Group By';
-$_['entry_return_status'] = 'Return Status';
+$_['entry_group']         = 'Gruplama';
+$_['entry_return_status'] = 'İade Durumu';
 $_['entry_status']        = 'Durum';
 $_['entry_sort_order']    = 'Sıralama';
 
 // Error
-$_['error_permission']    = 'Warning: You do not have permission to modify returns report!';
+$_['error_permission']    = 'Uyarı: İadeler raporunu değiştirme yetkiniz yok!';
