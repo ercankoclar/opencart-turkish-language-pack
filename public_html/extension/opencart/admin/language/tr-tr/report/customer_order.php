36d4d54b817b16605a210d6bea2e1abcfced1066
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:35:07 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/report/customer_order.php

--- public_html/extension/opencart/admin/language/tr-tr/report/customer_order.php
+++ public_html/extension/opencart/admin/language/tr-tr/report/customer_order.php
@@ -1,21 +1,21 @@
 <?php
 // Heading
-$_['heading_title']         = 'Customer Orders Report';
+$_['heading_title']         = 'Müşteri Siparişleri Raporu';
 
 // Text
 $_['text_extension']        = 'Eklentiler';
-$_['text_edit']             = 'Edit Customer Orders Report';
-$_['text_success']          = 'Success: You have modified customer orders report!';
+$_['text_edit']             = 'Müşteri Siparişleri Raporunu Düzenle';
+$_['text_success']          = 'Başarılı: Müşteri siparişleri raporunu değiştirdiniz!';
 $_['text_filter']           = 'Filtrele';
-$_['text_all_status']       = 'All Statuses';
+$_['text_all_status']       = 'Tüm Durumlar';
 
 // Column
-$_['column_customer']       = 'Customer Name';
-$_['column_email']          = 'E-Mail';
-$_['column_customer_group'] = 'Customer Group';
+$_['column_customer']       = 'Müşteri Adı';
+$_['column_email']          = 'E-posta';
+$_['column_customer_group'] = 'Müşteri Grubu';
 $_['column_status']         = 'Durum';
-$_['column_orders']         = 'No. Orders';
-$_['column_products']       = 'No. Products';
+$_['column_orders']         = 'Sipariş Sayısı';
+$_['column_products']       = 'Ürün Sayısı';
 $_['column_total']          = 'Toplam';
 $_['column_action']         = 'İşlem';
 
@@ -23,9 +23,9 @@ $_['column_action']         = 'İşlem';
 $_['entry_date_start']      = 'Başlangıç Tarihi';
 $_['entry_date_end']        = 'Bitiş Tarihi';
 $_['entry_customer']        = 'Müşteri';
-$_['entry_order_status']    = 'Order Status';
+$_['entry_order_status']    = 'Sipariş Durumu';
 $_['entry_status']          = 'Durum';
 $_['entry_sort_order']      = 'Sıralama';
 
 // Error
-$_['error_permission']      = 'Warning: You do not have permission to modify customer orders report!';
+$_['error_permission']      = 'Uyarı: Müşteri siparişleri raporunu değiştirme yetkiniz yok!';
