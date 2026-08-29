2ed9b4b566cd905438f123e8e1789b9909ff9f6a
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:45:34 +0300

    Update public_html/admin/language/tr-tr/marketing/coupon.php

--- public_html/admin/language/tr-tr/marketing/coupon.php
+++ public_html/admin/language/tr-tr/marketing/coupon.php
@@ -3,54 +3,54 @@
 $_['heading_title']       = 'Kuponlar';
 
 // Text
-$_['text_success']        = 'Success: You have modified coupons!';
-$_['text_list']           = 'Coupon List';
-$_['text_add']            = 'Add Coupon';
-$_['text_edit']           = 'Edit Coupon';
+$_['text_success']        = 'Başarılı: Kuponları değiştirdiniz!';
+$_['text_list']           = 'Kupon Listesi';
+$_['text_add']            = 'Kupon Ekle';
+$_['text_edit']           = 'Kuponu Düzenle';
 $_['text_percent']        = 'Yüzde';
-$_['text_amount']         = 'Fixed Amount';
-$_['text_coupon']         = 'History';
+$_['text_amount']         = 'Sabit Tutar';
+$_['text_coupon']         = 'Geçmiş';
 
 // Column
-$_['column_name']         = 'Coupon Name';
+$_['column_name']         = 'Kupon Adı';
 $_['column_code']         = 'Kod';
 $_['column_discount']     = 'İndirim';
 $_['column_date_start']   = 'Başlangıç Tarihi';
 $_['column_date_end']     = 'Bitiş Tarihi';
 $_['column_status']       = 'Durum';
-$_['column_order_id']     = 'Order ID';
+$_['column_order_id']     = 'Sipariş No';
 $_['column_customer']     = 'Müşteri';
 $_['column_amount']       = 'Tutar';
 $_['column_date_added']   = 'Eklenme Tarihi';
 $_['column_action']       = 'İşlem';
 
 // Entry
-$_['entry_name']          = 'Coupon Name';
+$_['entry_name']          = 'Kupon Adı';
 $_['entry_code']          = 'Kod';
-$_['entry_type']          = 'Type';
+$_['entry_type']          = 'Tür';
 $_['entry_discount']      = 'İndirim';
 $_['entry_logged']        = 'Müşteri Girişi';
 $_['entry_shipping']      = 'Ücretsiz Kargo';
-$_['entry_total']         = 'Total Amount';
+$_['entry_total']         = 'Toplam Tutar';
 $_['entry_category']      = 'Kategori';
 $_['entry_product']       = 'Ürünler';
-$_['entry_date']          = 'Dates';
+$_['entry_date']          = 'Tarihler';
 $_['entry_uses_total']    = 'Kupon Başına Kullanım Sayısı';
 $_['entry_uses_customer'] = 'Müşteri Başına Kullanım Sayısı';
 $_['entry_status']        = 'Durum';
 
 // Help
-$_['help_code']           = 'The code the customer enters to get the discount.';
-$_['help_type']           = 'Percentage or Fixed Amount.';
-$_['help_logged']         = 'Customer must be logged in to use the coupon.';
-$_['help_total']          = 'The total amount that must be reached before the coupon is valid.';
-$_['help_category']       = 'Choose all products under selected category.';
-$_['help_product']        = 'Choose specific products the coupon will apply to. Select no products to apply coupon to entire cart.';
-$_['help_uses_total']     = 'The maximum number of times the coupon can be used by any customer. Leave blank for unlimited';
-$_['help_uses_customer']  = 'The maximum number of times the coupon can be used by a single customer. Leave blank for unlimited';
+$_['help_code']           = 'Müşterinin indirimi almak için girdiği kod.';
+$_['help_type']           = 'Yüzde veya Sabit Tutar.';
+$_['help_logged']         = 'Kuponu kullanmak için müşterinin giriş yapmış olması gerekir.';
+$_['help_total']          = 'Kuponun geçerli olması için ulaşılması gereken toplam tutar.';
+$_['help_category']       = 'Seçilen kategori altındaki tüm ürünleri seçin.';
+$_['help_product']        = 'Kuponun uygulanacağı belirli ürünleri seçin. Kuponun tüm sepete uygulanması için ürün seçmeyin.';
+$_['help_uses_total']     = 'Kuponun herhangi bir müşteri tarafından kullanılabileceği maksimum sayı. Sınırsız kullanım için boş bırakın.';
+$_['help_uses_customer']  = 'Kuponun tek bir müşteri tarafından kullanılabileceği maksimum sayı. Sınırsız kullanım için boş bırakın.';
 
 // Error
-$_['error_permission']    = 'Warning: You do not have permission to modify coupons!';
-$_['error_exists']        = 'Warning: Coupon code is already in use!';
-$_['error_name']          = 'Coupon Name must be between 3 and 128 characters!';
-$_['error_code']          = 'Code must be between 3 and 20 characters!';
+$_['error_permission']    = 'Uyarı: Kuponları değiştirme yetkiniz yok!';
+$_['error_exists']        = 'Uyarı: Kupon kodu zaten kullanılıyor!';
+$_['error_name']          = 'Kupon Adı 3 ile 128 karakter arasında olmalıdır!';
+$_['error_code']          = 'Kod 3 ile 20 karakter arasında olmalıdır!';
