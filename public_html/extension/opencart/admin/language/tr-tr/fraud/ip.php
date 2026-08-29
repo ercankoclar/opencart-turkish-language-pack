d43d571b83a2a42d46615a3343328063bd45c4a1
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:26:50 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/fraud/ip.php

--- public_html/extension/opencart/admin/language/tr-tr/fraud/ip.php
+++ public_html/extension/opencart/admin/language/tr-tr/fraud/ip.php
@@ -4,27 +4,27 @@ $_['heading_title']      = 'IP';
 
 // Text
 $_['text_extension']     = 'Eklentiler';
-$_['text_fraud']         = 'Anti-Fraud';
-$_['text_success']       = 'Success: You have modified Anti-Fraud IP!';
-$_['text_edit']          = 'Edit Anti-Fraud IP';
-$_['text_ip_add']        = 'Add IP Address';
-$_['text_ip_list']       = 'Fraud IP Address List';
+$_['text_fraud']         = 'Dolandırıcılık Önleme';
+$_['text_success']       = 'Başarılı: Dolandırıcılık Önleme IP ayarlarını değiştirdiniz!';
+$_['text_edit']          = 'Dolandırıcılık Önleme IP Ayarlarını Düzenle';
+$_['text_ip_add']        = 'IP Adresi Ekle';
+$_['text_ip_list']       = 'Dolandırıcılık IP Adresleri Listesi';
 
 // Column
 $_['column_ip']          = 'IP';
-$_['column_total']       = 'Total Accounts';
-$_['column_date_added']  = 'Date Added';
-$_['column_action']      = 'Action';
+$_['column_total']       = 'Toplam Hesap';
+$_['column_date_added']  = 'Eklenme Tarihi';
+$_['column_action']      = 'İşlem';
 
 // Entry
 $_['entry_ip']           = 'IP';
 $_['entry_status']       = 'Durum';
-$_['entry_order_status'] = 'Order Status';
+$_['entry_order_status'] = 'Sipariş Durumu';
 
 // Help
-$_['help_order_status']  = 'Customers that have a banned IP on their accounts will be assigned this order status and will not be allowed to reach the complete status automatically.';
+$_['help_order_status']  = 'Hesaplarında yasaklanmış bir IP adresi bulunan müşterilere bu sipariş durumu atanır ve siparişlerin otomatik olarak tamamlandı durumuna ulaşmasına izin verilmez.';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify Anti-Fraud IP!';
-$_['error_required']     = 'IP Address required!';
-$_['error_invalid']      = 'IP Address invalid!';
+$_['error_permission']   = 'Uyarı: Dolandırıcılık Önleme IP ayarlarını değiştirme yetkiniz yok!';
+$_['error_required']     = 'IP Adresi gereklidir!';
+$_['error_invalid']      = 'IP Adresi geçersiz!';
