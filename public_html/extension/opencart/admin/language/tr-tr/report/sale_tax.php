30e869cb92db7c4db65910aeb2e5c93aacce858e
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:41:55 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/report/sale_tax.php

--- public_html/extension/opencart/admin/language/tr-tr/report/sale_tax.php
+++ public_html/extension/opencart/admin/language/tr-tr/report/sale_tax.php
@@ -1,32 +1,32 @@
 <?php
 // Heading
-$_['heading_title']      = 'Tax Report';
+$_['heading_title']      = 'Vergi Raporu';
 
 // Text
 $_['text_extension']     = 'Eklentiler';
-$_['text_edit']          = 'Edit Tax Report';
-$_['text_success']       = 'Success: You have modified tax report!';
+$_['text_edit']          = 'Vergi Raporunu Düzenle';
+$_['text_success']       = 'Başarılı: Vergi raporunu değiştirdiniz!';
 $_['text_filter']        = 'Filtrele';
-$_['text_year']          = 'Years';
-$_['text_month']         = 'Months';
-$_['text_week']          = 'Weeks';
-$_['text_day']           = 'Days';
-$_['text_all_status']    = 'All Statuses';
+$_['text_year']          = 'Yıllar';
+$_['text_month']         = 'Aylar';
+$_['text_week']          = 'Haftalar';
+$_['text_day']           = 'Günler';
+$_['text_all_status']    = 'Tüm Durumlar';
 
 // Column
 $_['column_date_start']  = 'Başlangıç Tarihi';
 $_['column_date_end']    = 'Bitiş Tarihi';
-$_['column_title']       = 'Tax Title';
-$_['column_orders']      = 'No. Orders';
+$_['column_title']       = 'Vergi Başlığı';
+$_['column_orders']      = 'Sipariş Sayısı';
 $_['column_total']       = 'Toplam';
 
 // Entry
 $_['entry_date_start']   = 'Başlangıç Tarihi';
 $_['entry_date_end']     = 'Bitiş Tarihi';
-$_['entry_group']        = 'Group By';
-$_['entry_order_status'] = 'Order Status';
+$_['entry_group']        = 'Gruplama';
+$_['entry_order_status'] = 'Sipariş Durumu';
 $_['entry_status']       = 'Durum';
 $_['entry_sort_order']   = 'Sıralama';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify tax report!';
+$_['error_permission']   = 'Uyarı: Vergi raporunu değiştirme yetkiniz yok!';
