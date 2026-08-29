f172cc256a742aea818077b054bb8b13f87454c3
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:47:19 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/total/credit.php

--- public_html/extension/opencart/admin/language/tr-tr/total/credit.php
+++ public_html/extension/opencart/admin/language/tr-tr/total/credit.php
@@ -4,12 +4,12 @@ $_['heading_title']    = 'Mağaza Kredisi';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified store credit total!';
-$_['text_edit']        = 'Edit Store Credit Total';
+$_['text_success']     = 'Başarılı: Mağaza kredisi toplamını değiştirdiniz!';
+$_['text_edit']        = 'Mağaza Kredisi Toplamını Düzenle';
 
 // Entry
 $_['entry_status']     = 'Durum';
 $_['entry_sort_order'] = 'Sıralama';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify store credit total!';
+$_['error_permission'] = 'Uyarı: Mağaza kredisi toplamını değiştirme yetkiniz yok!';
