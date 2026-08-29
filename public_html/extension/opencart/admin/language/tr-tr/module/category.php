50de566829fd335e376ba54454caf7c3400b9400
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:28:54 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/module/category.php

--- public_html/extension/opencart/admin/language/tr-tr/module/category.php
+++ public_html/extension/opencart/admin/language/tr-tr/module/category.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title']    = 'Category';
+$_['heading_title']    = 'Kategori';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified category module!';
-$_['text_edit']        = 'Edit Category Module';
+$_['text_success']     = 'Başarılı: Kategori modülünü değiştirdiniz!';
+$_['text_edit']        = 'Kategori Modülünü Düzenle';
 
 // Entry
 $_['entry_status']     = 'Durum';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify category module!';
+$_['error_permission'] = 'Uyarı: Kategori modülünü değiştirme yetkiniz yok!';
