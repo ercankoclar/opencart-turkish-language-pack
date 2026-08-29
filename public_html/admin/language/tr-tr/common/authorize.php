1d8e99cc889f12bc3df6638f1c4c4bd6516f55d5
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:10:46 +0300

    Update public_html/admin/language/tr-tr/common/authorize.php

--- public_html/admin/language/tr-tr/common/authorize.php
+++ public_html/admin/language/tr-tr/common/authorize.php
@@ -1,24 +1,25 @@
 <?php
+
 // Heading
-$_['heading_title'] = 'Protect your account';
+$_['heading_title'] = 'Hesabınızı Koruyun';
 
 // Text
-$_['text_security'] = 'You must answer security question whenever you login from an unknown device or computer.';
-$_['text_code']     = 'Click the send button to send a security code to your email account. Enter the code below to continue.';
-$_['text_sent']     = 'Success: An email has been sent to your email account with a security code!';
-$_['text_locked']   = 'Your account has been locked!';
-$_['text_reset']    = 'Your account has been locked because of to many incorrect attempts at entering the security code. Click the reset button to receive a reset link via your email account.';
-$_['text_link']     = 'Success: An email with a reset link has been sent your email address!';
-$_['text_unlocked'] = 'Your account has been unlocked!';
-$_['text_unlock']   = 'Your account has been unlocked. You will have to re-login and repeat the 2FA process again to continue.';
-$_['text_failed']   = 'Could not reset your security code!';
+$_['text_security'] = 'Bilinmeyen bir cihaz veya bilgisayardan giriş yaptığınızda güvenlik sorusunu yanıtlamanız gerekir.';
+$_['text_code']     = 'Güvenlik kodunu e-posta hesabınıza göndermek için gönder düğmesine tıklayın. Devam etmek için aşağıdaki kodu girin.';
+$_['text_sent']     = 'Başarılı: Güvenlik kodunu içeren bir e-posta hesabınıza gönderildi!';
+$_['text_locked']   = 'Hesabınız kilitlendi!';
+$_['text_reset']    = 'Güvenlik kodunu girerken çok fazla yanlış deneme yaptığınız için hesabınız kilitlendi. E-posta hesabınız üzerinden sıfırlama bağlantısı almak için sıfırla düğmesine tıklayın.';
+$_['text_link']     = 'Başarılı: Sıfırlama bağlantısını içeren bir e-posta adresinize gönderildi!';
+$_['text_unlocked'] = 'Hesabınızın kilidi açıldı!';
+$_['text_unlock']   = 'Hesabınızın kilidi açıldı. Devam etmek için yeniden giriş yapmanız ve 2FA işlemini tekrar gerçekleştirmeniz gerekir.';
+$_['text_failed']   = 'Güvenlik kodunuz sıfırlanamadı!';
 
 // Entry
-$_['entry_code']    = 'Security Code';
+$_['entry_code']    = 'Güvenlik Kodu';
 
 // Button
-$_['button_send']   = 'Send';
-$_['button_reset']  = 'Reset';
+$_['button_send']   = 'Gönder';
+$_['button_reset']  = 'Sıfırla';
 
 // Error
-$_['error_code']    = 'Security code does not match!';
+$_['error_code']    = 'Güvenlik kodu eşleşmiyor!';
