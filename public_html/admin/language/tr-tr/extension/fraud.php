99ea7acaaa908ec3ca929d0771ae66ea5177a278
ercankoclar <ercankoclar@duck.com>
2026-08-29 17:57:24 +0300

    Upload files to "public_html/admin/language/tr-tr/extension"

---
+++ public_html/admin/language/tr-tr/extension/fraud.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']    = 'Anti-Fraud';
+
+// Text
+$_['text_success']     = 'Success: You have modified anti-fraud!';
+$_['text_list']        = 'Anti-Fraud List';
+
+// Column
+$_['column_name']      = 'Anti-Fraud Name';
+$_['column_status']    = 'Durum';
+$_['column_action']    = 'İşlem';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify anti-fraud!';
+$_['error_extension']  = 'Warning: Extension does not exist!';
