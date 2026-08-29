a88f507c96306394351085f63b2d66db0ddb4679
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:38:39 +0300

    Update public_html/admin/language/tr-tr/mail/authorize.php

--- public_html/admin/language/tr-tr/mail/authorize.php
+++ public_html/admin/language/tr-tr/mail/authorize.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_subject'] = 'Security';
-$_['text_code']    = 'You must enter the security code in the admin security check.';
+$_['text_subject'] = 'Güvenlik';
+$_['text_code']    = 'Yönetici güvenlik kontrolünde güvenlik kodunu girmeniz gerekir.';
 $_['text_ip']      = 'IP:';
-$_['text_regards'] = 'Best Regards';
+$_['text_regards'] = 'Saygılarımla';
