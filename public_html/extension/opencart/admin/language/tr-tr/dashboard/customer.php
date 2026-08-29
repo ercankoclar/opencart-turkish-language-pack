b15d7518a2f63507adcfb0746e06df7489defd55
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:24:17 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/dashboard/customer.php

--- public_html/extension/opencart/admin/language/tr-tr/dashboard/customer.php
+++ public_html/extension/opencart/admin/language/tr-tr/dashboard/customer.php
@@ -1,17 +1,17 @@
 <?php
 // Heading
-$_['heading_title']    = 'Total Customers';
+$_['heading_title']    = 'Toplam Müşteri';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified dashboard customer!';
-$_['text_edit']        = 'Edit Dashboard Customer';
-$_['text_view']        = 'View more...';
+$_['text_success']     = 'Başarılı: Kontrol paneli müşteri bölümünü değiştirdiniz!';
+$_['text_edit']        = 'Kontrol Paneli Müşteri Bölümünü Düzenle';
+$_['text_view']        = 'Daha fazlasını görüntüle...';
 
 // Entry
 $_['entry_status']     = 'Durum';
-$_['entry_sort_order'] = 'Sort Order';
-$_['entry_width']      = 'Width';
+$_['entry_sort_order'] = 'Sıralama';
+$_['entry_width']      = 'Genişlik';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify dashboard customer!';
+$_['error_permission'] = 'Uyarı: Kontrol paneli müşteri bölümünü değiştirme yetkiniz yok!';
