96fb79de23262d0ee095d03efc7ac03276d5ffdd
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:53:19 +0300

    Update public_html/extension/opencart/catalog/language/tr-tr/module/account.php

--- public_html/extension/opencart/catalog/language/tr-tr/module/account.php
+++ public_html/extension/opencart/catalog/language/tr-tr/module/account.php
@@ -1,22 +1,22 @@
 <?php
 // Heading
-$_['heading_title']       = 'Account';
+$_['heading_title']       = 'Hesap';
 
 // Text
-$_['text_register']       = 'Register';
-$_['text_login']          = 'Login';
-$_['text_logout']         = 'Logout';
-$_['text_forgotten']      = 'Forgotten Password';
-$_['text_account']        = 'My Account';
-$_['text_edit']           = 'Edit Account';
-$_['text_password']       = 'Password';
-$_['text_address']        = 'Address Book';
-$_['text_wishlist']       = 'Wish List';
-$_['text_order']          = 'Order History';
-$_['text_download']       = 'Downloads';
-$_['text_reward']         = 'Reward Points';
-$_['text_return']         = 'Returns';
-$_['text_transaction']    = 'Transactions';
-$_['text_newsletter']     = 'Newsletter';
-$_['text_subscription']   = 'Subscriptions';
-$_['text_payment_method'] = 'Payment Methods';
+$_['text_register']       = 'Kayıt Ol';
+$_['text_login']          = 'Giriş Yap';
+$_['text_logout']         = 'Çıkış Yap';
+$_['text_forgotten']      = 'Şifremi Unuttum';
+$_['text_account']        = 'Hesabım';
+$_['text_edit']           = 'Hesabı Düzenle';
+$_['text_password']       = 'Şifre';
+$_['text_address']        = 'Adres Defteri';
+$_['text_wishlist']       = 'Favorilerim';
+$_['text_order']          = 'Sipariş Geçmişi';
+$_['text_download']       = 'İndirmeler';
+$_['text_reward']         = 'Ödül Puanları';
+$_['text_return']         = 'İadeler';
+$_['text_transaction']    = 'İşlemler';
+$_['text_newsletter']     = 'Bülten';
+$_['text_subscription']   = 'Abonelikler';
+$_['text_payment_method'] = 'Ödeme Yöntemleri';
