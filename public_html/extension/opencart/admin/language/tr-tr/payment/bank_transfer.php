8d6ff3bffcd4aaf77750ea8fe71893548e01d76f
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:32:43 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/payment/bank_transfer.php

--- public_html/extension/opencart/admin/language/tr-tr/payment/bank_transfer.php
+++ public_html/extension/opencart/admin/language/tr-tr/payment/bank_transfer.php
@@ -1,19 +1,19 @@
 <?php
 // Heading
-$_['heading_title']      = 'Bank Transfer';
+$_['heading_title']      = 'Banka Havalesi';
 
 // Text
 $_['text_extension']     = 'Eklentiler';
-$_['text_success']       = 'Success: You have modified bank transfer details!';
-$_['text_edit']          = 'Edit Bank Transfer';
+$_['text_success']       = 'Başarılı: Banka havalesi bilgilerini değiştirdiniz!';
+$_['text_edit']          = 'Banka Havalesini Düzenle';
 
 // Entry
-$_['entry_bank']         = 'Bank Transfer Instructions';
+$_['entry_bank']         = 'Banka Havalesi Talimatları';
 $_['entry_order_status'] = 'Sipariş Durumu';
 $_['entry_geo_zone']     = 'Coğrafi Bölge';
 $_['entry_status']       = 'Durum';
 $_['entry_sort_order']   = 'Sıralama';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify payment bank transfer!';
-$_['error_bank']         = 'Bank Transfer Instructions required!';
+$_['error_permission']   = 'Uyarı: Banka havalesi ödeme yöntemini değiştirme yetkiniz yok!';
+$_['error_bank']         = 'Banka havalesi talimatları gereklidir!';
