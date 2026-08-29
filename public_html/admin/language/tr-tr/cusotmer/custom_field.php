26f12d5347ad42a3077a5e371391cac1842f9d54
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:15:39 +0300

    Update public_html/admin/language/tr-tr/cusotmer/custom_field.php

--- public_html/admin/language/tr-tr/cusotmer/custom_field.php
+++ public_html/admin/language/tr-tr/cusotmer/custom_field.php
@@ -1,56 +1,56 @@
 <?php
 // Heading
-$_['heading_title']        = 'Custom Fields';
+$_['heading_title']        = 'Özel Alanlar';
 
 // Text
-$_['text_success']         = 'Success: You have modified custom fields!';
-$_['text_list']            = 'Custom Field List';
-$_['text_add']             = 'Add Custom Field';
-$_['text_edit']            = 'Edit Custom Field';
-$_['text_choose']          = 'Choose';
-$_['text_select']          = 'Select';
-$_['text_radio']           = 'Radio';
-$_['text_checkbox']        = 'Checkbox';
-$_['text_input']           = 'Input';
-$_['text_text']            = 'Text';
-$_['text_textarea']        = 'Textarea';
-$_['text_file']            = 'File';
-$_['text_date']            = 'Date';
-$_['text_datetime']        = 'Date &amp; Time';
-$_['text_time']            = 'Time';
+$_['text_success']         = 'Başarılı: Özel alanları değiştirdiniz!';
+$_['text_list']            = 'Özel Alan Listesi';
+$_['text_add']             = 'Özel Alan Ekle';
+$_['text_edit']            = 'Özel Alanı Düzenle';
+$_['text_choose']          = 'Seç';
+$_['text_select']          = 'Seçim';
+$_['text_radio']           = 'Radyo';
+$_['text_checkbox']        = 'Onay Kutusu';
+$_['text_input']           = 'Girdi';
+$_['text_text']            = 'Metin';
+$_['text_textarea']        = 'Metin Alanı';
+$_['text_file']            = 'Dosya';
+$_['text_date']            = 'Tarih';
+$_['text_datetime']        = 'Tarih ve Saat';
+$_['text_time']            = 'Saat';
 $_['text_account']         = 'Hesap';
 $_['text_address']         = 'Adres';
-$_['text_affiliate']       = 'Affiliate';
+$_['text_affiliate']       = 'İş Ortağı';
 $_['text_regex']           = 'Regex';
-$_['text_custom_field']    = 'Custom Field';
-$_['text_value']           = 'Custom Field Values';
+$_['text_custom_field']    = 'Özel Alan';
+$_['text_value']           = 'Özel Alan Değerleri';
 
 // Column
-$_['column_name']          = 'Custom Field Name';
-$_['column_location']      = 'Location';
-$_['column_type']          = 'Type';
+$_['column_name']          = 'Özel Alan Adı';
+$_['column_location']      = 'Konum';
+$_['column_type']          = 'Tür';
 $_['column_status']        = 'Durum';
 $_['column_sort_order']    = 'Sıralama';
 $_['column_action']        = 'İşlem';
 
 // Entry
-$_['entry_name']           = 'Custom Field Name';
-$_['entry_location']       = 'Location';
-$_['entry_type']           = 'Type';
-$_['entry_value']          = 'Value';
-$_['entry_validation']     = 'Validation';
-$_['entry_custom_value']   = 'Custom Field Value Name';
+$_['entry_name']           = 'Özel Alan Adı';
+$_['entry_location']       = 'Konum';
+$_['entry_type']           = 'Tür';
+$_['entry_value']          = 'Değer';
+$_['entry_validation']     = 'Doğrulama';
+$_['entry_custom_value']   = 'Özel Alan Değer Adı';
 $_['entry_customer_group'] = 'Müşteri Grubu';
-$_['entry_required']       = 'Required';
+$_['entry_required']       = 'Zorunlu';
 $_['entry_status']         = 'Durum';
 $_['entry_sort_order']     = 'Sıralama';
 
 // Help
-$_['help_regex']           = 'Use regex. E.g: /[^a-zA-Z0-9_-]/';
-$_['help_sort_order']      = 'Use minus to count backwards from the last field in the set.';
+$_['help_regex']           = 'Regex kullanın. Örneğin: /[^a-zA-Z0-9_-]/';
+$_['help_sort_order']      = 'Listedeki son alandan geriye doğru saymak için eksi değer kullanın.';
 
 // Error
-$_['error_permission']     = 'Warning: You do not have permission to modify custom fields!';
-$_['error_name']           = 'Custom Field Name must be between 1 and 128 characters!';
-$_['error_type']           = 'Warning: Custom Field Values required!';
-$_['error_custom_value']   = 'Custom Value Name must be between 1 and 128 characters!';
+$_['error_permission']     = 'Uyarı: Özel alanları değiştirme yetkiniz yok!';
+$_['error_name']           = 'Özel Alan Adı 1 ile 128 karakter arasında olmalıdır!';
+$_['error_type']           = 'Uyarı: Özel Alan Değerleri gereklidir!';
+$_['error_custom_value']   = 'Özel Değer Adı 1 ile 128 karakter arasında olmalıdır!';
