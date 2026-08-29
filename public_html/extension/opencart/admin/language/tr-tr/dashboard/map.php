3b151a10b518011b3710771470600b5fe17f3ddd
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:24:40 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/dashboard/map.php

--- public_html/extension/opencart/admin/language/tr-tr/dashboard/map.php
+++ public_html/extension/opencart/admin/language/tr-tr/dashboard/map.php
@@ -1,18 +1,18 @@
 <?php
 // Heading
-$_['heading_title']    = 'World Map';
+$_['heading_title']    = 'Dünya Haritası';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified dashboard map!';
-$_['text_edit']        = 'Edit Dashboard Map';
-$_['text_order']       = 'Orders';
-$_['text_sale']        = 'Sales';
+$_['text_success']     = 'Başarılı: Kontrol paneli haritasını değiştirdiniz!';
+$_['text_edit']        = 'Kontrol Paneli Haritasını Düzenle';
+$_['text_order']       = 'Siparişler';
+$_['text_sale']        = 'Satışlar';
 
 // Entry
 $_['entry_status']     = 'Durum';
-$_['entry_sort_order'] = 'Sort Order';
-$_['entry_width']      = 'Width';
+$_['entry_sort_order'] = 'Sıralama';
+$_['entry_width']      = 'Genişlik';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify dashboard map!';
+$_['error_permission'] = 'Uyarı: Kontrol paneli haritasını değiştirme yetkiniz yok!';
