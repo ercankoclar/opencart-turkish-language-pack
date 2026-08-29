9d8c7edd2d25f27c3c5a73c22a9698ffc852fd35
ercankoclar <ercankoclar@duck.com>
2026-08-29 18:13:33 +0300

    Update public_html/admin/language/tr-tr/common/header.php

--- public_html/admin/language/tr-tr/common/header.php
+++ public_html/admin/language/tr-tr/common/header.php
@@ -4,11 +4,11 @@ $_['heading_title']          = 'OpenCart';
 
 // Text
 $_['text_notification_all']  = 'Tümünü Göster';
-$_['text_notification_none'] = 'There are no notifications';
-$_['text_profile']           = 'Your Profile';
-$_['text_store']             = 'Stores';
-$_['text_help']              = 'Help';
-$_['text_homepage']          = 'OpenCart Homepage';
-$_['text_support']           = 'Support Forum';
-$_['text_documentation']     = 'Documentation';
+$_['text_notification_none'] = 'Bildirim bulunmuyor';
+$_['text_profile']           = 'Profiliniz';
+$_['text_store']             = 'Mağazalar';
+$_['text_help']              = 'Yardım';
+$_['text_homepage']          = 'OpenCart Ana Sayfası';
+$_['text_support']           = 'Destek Forumu';
+$_['text_documentation']     = 'Belgelendirme';
 $_['text_logout']            = 'Çıkış Yap';
