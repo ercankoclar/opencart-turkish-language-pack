dbea9983eccc6ccac67b6b8827bf949f697c13ee
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:47:49 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/total/handling.php

--- public_html/extension/opencart/admin/language/tr-tr/total/handling.php
+++ public_html/extension/opencart/admin/language/tr-tr/total/handling.php
@@ -1,21 +1,21 @@
 <?php
 // Heading
-$_['heading_title']    = 'Handling Fee';
+$_['heading_title']    = 'İşlem Ücreti';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified handling fee total!';
-$_['text_edit']        = 'Edit Handling Fee Total';
+$_['text_success']     = 'Başarılı: İşlem ücreti toplamını değiştirdiniz!';
+$_['text_edit']        = 'İşlem Ücreti Toplamını Düzenle';
 
 // Entry
 $_['entry_total']      = 'Sipariş Toplamı';
-$_['entry_fee']        = 'Fee';
-$_['entry_tax_class']  = 'Tax Class';
+$_['entry_fee']        = 'Ücret';
+$_['entry_tax_class']  = 'Vergi Sınıfı';
 $_['entry_status']     = 'Durum';
 $_['entry_sort_order'] = 'Sıralama';
 
 // Help
-$_['help_total']       = 'The checkout total the order must reach before this order total becomes active.';
+$_['help_total']       = 'Bu sipariş toplamının etkin hale gelmesi için siparişin ulaşması gereken toplam tutar.';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify handling fee total!';
+$_['error_permission'] = 'Uyarı: İşlem ücreti toplamını değiştirme yetkiniz yok!';
