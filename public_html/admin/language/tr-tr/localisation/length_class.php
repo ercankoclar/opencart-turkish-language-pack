c1a1c83a070f9e3a39f80ad4374b340a1ff2a737
ercankoclar <ercankoclar@duck.com>
2026-08-29 17:58:20 +0300

    Upload files to "public_html/admin/language/tr-tr/localisation"

---
+++ public_html/admin/language/tr-tr/localisation/length_class.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']    = 'Uzunluk Sınıfları';
+
+// Text
+$_['text_success']     = 'Success: You have modified length classes!';
+$_['text_list']        = 'Length Class List';
+$_['text_add']         = 'Add Length Class';
+$_['text_edit']        = 'Edit Length Class';
+
+// Column
+$_['column_title']     = 'Length Title';
+$_['column_unit']      = 'Length Unit';
+$_['column_value']     = 'Değer';
+$_['column_action']    = 'İşlem';
+
+// Entry
+$_['entry_title']      = 'Length Title';
+$_['entry_unit']       = 'Length Unit';
+$_['entry_value']      = 'Değer';
+
+// Help
+$_['help_value']       = 'The value of your default length in the current length unit. Set to 1 for your default length.';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify length classes!';
+$_['error_title']      = 'Length Title must be between 3 and 32 characters!';
+$_['error_unit']       = 'Length Unit must be between 1 and 4 characters!';
+$_['error_default']    = 'Warning: This length class cannot be deleted as it is currently assigned as the default store length class!';
+$_['error_product']    = 'Warning: This length class cannot be deleted as it is currently assigned to %s products!';
