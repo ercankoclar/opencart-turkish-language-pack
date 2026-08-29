bb800c08560de467d83c686e8e1027a7dbaea082
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:38:19 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/report/product_purchased.php

--- public_html/extension/opencart/admin/language/tr-tr/report/product_purchased.php
+++ public_html/extension/opencart/admin/language/tr-tr/report/product_purchased.php
@@ -1,18 +1,18 @@
 <?php
 // Heading
-$_['heading_title']      = 'Products Purchased Report';
+$_['heading_title']      = 'Satın Alınan Ürünler Raporu';
 
 // Text
 $_['text_extension']     = 'Eklentiler';
-$_['text_edit']          = 'Edit Products Purchased Report';
-$_['text_success']       = 'Success: You have modified products purchased report!';
+$_['text_edit']          = 'Satın Alınan Ürünler Raporunu Düzenle';
+$_['text_success']       = 'Başarılı: Satın alınan ürünler raporunu değiştirdiniz!';
 $_['text_filter']        = 'Filtrele';
-$_['text_all_status']    = 'All Statuses';
+$_['text_all_status']    = 'Tüm Durumlar';
 
 // Column
 $_['column_date_start']  = 'Başlangıç Tarihi';
 $_['column_date_end']    = 'Bitiş Tarihi';
-$_['column_name']        = 'Product Name';
+$_['column_name']        = 'Ürün Adı';
 $_['column_model']       = 'Model';
 $_['column_quantity']    = 'Miktar';
 $_['column_total']       = 'Toplam';
@@ -20,9 +20,9 @@ $_['column_total']       = 'Toplam';
 // Entry
 $_['entry_date_start']   = 'Başlangıç Tarihi';
 $_['entry_date_end']     = 'Bitiş Tarihi';
-$_['entry_order_status'] = 'Order Status';
+$_['entry_order_status'] = 'Sipariş Durumu';
 $_['entry_status']       = 'Durum';
 $_['entry_sort_order']   = 'Sıralama';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify products purchased report!';
+$_['error_permission']   = 'Uyarı: Satın alınan ürünler raporunu değiştirme yetkiniz yok!';
