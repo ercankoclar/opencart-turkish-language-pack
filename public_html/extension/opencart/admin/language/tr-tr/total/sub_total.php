c484c6a007a708089e97906b248eaa18abbbddc7
ercankoclar <ercankoclar@duck.com>
2026-08-29 19:49:49 +0300

    Update public_html/extension/opencart/admin/language/tr-tr/total/sub_total.php

--- public_html/extension/opencart/admin/language/tr-tr/total/sub_total.php
+++ public_html/extension/opencart/admin/language/tr-tr/total/sub_total.php
@@ -4,12 +4,12 @@ $_['heading_title']    = 'Ara Toplam';
 
 // Text
 $_['text_extension']   = 'Eklentiler';
-$_['text_success']     = 'Success: You have modified sub-total total!';
-$_['text_edit']        = 'Edit Sub-Total Total';
+$_['text_success']     = 'Başarılı: Ara toplamı değiştirdiniz!';
+$_['text_edit']        = 'Ara Toplamı Düzenle';
 
 // Entry
 $_['entry_status']     = 'Durum';
 $_['entry_sort_order'] = 'Sıralama';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify sub-total total!';
+$_['error_permission'] = 'Uyarı: Ara toplamı değiştirme yetkiniz yok!';
